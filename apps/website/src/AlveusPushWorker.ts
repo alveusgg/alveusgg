5b75c12233d3081f0491efece3f6861d9daddd04
Paul Golmann <mail@pje-web.de>
2023-01-07 20:02:44 +0100

    fix notification click url base

--- apps/website/src/AlveusPushWorker.ts
+++ apps/website/src/AlveusPushWorker.ts
@@ -2,12 +2,12 @@ declare const self: ServiceWorkerGlobalScope;
 
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import superjson from "superjson";
+
 import { type AppRouter } from "./server/trpc/router/_app";
 
 const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+  if (process.env.VERCEL_URL) return `https://www.alveus.gg`;
+  return `http://localhost:${process.env.PORT ?? 3000}`;
 };
 
 const trpc = createTRPCProxyClient<AppRouter>({
