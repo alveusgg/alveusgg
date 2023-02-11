3dac1c57f0c20057464e40daaa52aea4d8cf8db3
MattIPv4 <me@mattcowley.co.uk>
2023-02-11 20:09:51 +0000

    Add board of directors page

---
+++ apps/website/src/components/content/People.tsx
@@ -0,0 +1,38 @@
+import Heading from "./Heading"
+import React from "react"
+
+type PeopleProps = {
+  people: Record<string, {
+    image: string
+    name: string
+    title: string
+    description: React.ReactNode
+  }>,
+  sideBySide?: boolean,
+};
+
+const People: React.FC<PeopleProps> = ({ people, sideBySide = false }) => (
+  <ul className={`flex flex-wrap ${sideBySide ? "md:justify-center md:items-start" : ""}`}>
+    {Object.entries(people).map(([ key, person ]) => (
+      <li key={key} className={`basis-full flex flex-col text-center items-center ${sideBySide ? "md:basis-1/2" : "md:flex-row md:text-left"}`}>
+        <div className={`flex-shrink-0 p-4 mx-auto max-w-md ${sideBySide ? "" : "md:w-1/3"}`}>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src={person.image} alt="" className="w-full aspect-square object-cover rounded-2xl" />
+        </div>
+        <div className={`flex-grow p-4 ${sideBySide ? "" : "md:w-2/3"}`}>
+          <Heading level={2} className="text-4xl">
+            {person.name}
+          </Heading>
+          <Heading level={3} className="text-xl">
+            {person.title}
+          </Heading>
+          <div className="text-gray-500">
+            {person.description}
+          </div>
+        </div>
+      </li>
+    ))}
+  </ul>
+);
+
+export default People;
