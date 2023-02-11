e46ca9615367b370b7d62f0b2e84fa4262a569d5
Paul Golmann <mail@pje-web.de>
2023-02-12 00:23:11 +0100

    Fix privacy policy link in footer

--- apps/website/src/components/layout/footer/Footer.tsx
+++ apps/website/src/components/layout/footer/Footer.tsx
@@ -1,33 +1,33 @@
 import React from "react";
 import Link from "next/link";
-import Socials from "./Socials"
+import Socials from "./Socials";
 
 export const Footer: React.FC = () => {
   return (
     <>
       <Socials />
 
-      <footer className="bg-gray-800 py-4 md:py-2 px-2 md:px-0 text-gray-500">
+      <footer className="bg-gray-800 py-4 px-2 text-gray-500 md:py-2 md:px-0">
         <div className="container mx-auto">
           <ul className="flex flex-wrap items-center justify-between">
-            <li className="basis-full md:basis-1/3 p-2">
+            <li className="basis-full p-2 md:basis-1/3">
               <p>
                 Built by the Alveus.gg team and community,
-                <br/>
+                <br />
                 supported by the Alveus team.
               </p>
             </li>
-            <li className="basis-full md:basis-1/3 p-2 md:text-center">
+            <li className="basis-full p-2 md:basis-1/3 md:text-center">
               <Link
-                className="underline decoration-gray-400 hover:text-gray-300 transition-colors"
-                href="/apps/website/src/pages/privacy-policy"
+                className="underline decoration-gray-400 transition-colors hover:text-gray-300"
+                href="/privacy-policy"
               >
                 Privacy Policy
               </Link>
             </li>
-            <li className="basis-full md:basis-1/3 p-2 md:text-right">
+            <li className="basis-full p-2 md:basis-1/3 md:text-right">
               <Link
-                className="underline decoration-gray-400 hover:text-gray-300 transition-colors"
+                className="underline decoration-gray-400 transition-colors hover:text-gray-300"
                 href="https://www.alveussanctuary.org"
                 target="_blank"
                 rel="noreferrer"
