e28f2a580bb35c605053e8ae980ec9131a3189b8
MattIPv4 <me@mattcowley.co.uk>
2023-02-12 16:38:38 +0000

    Updated navbar design

--- apps/website/src/components/layout/footer/Socials.tsx
+++ apps/website/src/components/layout/footer/Socials.tsx
@@ -1,40 +1,8 @@
 import Section from "../../content/Section"
 import React from "react"
 import Heading from "../../content/Heading"
-import IconTwitch from "../../../icons/IconTwitch"
-import IconInstagram from "../../../icons/IconInstagram"
-import IconTikTok from "../../../icons/IconTikTok"
-import IconTwitter from "../../../icons/IconTwitter"
-import IconYouTube from "../../../icons/IconYouTube"
 import socialsImage from "../../../assets/socials.png"
-
-const socials = {
-  instagram: {
-    link: "https://www.instagram.com/alveussanctuary",
-    title: "Instagram",
-    icon: IconInstagram,
-  },
-  tiktok: {
-    link: "https://www.tiktok.com/@alveussanctuary",
-    title: "TikTok",
-    icon: IconTikTok,
-  },
-  twitter: {
-    link: "https://twitter.com/AlveusSanctuary",
-    title: "Twitter",
-    icon: IconTwitter,
-  },
-  twitch: {
-    link: "https://twitch.tv/alveussanctuary",
-    title: "Twitch.tv",
-    icon: IconTwitch,
-  },
-  youtube: {
-    link: "https://www.youtube.com/c/AlveusSanctuary",
-    title: "YouTube",
-    icon: IconYouTube,
-  },
-};
+import socials from "../../shared/data/socials"
 
 const Socials = () => (
   <Section dark className="pb-0">
---
+++ apps/website/src/components/shared/data/socials.tsx
@@ -0,0 +1,35 @@
+import IconInstagram from "../../../icons/IconInstagram"
+import IconTikTok from "../../../icons/IconTikTok"
+import IconTwitter from "../../../icons/IconTwitter"
+import IconTwitch from "../../../icons/IconTwitch"
+import IconYouTube from "../../../icons/IconYouTube"
+
+const socials = {
+  instagram: {
+    link: "https://www.instagram.com/alveussanctuary",
+    title: "Instagram",
+    icon: IconInstagram,
+  },
+  tiktok: {
+    link: "https://www.tiktok.com/@alveussanctuary",
+    title: "TikTok",
+    icon: IconTikTok,
+  },
+  twitter: {
+    link: "https://twitter.com/AlveusSanctuary",
+    title: "Twitter",
+    icon: IconTwitter,
+  },
+  twitch: {
+    link: "https://twitch.tv/alveussanctuary",
+    title: "Twitch.tv",
+    icon: IconTwitch,
+  },
+  youtube: {
+    link: "https://www.youtube.com/c/AlveusSanctuary",
+    title: "YouTube",
+    icon: IconYouTube,
+  },
+};
+
+export default socials;
