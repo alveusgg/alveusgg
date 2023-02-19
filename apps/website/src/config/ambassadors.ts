d6568a8fc46f6091bdf0e9e03775380dbc0c14e1
MattIPv4 <me@mattcowley.co.uk>
2023-02-19 03:16:42 +0000

    Reorganize ambassador images to allow multiple

--- apps/website/src/config/ambassadors.ts
+++ apps/website/src/config/ambassadors.ts
@@ -1,38 +1,38 @@
 import type { ImageProps } from "next/image"
 
-import stompyImage from "../assets/ambassadors/stompy.jpg"
-import georgieImage from "../assets/ambassadors/georgie.jpg"
-import ticoImage from "../assets/ambassadors/tico.jpg"
-import mileyImage from "../assets/ambassadors/miley.jpg"
-import miaImage from "../assets/ambassadors/mia.jpg"
-import sirenImage from "../assets/ambassadors/siren.jpg"
-import abbottImage from "../assets/ambassadors/abbott.png"
-import coconutImage from "../assets/ambassadors/coconut.png"
-import oliverImage from "../assets/ambassadors/oliver.jpg"
-import nuggetImage from "../assets/ambassadors/nugget.jpg"
-import henriettaImage from "../assets/ambassadors/henrietta.jpg"
-import henriqueImage from "../assets/ambassadors/henrique.jpg"
-import noodleImage from "../assets/ambassadors/noodle.jpg"
-import patchyImage from "../assets/ambassadors/patchy.png"
-import fennImage from "../assets/ambassadors/fenn.jpg"
-import reedImage from "../assets/ambassadors/reed.jpg"
-import serranoImage from "../assets/ambassadors/serrano.jpg"
-import jalapenoImage from "../assets/ambassadors/jalapeno.jpg"
-import snorkImage from "../assets/ambassadors/snork.jpg"
-import moominImage from "../assets/ambassadors/moomin.jpg"
-import winnieImage from "../assets/ambassadors/winnie.jpg"
-import hankImage from "../assets/ambassadors/hank.jpg"
-import barbaraImage from "../assets/ambassadors/barbara.jpg"
-import martyImage from "../assets/ambassadors/marty.jpg"
-import duckyImage from "../assets/ambassadors/ducky.jpg"
-import bbImage from "../assets/ambassadors/bb.jpg"
-import toasterImage from "../assets/ambassadors/toaster.jpg"
-import tortelliniImage from "../assets/ambassadors/tortellini.jpg"
-import puppyImage from "../assets/ambassadors/puppy.jpg"
-import chipsAhoyImage from "../assets/ambassadors/chips-ahoy.jpg"
-import nillaWaferImage from "../assets/ambassadors/nilla-wafer.jpg"
-import momoImage from "../assets/ambassadors/momo.jpg"
-import appaImage from "../assets/ambassadors/appa.jpg"
+import stompyImage1 from "../assets/ambassadors/stompy/01.jpg"
+import georgieImage1 from "../assets/ambassadors/georgie/01.jpg"
+import ticoImage1 from "../assets/ambassadors/tico/01.jpg"
+import mileyImage1 from "../assets/ambassadors/miley/01.jpg"
+import miaImage1 from "../assets/ambassadors/mia/01.jpg"
+import sirenImage1 from "../assets/ambassadors/siren/01.jpg"
+import abbottImage1 from "../assets/ambassadors/abbott/01.png"
+import coconutImage1 from "../assets/ambassadors/coconut/01.png"
+import oliverImage1 from "../assets/ambassadors/oliver/01.jpg"
+import nuggetImage1 from "../assets/ambassadors/nugget/01.jpg"
+import henriettaImage1 from "../assets/ambassadors/henrietta/01.jpg"
+import henriqueImage1 from "../assets/ambassadors/henrique/01.jpg"
+import noodleImage1 from "../assets/ambassadors/noodle/01.jpg"
+import patchyImage1 from "../assets/ambassadors/patchy/01.png"
+import fennImage1 from "../assets/ambassadors/fenn/01.jpg"
+import reedImage1 from "../assets/ambassadors/reed/01.jpg"
+import serranoImage1 from "../assets/ambassadors/serrano/01.jpg"
+import jalapenoImage1 from "../assets/ambassadors/jalapeno/01.jpg"
+import snorkImage1 from "../assets/ambassadors/snork/01.jpg"
+import moominImage1 from "../assets/ambassadors/moomin/01.jpg"
+import winnieImage1 from "../assets/ambassadors/winnie/01.jpg"
+import hankImage1 from "../assets/ambassadors/hank/01.jpg"
+import cockroachesImage1 from "../assets/ambassadors/cockroaches/01.jpg"
+import martyImage1 from "../assets/ambassadors/marty/01.jpg"
+import duckyImage1 from "../assets/ambassadors/ducky/01.jpg"
+import bbImage1 from "../assets/ambassadors/bb/01.jpg"
+import toasterImage1 from "../assets/ambassadors/toaster/01.jpg"
+import tortelliniImage1 from "../assets/ambassadors/tortellini/01.jpg"
+import puppyImage1 from "../assets/ambassadors/puppy/01.jpg"
+import chipsImage1 from "../assets/ambassadors/chips/01.jpg"
+import nillaImage1 from "../assets/ambassadors/nilla/01.jpg"
+import momoImage1 from "../assets/ambassadors/momo/01.jpg"
+import appaImage1 from "../assets/ambassadors/appa/01.jpg"
 
 type OneToNine = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 type ZeroToNine = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
@@ -92,7 +92,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Stompy was hatched in captivity and Maya hand was raised to be the first Alveus ambassador.",
     mission: "Stopping exotic meat trade, traditional medicine, and over exploitation of animal products in cosmetics.",
     images: [
-      { src: stompyImage, alt: "Stompy the Emu" },
+      { src: stompyImage1, alt: "Stompy the Emu" },
     ],
     homepage: {
       title: "Stompy!",
@@ -110,7 +110,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Georgie was part of an educational program at a zoo and was rehomed to Alveus.",
     mission: "He is an ambassador for the wildlife trade and how chytrid fungus is affecting amphibian species worldwide.",
     images: [
-      { src: georgieImage, alt: "Georgie the African Bullfrog" },
+      { src: georgieImage1, alt: "Georgie the African Bullfrog" },
     ],
     homepage: {
       title: "Georgie!",
@@ -128,7 +128,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Tico was rehomed to Alveus as the previous sanctuary’s owner was retiring,",
     mission: "She is an ambassador for how the pet trade has affected her parrot species and many others around the world.",
     images: [
-      { src: ticoImage, alt: "Tico the Blue and Gold Macaw" },
+      { src: ticoImage1, alt: "Tico the Blue and Gold Macaw" },
     ],
     homepage: null,
   },
@@ -143,7 +143,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Miley was rehomed to Alveus as the previous sanctuary’s owner was retiring.",
     mission: "She is an ambassador for how the pet trade has affected her parrot species and many others around the world.",
     images: [
-      { src: mileyImage, alt: "Miley the Catalina Macaw" },
+      { src: mileyImage1, alt: "Miley the Catalina Macaw" },
     ],
     homepage: null,
   },
@@ -158,7 +158,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Mia was rehomed to Alveus as the previous sanctuary’s owner was retiring.",
     mission: "She is an ambassador for how the pet trade has affected her parrot species and many others around the world.",
     images: [
-      { src: miaImage, alt: "Mia the African Grey" },
+      { src: miaImage1, alt: "Mia the African Grey" },
     ],
     homepage: null,
   },
@@ -173,7 +173,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Siren was rehomed to Alveus as the previous sanctuary’s owner was retiring.",
     mission: "She is an ambassador for how the pet trade has affected her parrot species and many others around the world.",
     images: [
-      { src: sirenImage, alt: "Siren the Blue-fronted Amazon" },
+      { src: sirenImage1, alt: "Siren the Blue-fronted Amazon" },
     ],
     homepage: null,
   },
@@ -188,7 +188,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Abbott was brought into a wildlife rehab center as a baby. He imprinted during the rehab process and then was deemed non-releasable.",
     mission: "He is an ambassador for educating people on the misconceptions that wildlife face as well as human-wildlife conflict.",
     images: [
-      { src: abbottImage, alt: "Abbott the American Crow" },
+      { src: abbottImage1, alt: "Abbott the American Crow" },
     ],
     homepage: null,
   },
@@ -203,7 +203,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Coconut was brought into a wildlife rehab center as a baby. He imprinted during the rehab process and then was deemed non-releasable.",
     mission: "He is an ambassador for educating people on the misconceptions that wildlife face as well as human-wildlife conflict.",
     images: [
-      { src: coconutImage, alt: "Coconut the American Crow" },
+      { src: coconutImage1, alt: "Coconut the American Crow" },
     ],
     homepage: null,
   },
@@ -218,7 +218,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Oliver was put in the reject/return cage at a local feed store.",
     mission: "He is an ambassador for the agricultural industry and how people can use consumer choice to impact the environment in a positive way.",
     images: [
-      { src: oliverImage, alt: "Oliver the Olive Egger Chicken" },
+      { src: oliverImage1, alt: "Oliver the Olive Egger Chicken" },
     ],
     homepage: null,
   },
@@ -233,7 +233,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Nugget was rehomed to Alveus because she was bullied by hens in her previous flock.",
     mission: "She is an ambassador for the agricultural industry and how people can use consumer choice to impact the environment in a positive way.",
     images: [
-      { src: nuggetImage, alt: "Nugget the Ameraucana Chicken" },
+      { src: nuggetImage1, alt: "Nugget the Ameraucana Chicken" },
     ],
     homepage: null,
   },
@@ -248,7 +248,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Henrietta was rehomed from a local farm.",
     mission: "She is an ambassador for the agricultural industry and how people can use consumer choice to impact the environment in a positive way.",
     images: [
-      { src: henriettaImage, alt: "Henrietta the Jersey Giant Chicken" },
+      { src: henriettaImage1, alt: "Henrietta the Jersey Giant Chicken" },
     ],
     homepage: null,
   },
@@ -263,7 +263,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Henrique was rehomed from a local farm.",
     mission: "She is an ambassador for the agricultural industry and how people can use consumer choice to impact the environment in a positive way.",
     images: [
-      { src: henriqueImage, alt: "Henrique the Half Drak Brahma, Half Saipan Chicken" },
+      { src: henriqueImage1, alt: "Henrique the Half Drak Brahma, Half Saipan Chicken" },
     ],
     homepage: null,
   },
@@ -278,7 +278,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Noodle was part of an educational program at a zoo and was rehomed to Alveus.",
     mission: "She is an ambassador for how the pet trade and habitat loss has affected hers and many other reptile species worldwide.",
     images: [
-      { src: noodleImage, alt: "Noodle the Carpet Python" },
+      { src: noodleImage1, alt: "Noodle the Carpet Python" },
     ],
     homepage: null,
   },
@@ -293,7 +293,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Patchy was rehomed to Alveus from a local breeder after it was discovered that he had a genetic defect and was missing an eye.",
     mission: "He is an ambassador for how the pet trade and habitat loss has affected his and many other reptile species worldwide.",
     images: [
-      { src: patchyImage, alt: "Patchy the Ball Python" },
+      { src: patchyImage1, alt: "Patchy the Ball Python" },
     ],
     homepage: null,
   },
@@ -308,7 +308,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Fenn was confiscated from the illegal pet trade by California Department of Fish and Wildlife. He was then rehomed to Alveus.",
     mission: "He is an ambassador for the exploitation of wildlife in the pet trade and the fur trade.",
     images: [
-      { src: fennImage, alt: "Fenn the European Red Fox" },
+      { src: fennImage1, alt: "Fenn the European Red Fox" },
     ],
     homepage: null,
   },
@@ -323,7 +323,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Reed was orphaned as a wild kit and was rescued to be raised at a zoo to be an educational ambassador. He was then rehomed to Alveus.",
     mission: "He is an ambassador for the exploitation of wildlife in the pet trade and the fur trade.",
     images: [
-      { src: reedImage, alt: "Reed the European Red Fox" },
+      { src: reedImage1, alt: "Reed the European Red Fox" },
     ],
     homepage: null,
   },
@@ -338,7 +338,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Rehomed to Alveus from a local equine rescue.",
     mission: "Serrano is an ambassador for the wildlife trade and use of wild animals in traditional medicine.",
     images: [
-      { src: serranoImage, alt: "Serrano the Domestic Donkey" },
+      { src: serranoImage1, alt: "Serrano the Domestic Donkey" },
     ],
     homepage: null,
   },
@@ -353,7 +353,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Rehomed to Alveus from a local equine rescue.",
     mission: "Jalapeño is an ambassador for the wildlife trade and use of wild animals in traditional medicine.",
     images: [
-      { src: jalapenoImage, alt: "Jalapeño the Domestic Donkey" },
+      { src: jalapenoImage1, alt: "Jalapeño the Domestic Donkey" },
     ],
     homepage: null,
   },
@@ -368,7 +368,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Snork was part of an educational program at a zoo and was rehomed to Alveus.",
     mission: "She is an ambassador for the exploitation of wildlife in the fur trade.",
     images: [
-      { src: snorkImage, alt: "Snork the Chinchilla" },
+      { src: snorkImage1, alt: "Snork the Chinchilla" },
     ],
     homepage: null,
   },
@@ -383,7 +383,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Rehomed from a local pet owner.",
     mission: "He is an ambassador for the exploitation of wildlife in the fur trade.",
     images: [
-      { src: moominImage, alt: "Moomin the Chinchilla" },
+      { src: moominImage1, alt: "Moomin the Chinchilla" },
     ],
     homepage: {
       title: "Moomin is Movin' In!",
@@ -401,7 +401,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Winnie came from a cattle operation in Oklahoma.",
     mission: "She is an ambassador for the beef industry and how people can use their consumer choice to impact the environment in a positive way.",
     images: [
-      { src: winnieImage, alt: "Winnie the Red Angus Beef Cow" },
+      { src: winnieImage1, alt: "Winnie the Red Angus Beef Cow" },
     ],
     homepage: null,
   },
@@ -416,7 +416,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Our smokey ghost millipede was born in captivity and ethically sourced.",
     mission: "He is an ambassador for the importance of invertebrates and the misconceptions they face.",
     images: [
-      { src: hankImage, alt: "Hank the Smokey Ghost Millipede" },
+      { src: hankImage1, alt: "Hank the Smokey Ghost Millipede" },
     ],
     homepage: null,
   },
@@ -431,7 +431,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Our cockroach colony was started with 7 roaches that were part of an educational colony at a school in Pennsylvania.",
     mission: "They are ambassadors for the importance of invertebrates and the misconceptions they face.",
     images: [
-      { src: barbaraImage, alt: "Barbara the Madagascar Hissing Cockroach" },
+      { src: cockroachesImage1, alt: "Barbara the Madagascar Hissing Cockroach" },
     ],
     homepage: null,
   },
@@ -446,7 +446,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Our zebra isopods were born in captivity and ethically sourced.",
     mission: "They are ambassadors for the importance of invertebrates and the misconceptions they face.",
     images: [
-      { src: martyImage, alt: "Marty the Zebra Isopods" },
+      { src: martyImage1, alt: "Marty the Zebra Isopods" },
     ],
     homepage: null,
   },
@@ -461,7 +461,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Our rubber ducky isopods were born in captivity and ethically sourced.",
     mission: "They are ambassadors for the importance of invertebrates and the misconceptions they face.",
     images: [
-      { src: duckyImage, alt: "Ducky the Rubber Ducky Isopods" },
+      { src: duckyImage1, alt: "Ducky the Rubber Ducky Isopods" },
     ],
     homepage: null,
   },
@@ -476,7 +476,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Our powder orange isopods were born in captivity and ethically sourced.",
     mission: "They are ambassadors for the importance of invertebrates and the misconceptions they face.",
     images: [
-      { src: bbImage, alt: "BB the Spanish Orange Isopods" },
+      { src: bbImage1, alt: "BB the Spanish Orange Isopods" },
     ],
     homepage: null,
   },
@@ -491,7 +491,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Toaster Strudel was part of an educational program at a zoo and was rehomed to Alveus.",
     mission: "He is an ambassador for how human development and invasive species can affect natural habitats and native species.",
     images: [
-      { src: toasterImage, alt: "Toaster Strudel the Blue-tounged Skink" },
+      { src: toasterImage1, alt: "Toaster Strudel the Blue-tounged Skink" },
     ],
     homepage: null,
   },
@@ -506,7 +506,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Our emperor scorpions were born in captivity and ethically sourced.",
     mission: "He is an ambassador for the importance of invertebrates and the misconceptions they face.",
     images: [
-      { src: tortelliniImage, alt: "Tortellini the Emperor Scorpion" },
+      { src: tortelliniImage1, alt: "Tortellini the Emperor Scorpion" },
     ],
     homepage: null,
   },
@@ -521,7 +521,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Our emperor scorpions were born in captivity and ethically sourced.",
     mission: "He is an ambassador for the importance of invertebrates and the misconceptions they face.",
     images: [
-      { src: puppyImage, alt: "Puppy the Emperor Scorpion" },
+      { src: puppyImage1, alt: "Puppy the Emperor Scorpion" },
     ],
     homepage: null,
   },
@@ -536,7 +536,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Chips Ahoy was bred as a feeder rat for snakes, but she will now live out her full life at Alveus.",
     mission: "She is an ambassadors for how rodenticide use and outdoor cats affect all levels of wildlife.",
     images: [
-      { src: chipsAhoyImage, alt: "Chips Ahoy the Rat" },
+      { src: chipsImage1, alt: "Chips Ahoy the Rat" },
     ],
     homepage: null,
   },
@@ -551,7 +551,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Nilla Wafer was bred as a feeder rat for snakes, but she will now live out her full life at Alveus.",
     mission: "She is an ambassadors for how rodenticide use and outdoor cats affect all levels of wildlife.",
     images: [
-      { src: nillaWaferImage, alt: "Nilla Wafer the Rat" },
+      { src: nillaImage1, alt: "Nilla Wafer the Rat" },
     ],
     homepage: null,
   },
@@ -566,7 +566,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Momo was rescued by Alveus after being surrendered to a veterinarian due to neglect.",
     mission: "He is an ambassador for the exotic pet trade and how it affects primate species worldwide.",
     images: [
-      { src: momoImage, alt: "Momo the Black Tufted Marmoset" },
+      { src: momoImage1, alt: "Momo the Black Tufted Marmoset" },
     ],
     homepage: null,
   },
@@ -581,7 +581,7 @@ const ambassadors: Record<string, Ambassador> = {
     story: "Appa was rescued by Alveus after being surrendered to a veterinarian due to neglect.",
     mission: "He is an ambassador for the exotic pet trade and how it affects primate species worldwide.",
     images: [
-      { src: appaImage, alt: "Appa the Common Marmoset" },
+      { src: appaImage1, alt: "Appa the Common Marmoset" },
     ],
     homepage: null,
   },
