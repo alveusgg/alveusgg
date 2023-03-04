d1ac4bf02c883c9e0459820af65e979457e7e03e
Paul Golmann <mail@pje-web.de>
2023-03-04 16:45:36 +0100

    feat(scheduler): use vercel cron job to run

---
+++ apps/website/src/pages/api/cron.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { runScheduledTasks } from "@/server/actions/scheduler";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    await runScheduledTasks();
+    res.status(200).json({ success: true });
+  } catch (err) {
+    res.status(500).json({
+      statusCode: 500,
+      message: err instanceof Error ? err.message : "unknown error",
+    });
+  }
+}
