6028885fb5bf4b2b5a61e4f7f322f63633469823
Paul Golmann <mail@pje-web.de>
2023-03-04 01:28:58 +0100

    feat: render date/times in en-US format on server

--- apps/website/src/components/shared/LocalDate.tsx
+++ apps/website/src/components/shared/LocalDate.tsx
@@ -1,19 +1,24 @@
 import React, { useEffect, useRef } from "react";
 
-const dtf = new Intl.DateTimeFormat(undefined, {
+const defaultLocale = "en-US";
+const dtfOptions = {
   day: "2-digit",
   month: "2-digit",
   year: "2-digit",
-});
+} satisfies Intl.DateTimeFormatOptions;
+const isomorphicDtf = new Intl.DateTimeFormat(defaultLocale, dtfOptions);
+const clientDtf = new Intl.DateTimeFormat(undefined, dtfOptions);
 
 export const LocalDate: React.FC<{ dateTime: Date }> = ({ dateTime }) => {
-  const localTimeRef = useRef<HTMLDivElement>(null);
+  const localDateRef = useRef<HTMLDivElement>(null);
+
+  const initialValue = isomorphicDtf.format(dateTime);
 
   useEffect(() => {
-    if (localTimeRef.current) {
-      localTimeRef.current.textContent = dtf.format(dateTime);
+    if (localDateRef.current) {
+      localDateRef.current.textContent = clientDtf.format(dateTime);
     }
   }, [dateTime]);
 
-  return <span ref={localTimeRef}></span>;
+  return <span ref={localDateRef}>{initialValue}</span>;
 };
--- apps/website/src/components/shared/LocalTime.tsx
+++ apps/website/src/components/shared/LocalTime.tsx
@@ -1,21 +1,25 @@
 import React, { useEffect, useRef } from "react";
 
-const dtf = new Intl.DateTimeFormat(undefined, {
+const defaultLocale = "en-US";
+const dtfOptions = {
   day: "2-digit",
   month: "2-digit",
   year: "2-digit",
   hour: "2-digit",
   minute: "2-digit",
-});
+} satisfies Intl.DateTimeFormatOptions;
+const isomorphicDtf = new Intl.DateTimeFormat(defaultLocale, dtfOptions);
+const clientDtf = new Intl.DateTimeFormat("de", dtfOptions);
 
 export const LocalTime: React.FC<{ dateTime: Date }> = ({ dateTime }) => {
   const localTimeRef = useRef<HTMLDivElement>(null);
+  const initialValue = isomorphicDtf.format(dateTime);
 
   useEffect(() => {
     if (localTimeRef.current) {
-      localTimeRef.current.textContent = dtf.format(dateTime);
+      localTimeRef.current.textContent = clientDtf.format(dateTime);
     }
   }, [dateTime]);
 
-  return <span ref={localTimeRef}></span>;
+  return <span ref={localTimeRef}>{initialValue}</span>;
 };
