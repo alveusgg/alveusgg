ab085c0c914f9e5f6997373bc8be7e4e2e7a6fb0
Paul Golmann <mail@pje-web.de>
2023-03-11 02:43:35 +0100

    fix(utils/attrs): handle referrerpolicy attr

--- apps/website/src/utils/attrs.ts
+++ apps/website/src/utils/attrs.ts
@@ -2,7 +2,15 @@ import reactAttrConverter from "react-attr-converter";
 
 export type HTMLAttributes = Record<string, string>;
 
+const convertAttributeKey = (key: string) => {
+  if (key === "referrerpolicy") {
+    return "referrerPolicy";
+  }
+
+  return reactAttrConverter(key);
+};
+
 export const htmlToReact = (obj: HTMLAttributes) =>
   Object.fromEntries(
-    Object.entries(obj).map(([key, value]) => [reactAttrConverter(key), value])
+    Object.entries(obj).map(([key, value]) => [convertAttributeKey(key), value])
   );
