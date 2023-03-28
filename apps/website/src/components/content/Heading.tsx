01cb38f58b871f27428da191e1dabe4c490e2478
MattIPv4 <me@mattcowley.co.uk>
2023-03-28 18:51:37 +0100

    Fix title causing overflow

--- apps/website/src/components/content/Heading.tsx
+++ apps/website/src/components/content/Heading.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from "react";
 
-type HeadingProps = {
+export type HeadingProps = {
   children?: React.ReactNode;
   level?: 1 | 2 | 3 | 4 | 5 | 6;
   className?: string;
--- apps/website/src/components/content/Markdown.tsx
+++ apps/website/src/components/content/Markdown.tsx
@@ -3,7 +3,22 @@ import Link from "next/link";
 import ReactMarkdown, { type Components, type Options } from "react-markdown";
 import headingId from "remark-heading-id";
 
-import Heading from "@/components/content/Heading";
+import Heading, { type HeadingProps } from "@/components/content/Heading";
+
+const MarkdownHeading: React.FC<HeadingProps> = ({
+  level,
+  className,
+  children,
+  id,
+}) => (
+  <Heading
+    level={level}
+    className={[className, "break-words"].filter(Boolean).join(" ")}
+    id={id}
+  >
+    {id ? <Link href={`#${id}`}>{children}</Link> : children}
+  </Heading>
+);
 
 type MarkdownProps = {
   content: string;
@@ -13,24 +28,34 @@ const Markdown: React.FC<MarkdownProps> = ({ content }) => {
   const components: Components = useMemo(
     () => ({
       h1: ({ children, id }) => (
-        <Heading level={1} className="text-5xl" id={id}>
-          {id ? <Link href={`#${id}`}>{children}</Link> : children}
-        </Heading>
+        <MarkdownHeading level={1} className="text-5xl" id={id}>
+          {children}
+        </MarkdownHeading>
       ),
       h2: ({ children, id }) => (
-        <Heading level={2} className="mt-8" id={id}>
-          {id ? <Link href={`#${id}`}>{children}</Link> : children}
-        </Heading>
+        <MarkdownHeading level={2} className="mt-8" id={id}>
+          {children}
+        </MarkdownHeading>
       ),
       h3: ({ children, id }) => (
-        <Heading level={3} className="mt-4 text-2xl" id={id}>
-          {id ? <Link href={`#${id}`}>{children}</Link> : children}
-        </Heading>
+        <MarkdownHeading level={3} className="mt-4 text-2xl" id={id}>
+          {children}
+        </MarkdownHeading>
       ),
       h4: ({ children, id }) => (
-        <Heading level={4} className="text-xl" id={id}>
-          {id ? <Link href={`#${id}`}>{children}</Link> : children}
-        </Heading>
+        <MarkdownHeading level={4} className="text-xl" id={id}>
+          {children}
+        </MarkdownHeading>
+      ),
+      h5: ({ children, id }) => (
+        <MarkdownHeading level={5} className="text-lg" id={id}>
+          {children}
+        </MarkdownHeading>
+      ),
+      h6: ({ children, id }) => (
+        <MarkdownHeading level={6} className="text-base" id={id}>
+          {children}
+        </MarkdownHeading>
       ),
       ul: ({ children }) => <ul className="my-2 ml-8 list-disc">{children}</ul>,
       a: ({ children, href }) => {
