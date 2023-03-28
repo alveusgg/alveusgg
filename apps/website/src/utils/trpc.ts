ec429ac0013d63f5007c2c7db5eebc91d0fb75e7
Paul Golmann <mail@pje-web.de>
2023-03-29 01:43:42 +0200

    chore: simplify trpc base url (use NEXT_PUBLIC_BASE_URL)

--- apps/website/src/utils/trpc.ts
+++ apps/website/src/utils/trpc.ts
@@ -3,12 +3,10 @@ import { createTRPCNext } from "@trpc/next";
 import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 import superjson from "superjson";
 
-import { type AppRouter } from "../server/trpc/router/_app";
+import { type AppRouter } from "@/server/trpc/router/_app";
 
 const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+  return typeof window === "undefined" ? process.env.NEXT_PUBLIC_BASE_URL : ""; // browser should use relative url
 };
 
 export const trpc = createTRPCNext<AppRouter>({
