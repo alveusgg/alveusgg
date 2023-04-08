a5302241626ca766e5f2d593ad5589c5d35390c8
MattIPv4 <me@mattcowley.co.uk>
2023-04-08 03:33:53 +0100

    Add EIN to footer

--- apps/website/src/components/layout/footer/Footer.tsx
+++ apps/website/src/components/layout/footer/Footer.tsx
@@ -2,46 +2,12 @@ import React, { useMemo } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-import IconGitHub from "@/icons/IconGitHub";
-
 import Socials from "./Socials";
+
+import IconGitHub from "@/icons/IconGitHub";
 
 const footerLinkClasses =
   "underline decoration-gray-600 underline-offset-2 transition-colors hover:text-gray-300";
-
-const credits =
-  /* prettier-ignore */ <>
-  Original design by
-  {" "}
-  <Link
-    className={footerLinkClasses}
-    href="https://chanelrooh.com"
-    target="_blank"
-  >
-    Chanelrooh
-  </Link>
-  {", "}
-  <br/>
-  built by the
-  {" "}
-  <Link
-    className={footerLinkClasses}
-    href="https://github.com/alveusgg"
-    rel="noreferrer"
-    target="_blank"
-  >
-    <IconGitHub
-      size={16}
-      className="mr-1 inline-block"
-    />
-    Alveus.gg team
-  </Link>
-  {" "}
-  and community
-  {", "}
-  <br/>
-  supported by the Alveus team.
-</>;
 
 export const Footer: React.FC = () => {
   const router = useRouter();
@@ -54,11 +20,36 @@ export const Footer: React.FC = () => {
     <>
       {!isAdmin && <Socials />}
 
-      <footer className="bg-gray-800 py-4 px-2 text-gray-400 md:py-2 md:px-0">
+      <footer className="bg-gray-800 px-2 py-4 text-gray-400 md:px-0 md:py-2">
         <div className="container mx-auto">
           <ul className="flex flex-wrap items-center justify-between">
             <li className="basis-full p-2 md:basis-1/3">
-              <p>{credits}</p>
+              <p>
+                Original design by{" "}
+                <Link
+                  className={footerLinkClasses}
+                  href="https://chanelrooh.com"
+                  target="_blank"
+                >
+                  Chanelrooh
+                </Link>
+                {", "}
+                <br />
+                built by the{" "}
+                <Link
+                  className={footerLinkClasses}
+                  href="https://github.com/alveusgg"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <IconGitHub size={16} className="mr-1 inline-block" />
+                  Alveus.gg team
+                </Link>{" "}
+                and community
+                {", "}
+                <br />
+                supported by the Alveus team.
+              </p>
             </li>
             <li className="basis-full p-2 md:basis-1/3 md:text-center">
               <ul className="flex flex-col gap-1 md:items-center">
@@ -78,6 +69,7 @@ export const Footer: React.FC = () => {
               <p>
                 Copyright &copy; {new Date().getFullYear()} Alveus Sanctuary.
                 <br /> All rights reserved.
+                <br /> EIN: 86-1772907.
               </p>
             </li>
           </ul>
