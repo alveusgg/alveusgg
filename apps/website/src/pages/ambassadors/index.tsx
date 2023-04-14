0deed394f8687d0f22a3a4b75cbb7bdf64596a89
MattIPv4 <me@mattcowley.co.uk>
2023-04-14 22:29:12 +0100

    All the ambassadors page to be grouped by enclosure

--- apps/website/src/pages/ambassadors/index.tsx
+++ apps/website/src/pages/ambassadors/index.tsx
@@ -1,27 +1,43 @@
 import { type NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useMemo } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 
 import ambassadors, {
   type AmbassadorKey,
 } from "@alveusgg/data/src/ambassadors/core";
 import { getAmbassadorImages } from "@alveusgg/data/src/ambassadors/images";
+import enclosures, {
+  isEnclosureKey,
+  type EnclosureKey,
+} from "@alveusgg/data/src/enclosures";
 
 import Section from "@/components/content/Section";
 import Heading from "@/components/content/Heading";
 import Meta from "@/components/content/Meta";
-import { camelToKebab } from "@/utils/string-case";
-import { typeSafeObjectKeys } from "@/utils/helpers";
+import { camelToKebab, kebabToCamel } from "@/utils/string-case";
+import { typeSafeObjectEntries, typeSafeObjectKeys } from "@/utils/helpers";
 
 import leafRightImage1 from "@/assets/floral/leaf-right-1.png";
 import leafLeftImage1 from "@/assets/floral/leaf-left-1.png";
 import leafRightImage2 from "@/assets/floral/leaf-right-2.png";
 import leafLeftImage2 from "@/assets/floral/leaf-left-2.png";
 
-const AmbassadorItem: React.FC<{ ambassador: AmbassadorKey }> = ({
-  ambassador,
-}) => {
+type AmbassadorsByEnclosure = Partial<Record<EnclosureKey, AmbassadorKey[]>>;
+const ambassadorsByEnclosure = Object.entries(
+  ambassadors
+).reduce<AmbassadorsByEnclosure>(
+  (acc, [key, val]) => ({
+    ...acc,
+    [val.enclosure]: [...(acc[val.enclosure] || []), key],
+  }),
+  {}
+);
+
+const AmbassadorItem: React.FC<{
+  ambassador: AmbassadorKey;
+  level?: React.ComponentProps<typeof Heading>["level"];
+}> = ({ ambassador, level = 2 }) => {
   const data = useMemo(() => ambassadors[ambassador], [ambassador]);
   const images = useMemo(() => getAmbassadorImages(ambassador), [ambassador]);
 
@@ -36,7 +52,7 @@ const AmbassadorItem: React.FC<{ ambassador: AmbassadorKey }> = ({
           className="aspect-4/3 h-auto w-full rounded-xl object-cover transition-filter group-hover:brightness-105 group-hover:contrast-115 group-hover:saturate-110"
         />
         <Heading
-          level={2}
+          level={level}
           className="mb-0 mt-2 text-center transition-colors group-hover:text-alveus-green-700"
         >
           {data.name}
@@ -49,7 +65,105 @@ const AmbassadorItem: React.FC<{ ambassador: AmbassadorKey }> = ({
   );
 };
 
+const AmbassadorItems: React.FC<{
+  ambassadors: AmbassadorKey[];
+  className?: string;
+  level?: React.ComponentProps<typeof Heading>["level"];
+}> = ({ ambassadors, className, level }) => (
+  <div className={["flex flex-wrap", className].filter(Boolean).join(" ")}>
+    {ambassadors.map((key) => (
+      <AmbassadorItem key={key} ambassador={key} level={level} />
+    ))}
+  </div>
+);
+
+const AmbassadorEnclosure: React.FC<{
+  enclosure: EnclosureKey;
+  active?: boolean;
+}> = ({ enclosure, active = false }) => {
+  const data = useMemo(() => enclosures[enclosure], [enclosure]);
+  const ambassadors = useMemo(
+    () => ambassadorsByEnclosure[enclosure] || [],
+    [enclosure]
+  );
+
+  // If this enclosure is the "active" one in the URL, scroll it into view
+  const scroll = useCallback(
+    (node: HTMLDivElement | null) => {
+      if (node && active) node.scrollIntoView({ behavior: "smooth" });
+    },
+    [active]
+  );
+
+  return (
+    <div ref={scroll}>
+      <Heading
+        level={2}
+        className="mb-4"
+        id={`enclosures:${camelToKebab(enclosure)}`}
+        link
+      >
+        {data.name}
+      </Heading>
+      <AmbassadorItems ambassadors={ambassadors} className="ml-8" level={3} />
+    </div>
+  );
+};
+
+const tabs = {
+  all: "All Ambassadors",
+  enclosures: "Enclosures",
+} as const;
+
+type TabKey = keyof typeof tabs;
+
+const isTabKey = (key: string): key is TabKey =>
+  Object.keys(tabs).includes(key);
+
 const AmbassadorsPage: NextPage = () => {
+  const [checked, setChecked] = useState(false);
+  const [tab, setTab] = useState<TabKey>("all");
+  const [active, setActive] = useState<string | null>(null);
+
+  // Check the hash to see if we should be on the enclosures tab
+  const checkHash = useCallback(() => {
+    const match = window.location.hash.match(/^#([^:]+)(?::(.+))?$/);
+    if (match && match[1] && isTabKey(match[1])) {
+      setTab(match[1]);
+      setActive(
+        match[1] === "enclosures" &&
+          match[2] &&
+          isEnclosureKey(kebabToCamel(match[2]))
+          ? kebabToCamel(match[2])
+          : null
+      );
+    } else {
+      setTab("all");
+      setActive(null);
+    }
+    setChecked(true);
+  }, []);
+  useEffect(() => {
+    checkHash();
+    window.addEventListener("hashchange", checkHash);
+    return () => window.removeEventListener("hashchange", checkHash);
+  }, [checkHash]);
+
+  // When the tab changes, update the hash
+  useEffect(() => {
+    if (!checked) return;
+
+    const current = window.location.toString();
+    const url = new URL(current);
+
+    if (tab === "all") url.hash = "";
+    else if (active) url.hash = `${tab}:${active}`;
+    else url.hash = tab;
+
+    const updated = url.toString();
+    if (current !== updated) window.history.pushState({}, "", updated);
+  }, [tab]);
+
   return (
     <>
       <Meta
@@ -98,14 +212,52 @@ const AmbassadorsPage: NextPage = () => {
           className="pointer-events-none absolute -bottom-24 left-0 z-10 hidden h-auto w-1/2 max-w-[12rem] select-none lg:block"
         />
 
-        <Section className="flex-grow pt-0" containerClassName="flex flex-wrap">
-          <p className="mb-4 mt-8 basis-full text-center text-xl font-semibold">
-            Click each ambassador for information and highlights!
-          </p>
+        <Section className="flex-grow pt-0">
+          <div className="my-4 flex flex-col items-center gap-4">
+            <div className="border-b border-alveus-green text-center text-xl font-semibold">
+              <ul className="-mb-px flex flex-wrap">
+                {typeSafeObjectEntries(tabs).map(([key, val]) => (
+                  <li key={key} className="mx-4">
+                    <button
+                      type="button"
+                      onClick={() => {
+                        setTab(key);
+                        setActive(null);
+                      }}
+                      className={[
+                        "inline-block rounded-t-lg border-b-2 p-4 hover:border-alveus-green-500 hover:text-alveus-green-500",
+                        tab === key
+                          ? "border-alveus-green-700 text-alveus-green-700"
+                          : "border-transparent",
+                      ]
+                        .filter(Boolean)
+                        .join(" ")}
+                      aria-current={tab === key ? "page" : undefined}
+                    >
+                      {val}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
 
-          {typeSafeObjectKeys(ambassadors).map((key) => (
-            <AmbassadorItem key={key} ambassador={key} />
-          ))}
+            <p className="text-center text-xl font-semibold">
+              Click each ambassador for information and highlights!
+            </p>
+          </div>
+
+          {tab === "all" && (
+            <AmbassadorItems ambassadors={typeSafeObjectKeys(ambassadors)} />
+          )}
+
+          {tab === "enclosures" &&
+            typeSafeObjectKeys(ambassadorsByEnclosure).map((key) => (
+              <AmbassadorEnclosure
+                key={key}
+                enclosure={key}
+                active={key === active}
+              />
+            ))}
         </Section>
       </div>
     </>
