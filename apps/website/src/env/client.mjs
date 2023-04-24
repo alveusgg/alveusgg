9ee5af8a994d88cf9757ee188d41575a45f0359f
Paul Golmann <mail@pje-web.de>
2023-04-24 23:40:20 +0200

    chore(env): refactor formatErrors into separate file

--- apps/website/src/env/client.mjs
+++ apps/website/src/env/client.mjs
@@ -1,19 +1,8 @@
 // @ts-check
+import { formatErrors } from "./formatErrors.mjs";
 import { clientEnv, clientSchema } from "./schema.mjs";
 
 const _clientEnv = clientSchema.safeParse(clientEnv);
-
-/** @returns Array<string> */
-export const formatErrors = (
-  /** @type {import('zod').ZodFormattedError<Map<string,string>,string>} */
-  errors
-) =>
-  Object.entries(errors)
-    .map(([name, value]) => {
-      if (value && "_errors" in value)
-        return `${name}: ${value._errors.join(", ")}\n`;
-    })
-    .filter(Boolean);
 
 if (!_clientEnv.success) {
   console.error(
---
+++ apps/website/src/env/formatErrors.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+
+/** @returns Array<string> */
+export const formatErrors = (
+  /** @type {import('zod').ZodFormattedError<Map<string,string>,string>} */
+  errors
+) =>
+  Object.entries(errors)
+    .map(([name, value]) => {
+      if (value && "_errors" in value)
+        return `${name}: ${value._errors.join(", ")}\n`;
+    })
+    .filter(Boolean);
