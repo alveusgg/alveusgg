a19f7e1041c53273daf5b8e32d8c2aeb3e17cb3e
MattIPv4 <me@mattcowley.co.uk>
2023-06-03 01:00:18 +0100

    Add next lint to lint-staged for website only

---
+++ apps/website/lint-staged.config.cjs
@@ -0,0 +1,9 @@
+const mainConfig = require("../../lint-staged.config.js");
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix ${filenames.map((file) => `--file ${file}`).join(" ")}`;
+
+module.exports = {
+  ...mainConfig,
+  "*.{js,jsx,ts,tsx}": buildEslintCommand,
+};
