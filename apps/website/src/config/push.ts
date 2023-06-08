879579a8172668cac6dcc02ad2a4363c03fe7abc
Paul Golmann <mail@pje-web.de>
2023-06-09 00:21:18 +0200

    fix: fix push notification language code (has to be two letter)

--- apps/website/src/config/push.ts
+++ apps/website/src/config/push.ts
@@ -1,7 +1,7 @@
 import { env } from "@/env/server.mjs";
 
-export const pushLang = env.PUSH_LANG || "en-US";
-export const pushTextDir = env.PUSH_TEXT_DIR || "ltr";
+export const pushLang = env.PUSH_LANG || "en"; // two letter language code
+export const pushTextDir = env.PUSH_TEXT_DIR || "ltr"; // "ltr" or "rtl"
 
 export const pushBatchSize = env.PUSH_BATCH_SIZE || 10;
 export const pushMaxAttempts = env.PUSH_MAX_ATTEMPTS || 5;
