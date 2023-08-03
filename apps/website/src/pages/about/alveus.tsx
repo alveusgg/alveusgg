8883e89bc8a7b279535e0ea0d58a09800444b15f
MattIPv4 <me@mattcowley.co.uk>
2023-08-03 01:13:44 +0100

    Clean up old sections on about page

--- apps/website/src/pages/about/alveus.tsx
+++ apps/website/src/pages/about/alveus.tsx
@@ -6,16 +6,10 @@ import Section from "@/components/content/Section";
 import Heading from "@/components/content/Heading";
 import Meta from "@/components/content/Meta";
 import Link from "@/components/content/Link";
-import { Lightbox } from "@/components/content/YouTube";
-import TransparencySealSection from "@/components/content/TransparencySealSection";
+import { Lightbox, Preview } from "@/components/content/YouTube";
 
-import IconTwitch from "@/icons/IconTwitch";
-
-import mayaImage from "@/assets/maya.png";
-import leafRightImage1 from "@/assets/floral/leaf-right-1.png";
-import leafRightImage2 from "@/assets/floral/leaf-right-2.png";
-import leafLeftImage1 from "@/assets/floral/leaf-left-1.png";
 import leafLeftImage2 from "@/assets/floral/leaf-left-2.png";
+import imageGuidestarSeal from "@/assets/guidestar-candid-gold-seal.svg";
 
 const stats = {
   averageTime: {
@@ -51,19 +45,6 @@ const stats = {
   },
 };
 
-const views = {
-  us: { name: "United States", value: 27 },
-  de: { name: "Germany", value: 7 },
-  kr: { name: "South Korea", value: 6 },
-  ru: { name: "Russia", value: 6 },
-  fr: { name: "France", value: 4 },
-  ca: { name: "Canada", value: 4 },
-  br: { name: "Brazil", value: 4 },
-  gb: { name: "United Kingdom", value: 3 },
-  tw: { name: "Taiwan", value: 3 },
-  se: { name: "Sweden", value: 2 },
-};
-
 const AboutAlveusPage: NextPage = () => {
   return (
     <>
@@ -75,72 +56,48 @@ const AboutAlveusPage: NextPage = () => {
       {/* Nav background */}
       <div className="-mt-40 hidden h-40 bg-alveus-green-900 lg:block" />
 
-      <div className="relative">
-        <Image
-          src={leafRightImage1}
-          alt=""
-          className="pointer-events-none absolute -top-8 right-0 z-10 hidden h-auto w-1/2 max-w-sm select-none lg:block"
-        />
+      <Section
+        dark
+        className="py-8"
+        containerClassName="flex flex-wrap items-center justify-between"
+      >
+        <div className="flex w-full flex-col gap-4 pb-16 pt-4 md:w-2/5 md:py-24">
+          <Heading className="my-0">Alveus Sanctuary</Heading>
+          <p className="text-lg">
+            Alveus is a non profit organization founded by Maya Higa that
+            functions as an exotic animal sanctuary and as a virtual education
+            center facility to provide permanent homes to non-releasable exotic
+            animals.
+          </p>
+          <p className="text-lg">
+            Watch the video to learn more about why Maya founded Alveus, and the
+            mission of the organization.
+          </p>
+        </div>
 
-        <Section dark className="py-24">
-          <div className="w-full lg:w-3/5">
-            <Heading level={1}>About Alveus Sanctuary</Heading>
-            <p className="text-lg">
-              Alveus is a non profit organization founded by Maya Higa that
-              functions as an exotic animal sanctuary and as a virtual education
-              center facility to provide permanent homes to non-releasable
-              exotic animals.
-            </p>
-          </div>
-        </Section>
-      </div>
+        <div className="w-full max-w-2xl p-4 pt-8 md:mx-0 md:w-3/5 md:pt-4">
+          <Lightbox>
+            {({ Trigger }) => (
+              <Trigger videoId="jXTqWIc--jo">
+                <Preview videoId="jXTqWIc--jo" />
+                <Heading level={2} className="text-center">
+                  Announcing Alveus
+                </Heading>
+              </Trigger>
+            )}
+          </Lightbox>
+        </div>
+      </Section>
 
-      <div className="relative">
-        <Image
-          src={leafLeftImage1}
-          alt=""
-          className="pointer-events-none absolute -bottom-32 left-0 z-10 hidden h-auto w-1/2 max-w-[10rem] select-none lg:block 2xl:-bottom-48 2xl:max-w-[12rem]"
-        />
-
-        <Section>
-          <div className="flex flex-wrap-reverse items-center">
-            <div className="basis-full pt-8 md:basis-1/2 md:pr-8 md:pt-0">
-              <Image
-                src={mayaImage}
-                alt="Maya Higa, holding an owl in one photo, and a falcon in the second photo"
-                className="ml-auto h-auto w-full max-w-lg"
-              />
-            </div>
-
-            <div className="basis-full md:basis-1/2 md:px-4">
-              <Heading level={2}>Watch the Launch Video</Heading>
-              <p className="my-2 font-serif text-lg font-bold italic">
-                The Start of it all!
-              </p>
-              <p className="my-4 text-lg">
-                Find out more about Alveus and our aims here.
-              </p>
-              <Lightbox>
-                {({ Trigger }) => (
-                  <Trigger
-                    videoId="jXTqWIc--jo"
-                    className="inline-block rounded-full border-2 border-alveus-green px-6 py-4 text-xl transition-colors hover:bg-alveus-green hover:text-alveus-tan"
-                  >
-                    Watch the Video
-                  </Trigger>
-                )}
-              </Lightbox>
-            </div>
-          </div>
-        </Section>
-      </div>
-
-      <Section dark className="text-center">
-        <Heading level={2}>Why Twitch.tv</Heading>
-        <p>
+      <Section className="text-center">
+        <Heading id="twitch" level={2}>
+          Why Twitch.tv
+        </Heading>
+        <p className="mx-auto max-w-2xl">
           Twitch offers our guests the opportunity to connect with viewers from
-          around the globe. Here are some statistics that represent Twitch and
-          it&apos;s users:
+          around the globe, and allows us to target a demographic that is
+          otherwise far less likely to be exposed to conservation education. We
+          see Twitch as an untapped reservoir for doing good.
         </p>
 
         <ul className="mb-2 mt-6 flex flex-wrap justify-center md:mt-12">
@@ -148,7 +105,7 @@ const AboutAlveusPage: NextPage = () => {
             <li key={key} className="basis-full py-4 md:basis-1/3 md:px-4">
               <Link
                 href={stat.source}
-                className="flex h-full flex-col justify-center rounded-2xl bg-alveus-green-900 px-6 py-6 shadow-lg transition-shadow hover:shadow-xl"
+                className="flex h-full flex-col justify-center rounded-2xl bg-alveus-green px-6 py-6 text-alveus-tan shadow-lg transition-shadow hover:shadow-xl"
                 external
                 custom
               >
@@ -165,68 +122,60 @@ const AboutAlveusPage: NextPage = () => {
         <p>Click each box for source.</p>
       </Section>
 
-      <div className="relative">
-        <Image
-          src={leafRightImage2}
-          alt=""
-          className="pointer-events-none absolute -top-24 right-0 z-10 hidden h-auto w-1/2 max-w-[12rem] select-none lg:block"
-        />
+      {/* TODO: CTA slice for maya */}
 
-        <Section>
-          <Heading level={2} className="text-center">
-            Views By Country
-          </Heading>
+      {/* TODO: Timeline of Alveus (fundraising + enclosures), embed tour videos, CTA to events? */}
 
-          <ul className="mb-3 mt-6 flex flex-wrap justify-center">
-            {Object.entries(views).map(([key, country]) => (
-              <li key={key} className="basis-full py-2 md:basis-1/2 md:px-4">
-                <p>{country.name}</p>
-                <div className="w-full overflow-clip rounded-full bg-alveus-green">
-                  <div
-                    className="bg-alveus-green-900 py-2 text-right text-xs leading-none text-alveus-tan"
-                    style={{ width: `${country.value}%` }}
-                  >
-                    <span className="px-3">{country.value}%</span>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-
-          <p className="text-center text-xs text-gray-500">
-            (Source:{" "}
-            <Link href="https://www.similarweb.com/website/twitch.tv" external>
-              SimilarWeb
-            </Link>
-            )
-          </p>
-        </Section>
-      </div>
+      {/* TODO: CTA slice for ambassadors */}
 
       {/* Grow the last section to cover the page */}
       <div className="relative flex flex-grow flex-col">
         <Image
           src={leafLeftImage2}
           alt=""
-          className="pointer-events-none absolute -bottom-24 left-0 z-10 hidden h-auto w-1/2 max-w-[12rem] select-none lg:block"
+          className="pointer-events-none absolute -bottom-24 right-0 z-10 hidden h-auto w-1/2 max-w-[12rem] -scale-x-100 select-none lg:block"
         />
 
         <Section
           dark
-          className="flex-grow py-24"
-          containerClassName="flex flex-col items-center"
+          className="flex-grow bg-alveus-green-900"
+          containerClassName="flex flex-col-reverse md:flex-row-reverse gap-4 md:gap-10"
         >
-          <p className="my-2 text-center font-serif text-7xl font-bold">
-            1,000,000 Dollars Raised
-          </p>
-          <p className="my-2 font-serif text-4xl font-bold">Using Twitch!</p>
-          <IconTwitch
-            size={64}
-            className="mt-6 transition-colors hover:text-alveus-green-900"
-          />
-        </Section>
+          <div>
+            <Heading id="seal" level={2}>
+              Gold rated transparency
+            </Heading>
 
-        <TransparencySealSection />
+            <p>
+              Alveus&apos; transparency has been rated gold on Candid
+              (GuideStar). Candid is a leading source of information on
+              non-profit organizations, helping donors and funders make informed
+              decisions about their support. Check out our{" "}
+              <Link
+                external
+                href="https://www.guidestar.org/profile/86-1772907"
+              >
+                non-profit profile on Candid
+              </Link>
+              .
+            </p>
+          </div>
+
+          <Link
+            className="flex-shrink-0"
+            external
+            custom
+            href="https://www.guidestar.org/profile/86-1772907"
+          >
+            <Image
+              className="h-40 w-40"
+              src={imageGuidestarSeal}
+              width={200}
+              height={200}
+              alt="Gold Transparency Seal 2023 by Candid"
+            />
+          </Link>
+        </Section>
       </div>
     </>
   );
