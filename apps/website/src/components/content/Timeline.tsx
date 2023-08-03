7d4ac34b28252dc6d6c2944d2c1026b05f4bdbd2
MattIPv4 <me@mattcowley.co.uk>
2023-08-03 02:33:11 +0100

    Abstract timeline component

---
+++ apps/website/src/components/content/Timeline.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+import IconCalendar from "@/icons/IconCalendar";
+
+type TimelineProps = {
+  items: {
+    date: string;
+    content: React.ReactNode;
+  }[];
+};
+
+const Timeline: React.FC<TimelineProps> = ({ items }) => (
+  <div className="relative z-0 mx-auto mt-16 max-w-6xl">
+    <div className="absolute left-5 -z-10 h-full w-1 -translate-x-1/2 bg-alveus-green md:left-1/2" />
+    <ol>
+      {items.map((item, idx) => (
+        <li key={item.date} className="relative my-4 flex items-start">
+          <div
+            className={`absolute left-5 inline-block ${
+              idx % 2 ? "md:left-1/2" : "md:left-auto md:right-1/2"
+            }`}
+          >
+            <div
+              className={`
+            mt-1
+            hidden
+            border-y-[1rem] border-solid border-y-transparent md:block
+            ${
+              idx % 2
+                ? "ml-6 border-l-0 border-r-[1rem] border-r-alveus-green"
+                : "mr-6 border-l-[1rem] border-r-0 border-l-alveus-green"
+            }`}
+            />
+            <div
+              className={`
+            ml-6
+            mt-1 border-y-[1rem] border-l-0 border-r-[1rem]
+            border-solid border-y-transparent border-r-alveus-green md:hidden`}
+            />
+          </div>
+          <div className="rounded-full bg-alveus-green p-3 text-alveus-tan">
+            <IconCalendar size={16} />
+          </div>
+          <div
+            className={`hidden basis-1/2 px-4 pt-2 md:block ${
+              idx % 2 ? "md:order-first" : "md:order-last"
+            }`}
+          >
+            <p
+              className={`text-md text-alveus-green-500 ${
+                idx % 2 ? "text-right" : "text-left"
+              }`}
+            >
+              {item.date}
+            </p>
+          </div>
+          <div
+            className={`basis-full px-4 md:basis-1/2 ${
+              idx % 2 ? "md:order-last" : "md:order-first"
+            }`}
+          >
+            <div className="text-md rounded-lg bg-alveus-green p-6 text-alveus-tan">
+              <p className="mb-4 md:hidden">{item.date}</p>
+              {item.content}
+            </div>
+          </div>
+        </li>
+      ))}
+    </ol>
+  </div>
+);
+
+export default Timeline;
--- apps/website/src/pages/about/maya.tsx
+++ apps/website/src/pages/about/maya.tsx
@@ -5,8 +5,8 @@ import React from "react";
 import Section from "@/components/content/Section";
 import Heading from "@/components/content/Heading";
 import Meta from "@/components/content/Meta";
+import Timeline from "@/components/content/Timeline";
 import IconTwitch from "@/icons/IconTwitch";
-import IconCalendar from "@/icons/IconCalendar";
 
 import mayaImage from "@/assets/maya.png";
 import leafRightImage1 from "@/assets/floral/leaf-right-1.png";
@@ -17,43 +17,72 @@ import leafLeftImage3 from "@/assets/floral/leaf-left-3.png";
 const experience = [
   {
     date: "August, 2015",
-    text: "Level 3 certification in Parelli Natural Horsemanship",
+    content: <p>Level 3 certification in Parelli Natural Horsemanship</p>,
   },
   {
     date: "June, 2016",
-    text: "County 4-H All Star Ambassador",
+    content: <p>County 4-H All Star Ambassador</p>,
   },
   {
     date: "June, 2018",
-    text: "Outreach Education and Exotic Animal Husbandry intern at Zoo to You Conservation Ambassadors",
+    content: (
+      <p>
+        Outreach Education and Exotic Animal Husbandry intern at Zoo to You
+        Conservation Ambassadors
+      </p>
+    ),
   },
   {
     date: "January, 2019",
-    text: "Outreach Education and Exotic Animal Husbandry intern at Charles Paddock Zoo",
+    content: (
+      <p>
+        Outreach Education and Exotic Animal Husbandry intern at Charles Paddock
+        Zoo
+      </p>
+    ),
   },
   {
     date: "February, 2019",
-    text: "Falconry license issued by the Department of Fish and Wildlife",
+    content: (
+      <p>Falconry license issued by the Department of Fish and Wildlife</p>
+    ),
   },
   {
     date: "June, 2019",
-    text: "Education and Husbandry intern at Free Flight Exotic Bird Sanctuary",
+    content: (
+      <p>Education and Husbandry intern at Free Flight Exotic Bird Sanctuary</p>
+    ),
   },
   {
     date: "June, 2020",
-    text: "Graduated from California Polytechnic University at San Luis Obispo (earning Bachelor of Science in Agricultural Education and Communication)",
+    content: (
+      <p>
+        Graduated from California Polytechnic University at San Luis Obispo
+        (earning Bachelor of Science in Agricultural Education and
+        Communication)
+      </p>
+    ),
   },
   {
     date: "October, 2020 – October, 2021",
-    text: "Board of Directors for a wildlife rehabilitation center in Central Texas (role focused on raptor rehabilitation and husbandry)",
+    content: (
+      <p>
+        Board of Directors for a wildlife rehabilitation center in Central Texas
+        (role focused on raptor rehabilitation and husbandry)
+      </p>
+    ),
   },
   {
     date: "February, 2021",
-    text: "Founded Alveus Sanctuary",
+    content: <p>Founded Alveus Sanctuary</p>,
   },
   {
     date: "March, 2021 – March, 2022",
-    text: "Governing board of Make a Wish Foundation in Central and South Texas",
+    content: (
+      <p>
+        Governing board of Make a Wish Foundation in Central and South Texas
+      </p>
+    ),
   },
 ];
 
@@ -162,64 +191,7 @@ const AboutMayaPage: NextPage = () => {
             Maya&apos;s Experience
           </Heading>
 
-          <div className="relative z-0 mx-auto mt-16 max-w-6xl">
-            <div className="absolute left-5 -z-10 h-full w-1 -translate-x-1/2 bg-alveus-green md:left-1/2" />
-            <ol>
-              {experience.map((item, idx) => (
-                <li key={item.date} className="relative my-4 flex items-start">
-                  <div
-                    className={`absolute left-5 inline-block ${
-                      idx % 2 ? "md:left-1/2" : "md:left-auto md:right-1/2"
-                    }`}
-                  >
-                    <div
-                      className={`
-                    mt-1
-                    hidden
-                    border-y-[1rem] border-solid border-y-transparent md:block
-                    ${
-                      idx % 2
-                        ? "ml-6 border-l-0 border-r-[1rem] border-r-alveus-green"
-                        : "mr-6 border-l-[1rem] border-r-0 border-l-alveus-green"
-                    }`}
-                    />
-                    <div
-                      className={`
-                    ml-6
-                    mt-1 border-y-[1rem] border-l-0 border-r-[1rem]
-                    border-solid border-y-transparent border-r-alveus-green md:hidden`}
-                    />
-                  </div>
-                  <div className="rounded-full bg-alveus-green p-3 text-alveus-tan">
-                    <IconCalendar size={16} />
-                  </div>
-                  <div
-                    className={`hidden basis-1/2 px-4 pt-2 md:block ${
-                      idx % 2 ? "md:order-first" : "md:order-last"
-                    }`}
-                  >
-                    <p
-                      className={`text-md text-alveus-green-500 ${
-                        idx % 2 ? "text-right" : "text-left"
-                      }`}
-                    >
-                      {item.date}
-                    </p>
-                  </div>
-                  <div
-                    className={`basis-full px-4 md:basis-1/2 ${
-                      idx % 2 ? "md:order-last" : "md:order-first"
-                    }`}
-                  >
-                    <div className="text-md rounded-lg bg-alveus-green p-6 text-alveus-tan">
-                      <p className="mb-4 md:hidden">{item.date}</p>
-                      <p>{item.text}</p>
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ol>
-          </div>
+          <Timeline items={experience} />
         </Section>
       </div>
     </>
