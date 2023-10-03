73f3bb3eb1eab20d46d1e3bfd61aa017358214f7
MattIPv4 <me@mattcowley.co.uk>
2023-10-03 03:39:06 +0100

    Add accessible text content for 2022 report

--- apps/website/src/pages/about/annual-reports/2022.tsx
+++ apps/website/src/pages/about/annual-reports/2022.tsx
@@ -40,6 +40,330 @@ const AboutAnnualReport2022Page: NextPage = () => {
           alt=""
           className="mx-auto h-auto w-full max-w-3xl"
         />
+
+        <div className="sr-only">
+          <h2>Followers</h2>
+
+          <table>
+            <thead>
+              <tr>
+                <th>Platform</th>
+                <th>Start 2022</th>
+                <th>End 2022</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>Twitch</td>
+                <td>19,575</td>
+                <td>68,600</td>
+              </tr>
+              <tr>
+                <td>Instagram</td>
+                <td>33,742</td>
+                <td>47,000</td>
+              </tr>
+              <tr>
+                <td>YouTube</td>
+                <td>0</td>
+                <td>25,400</td>
+              </tr>
+              <tr>
+                <td>TikTok</td>
+                <td>27,800</td>
+                <td>41,800</td>
+              </tr>
+              <tr>
+                <td>X (Twitter)</td>
+                <td>55,219</td>
+                <td>67,700</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <h2>Twitch Demographics</h2>
+
+          <dl>
+            <dt>Male</dt>
+            <dd>78.36%</dd>
+
+            <dt>Female</dt>
+            <dd>19.64%</dd>
+
+            <dt>Other</dt>
+            <dd>2%</dd>
+          </dl>
+
+          <dl>
+            <dt>18-24</dt>
+            <dd>35.85%</dd>
+
+            <dt>25-34</dt>
+            <dd>32.14%</dd>
+
+            <dt>35-44</dt>
+            <dd>15.33%</dd>
+
+            <dt>45-54</dt>
+            <dd>8.62%</dd>
+
+            <dt>55-64</dt>
+            <dd>4.97%</dd>
+          </dl>
+
+          <dl>
+            <dt>USA</dt>
+            <dd>20.48%</dd>
+
+            <dt>Germany</dt>
+            <dd>6.54%</dd>
+
+            <dt>South Korea</dt>
+            <dd>5.09%</dd>
+
+            <dt>Russia</dt>
+            <dd>4.7%</dd>
+
+            <dt>France</dt>
+            <dd>4.27%</dd>
+
+            <dt>Others</dt>
+            <dd>58.95%</dd>
+          </dl>
+
+          <h3>50.3 million minutes watched</h3>
+          <h3>2.65 million live views</h3>
+          <h3>18,689 peak viewership</h3>
+
+          <h2>Financial Report</h2>
+
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <strong>Total Income</strong>
+                </td>
+                <td>
+                  <strong>300,066.79</strong>
+                </td>
+              </tr>
+              <tr>
+                <td>Donations and Grants</td>
+                <td>266,585.66</td>
+              </tr>
+              <tr>
+                <td>Merch</td>
+                <td>33,481.13</td>
+              </tr>
+            </tbody>
+
+            <tbody>
+              <tr>
+                <td colSpan={2}>
+                  <hr />
+                </td>
+              </tr>
+            </tbody>
+
+            <tbody>
+              <tr>
+                <td>
+                  <strong>Total Expenses</strong>
+                </td>
+                <td>
+                  <strong>285,838.93</strong>
+                </td>
+              </tr>
+              <tr>
+                <td>Operational Expenses</td>
+                <td>32,679.86</td>
+              </tr>
+              <tr>
+                <td>Advertising Expense</td>
+                <td>154.61</td>
+              </tr>
+              <tr>
+                <td>Fundraising Expense</td>
+                <td>4,218.80</td>
+              </tr>
+              <tr>
+                <td>Salary Expense</td>
+                <td>121,240.94</td>
+              </tr>
+              <tr>
+                <td>Insurance Expense</td>
+                <td>16,429.08</td>
+              </tr>
+              <tr>
+                <td>Repairs and Maintenance Expense</td>
+                <td>38,792.41</td>
+              </tr>
+              <tr>
+                <td>Meals &amp; Entertainment</td>
+                <td>1,344.43</td>
+              </tr>
+              <tr>
+                <td>Office Supplies &amp; Software Expense</td>
+                <td>5,551.98</td>
+              </tr>
+              <tr>
+                <td>Other Income &amp; Expenses</td>
+                <td>3,318.66</td>
+              </tr>
+              <tr>
+                <td>Tax Expense</td>
+                <td>147</td>
+              </tr>
+              <tr>
+                <td>Depreciation</td>
+                <td>10,297.08</td>
+              </tr>
+              <tr>
+                <td>Utilities Expense</td>
+                <td>9,516.09</td>
+              </tr>
+              <tr>
+                <td>Travel Expense</td>
+                <td>3,229.19</td>
+              </tr>
+              <tr>
+                <td>Contractor Expense</td>
+                <td>15,512.46</td>
+              </tr>
+              <tr>
+                <td>Streaming Expense</td>
+                <td>17,024.54</td>
+              </tr>
+              <tr>
+                <td>Legal &amp; Professional Fees</td>
+                <td>6,381.80</td>
+              </tr>
+            </tbody>
+
+            <tbody>
+              <tr>
+                <td colSpan={2}>
+                  <hr />
+                </td>
+              </tr>
+            </tbody>
+
+            <tbody>
+              <tr>
+                <td>
+                  <strong>Net Income</strong>
+                </td>
+                <td>
+                  <strong>14,227.86</strong>
+                </td>
+              </tr>
+            </tbody>
+
+            <tbody>
+              <tr>
+                <td colSpan={2}>
+                  <hr />
+                </td>
+              </tr>
+            </tbody>
+
+            <tbody>
+              <tr>
+                <td>
+                  <strong>Total Assets</strong>
+                </td>
+                <td>
+                  <strong>621,387.23</strong>
+                </td>
+              </tr>
+              <tr>
+                <td>Total Current Assets - Cash</td>
+                <td>117,763.40</td>
+              </tr>
+              <tr>
+                <td>Total Fixed Assets</td>
+                <td>503,623.83</td>
+              </tr>
+            </tbody>
+
+            <tbody>
+              <tr>
+                <td colSpan={2}>
+                  <hr />
+                </td>
+              </tr>
+            </tbody>
+
+            <tbody>
+              <tr>
+                <td>
+                  <strong>Total Liabilities</strong>
+                </td>
+                <td>
+                  <strong>677.99</strong>
+                </td>
+              </tr>
+            </tbody>
+
+            <tbody>
+              <tr>
+                <td colSpan={2}>
+                  <hr />
+                </td>
+              </tr>
+            </tbody>
+
+            <tbody>
+              <tr>
+                <td>
+                  <strong>Total Equity - Retained Earnings</strong>
+                </td>
+                <td>
+                  <strong>620,709.24</strong>
+                </td>
+              </tr>
+            </tbody>
+
+            <tbody>
+              <tr>
+                <td colSpan={2}>
+                  <hr />
+                </td>
+              </tr>
+            </tbody>
+
+            <tbody>
+              <tr>
+                <td>
+                  <strong>Total Liabilities &amp; Equity</strong>
+                </td>
+                <td>
+                  <strong>621,387.23</strong>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          <h2>Continuing Our Mission</h2>
+
+          <p>
+            The mission of Alveus is to inspire online audiences to engage in
+            conservation efforts. We do this by nurturing a connection between
+            viewers and our animal ambassadors so that they learn to care about
+            their species.
+          </p>
+
+          <p>
+            With your continued support, we are able to remain a powerful force
+            for conservation while providing enriched and comfortable lives for
+            our animal ambassadors.
+          </p>
+
+          <p>
+            <strong>Thank you.</strong>
+          </p>
+        </div>
       </Section>
     </>
   );
