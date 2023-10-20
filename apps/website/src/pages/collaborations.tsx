71703b6a363042b4bcff7e8d6b67d4f6ad42f42a
MattIPv4 <me@mattcowley.co.uk>
2023-10-20 01:27:39 +0100

    Update Daily Dose collab video

--- apps/website/src/pages/collaborations.tsx
+++ apps/website/src/pages/collaborations.tsx
@@ -89,7 +89,8 @@ const collaborations = {
     name: "Daily Dose",
     link: "https://www.youtube.com/@DailyDoseOfInternet",
     date: new Date("2023-04-25"),
-    videoId: "j8GxD4adNyk",
+    videoId: "M9BBwvR3i-E",
+    vodId: "j8GxD4adNyk",
   },
   graycen: {
     name: "Graycen",
