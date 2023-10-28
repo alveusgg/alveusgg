de78592e45fe1d4181576ab8e0b7234d833f95e7
Paul Golmann <mail@pje-web.de>
2023-10-28 04:42:11 +0200

    remove additional share links

--- apps/website/src/components/shared/ShareButton.tsx
+++ apps/website/src/components/shared/ShareButton.tsx
@@ -3,8 +3,6 @@ import { Fragment, useEffect, useId, useState } from "react";
 
 import { Popover } from "@headlessui/react";
 
-import { env } from "@/env/index.mjs";
-
 import { classes } from "@/utils/classes";
 
 import IconTwitter from "@/icons/IconTwitter";
@@ -12,13 +10,8 @@ import IconFacebook from "@/icons/IconFacebook";
 import IconShare from "@/icons/IconShare";
 import IconEnvelope from "@/icons/IconEnvelope";
 import IconClipboard from "@/icons/IconClipboard";
-import IconReddit from "@/icons/IconReddit";
-import IconFacebookMessenger from "@/icons/IconFacebookMessenger";
-import IconTelegram from "@/icons/IconTelegram";
-import IconWhatsapp from "@/icons/IconWhatsapp";
-import IconSMS from "@/icons/IconSMS";
 
-import { Button } from "@/components/shared/Button";
+import { Button, defaultButtonClasses } from "@/components/shared/Button";
 import { PopoverButton } from "@/components/shared/PopoverButton";
 import { QRCode } from "@/components/QrCode";
 
@@ -33,66 +26,6 @@ function createFacebookLink({ url }: { url: string }) {
   const link = new URL("https://www.facebook.com/sharer/sharer.php");
   link.searchParams.append("u", url);
   return link.toString();
-}
-
-function createFacebookMessengerLink({
-  url,
-  appId,
-}: {
-  url: string;
-  appId: string;
-}) {
-  const link = new URL("https://www.facebook.com/dialog/send");
-  link.searchParams.append("link", url);
-  link.searchParams.append("redirect_uri", url);
-  link.searchParams.append("app_id", appId);
-  return link.toString();
-}
-
-function createWhatsappLink({ text, url }: { text: string; url: string }) {
-  const link = new URL("whatsapp://send");
-  link.searchParams.append("text", `${text}\n\n${url}`);
-  return link.toString();
-}
-
-function createTelegramLink({ text, url }: { text: string; url: string }) {
-  const link = new URL("https://telegram.me/share/url");
-  link.searchParams.append("text", `${text}\n\n${url}`);
-  return link.toString();
-}
-
-function createRedditLink({ text, url }: { text: string; url: string }) {
-  const link = new URL("https://www.reddit.com/submit");
-  link.searchParams.append("title", text);
-  link.searchParams.append("url", url);
-  link.searchParams.append("resubmit", "true");
-  return link.toString();
-}
-
-// Only works with Firefox on Desktop and Android
-function createSMSLink({ text, url }: { text: string; url: string }) {
-  const link = new URL("sms:");
-  link.searchParams.append("body", `${text}\n\n${url}`);
-  return link.toString().replaceAll("+", "%20");
-}
-
-// Only works with Safari on macOS and iOS
-function createIMessageLink({ text, url }: { text: string; url: string }) {
-  const link = new URL("imessage:");
-  link.searchParams.append("body", `${text}\n\n${url}`);
-  return link.toString().replaceAll("+", "%20");
-}
-
-const isBrowser = typeof window !== "undefined";
-const isIOS = isBrowser && navigator.userAgent.includes("iPhone");
-const isMacOS = isBrowser && navigator.userAgent.includes("Macintosh");
-const isFirefox = isBrowser && navigator.userAgent.includes("Firefox");
-const canSentMessageByProtocol = isBrowser && (isIOS || isMacOS || isFirefox);
-
-function createMessageLink({ text, url }: { text: string; url: string }) {
-  if (!canSentMessageByProtocol) return;
-  if (isIOS || isMacOS) return createIMessageLink({ text, url });
-  return createSMSLink({ text, url });
 }
 
 function createEmailLink({
@@ -119,7 +52,10 @@ function ShareLink({
       <a
         target="_blank"
         rel="noopener noreferrer"
-        className={classes("flex gap-1 rounded-lg p-1 text-sm", className)}
+        className={classes(
+          "flex gap-1 rounded-lg p-1 text-sm hover:underline",
+          className,
+        )}
         {...attributes}
       />
     </Popover.Button>
@@ -195,6 +131,12 @@ export function ShareButton({
         <Button
           width="auto"
           size="small"
+          className={classes(
+            defaultButtonClasses,
+            "transition-colors hover:bg-gray-900",
+            linkCopiedState === "success" && "bg-green-900",
+            linkCopiedState === "error" && "bg-red-900",
+          )}
           onClick={() => {
             const linkInputElement = document.getElementById(
               linkInputId,
@@ -237,51 +179,6 @@ export function ShareButton({
         >
           <IconEnvelope className="h-4 w-4" />
           Email
-        </ShareLink>
-        {canSentMessageByProtocol && (
-          <ShareLink
-            href={createMessageLink({ text, url })}
-            title="Share via SMS/iMessage"
-          >
-            <IconSMS className="h-4 w-4" />
-            Message
-          </ShareLink>
-        )}
-      </div>
-
-      <div className="flex gap-2">
-        {env.NEXT_PUBLIC_FACEBOOK_APP_ID && (
-          <ShareLink
-            href={createFacebookMessengerLink({
-              url,
-              appId: env.NEXT_PUBLIC_FACEBOOK_APP_ID,
-            })}
-            title="Share on Facebook Messenger"
-          >
-            <IconFacebookMessenger className="h-4 w-4" />
-            Messenger
-          </ShareLink>
-        )}
-        <ShareLink
-          href={createWhatsappLink({ text, url })}
-          title="Share on Whatsapp"
-        >
-          <IconWhatsapp className="h-4 w-4" />
-          Whatsapp
-        </ShareLink>
-        <ShareLink
-          href={createTelegramLink({ text, url })}
-          title="Share on Telegram"
-        >
-          <IconTelegram className="h-4 w-4" />
-          Telegram
-        </ShareLink>
-        <ShareLink
-          href={createRedditLink({ text, url })}
-          title="Share on Reddit"
-        >
-          <IconReddit className="h-4 w-4" />
-          Reddit
         </ShareLink>
       </div>
 
