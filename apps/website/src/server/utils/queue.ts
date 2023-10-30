ef643a6bc66d5726e99f491a810984834fb7edce
Paul Golmann <mail@pje-web.de>
2023-10-30 21:21:53 +0100

    improved queue util
    - support other http methods through the fetchUrl method
    - allow passing additional fetch init options to the methods
    - add option to delay the call

--- apps/website/src/server/utils/queue.ts
+++ apps/website/src/server/utils/queue.ts
@@ -1,32 +1,70 @@
 import { env } from "@/env/index.mjs";
 
-export function callEndpoint<T>(endpoint: string, body: T) {
-  const bodyText = JSON.stringify(body);
-  const fullEndpointUrl = new URL(
-    endpoint,
-    env.NEXT_PUBLIC_BASE_URL,
-  ).toString();
+type QueueFetchInit = RequestInit & { headers?: Record<string, string> };
+type QueueFetchOptions = { delaySeconds?: number };
+
+const hasQueueEnv = Boolean(env.UPSTASH_QSTASH_URL && env.UPSTASH_QSTASH_KEY);
+
+export function canQueue() {
+  return hasQueueEnv;
+}
+
+export async function fetchUrl(
+  url: string,
+  fetchInit?: QueueFetchInit,
+  options?: QueueFetchOptions,
+) {
+  const delay = options?.delaySeconds;
+  const fullUrl = new URL(url, env.NEXT_PUBLIC_BASE_URL).toString();
 
   // Use queue
-  if (env.UPSTASH_QSTASH_URL && env.UPSTASH_QSTASH_KEY) {
-    return fetch(`${env.UPSTASH_QSTASH_URL}${fullEndpointUrl}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${env.UPSTASH_QSTASH_KEY}`,
-        "Upstash-Forward-Authorization": `Bearer ${env.ACTION_API_SECRET}`,
-      },
-      body: bodyText,
+  if (canQueue()) {
+    const headers = fetchInit?.headers ?? {};
+    if (headers.Authorization) {
+      headers["Upstash-Forward-Authorization"] = headers.Authorization;
+    }
+    if (options?.delaySeconds) {
+      headers["Upstash-Delay"] = `${delay}s`;
+    }
+    headers.Authorization = `Bearer ${env.UPSTASH_QSTASH_KEY}`;
+
+    return fetch(`${env.UPSTASH_QSTASH_URL}${fullUrl}`, {
+      ...fetchInit,
+      headers,
+    });
+  }
+
+  if (delay) {
+    console.warn(
+      "Delaying queued call without a configured queue! This will block the request for the delay time!",
+    );
+    await new Promise((resolve) => {
+      setTimeout(resolve, delay * 1000);
     });
   }
 
   // Call directly
-  return fetch(fullEndpointUrl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${env.ACTION_API_SECRET}`,
+  return fetch(fullUrl);
+}
+
+export function callEndpoint<T>(
+  endpoint: string,
+  body: T,
+  fetchInit?: QueueFetchInit,
+  options?: QueueFetchOptions,
+) {
+  return fetchUrl(
+    endpoint,
+    {
+      ...fetchInit,
+      method: "POST",
+      headers: {
+        ...fetchInit?.headers,
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${env.ACTION_API_SECRET}`,
+      } as QueueFetchInit["headers"],
+      body: JSON.stringify(body),
     },
-    body: bodyText,
-  });
+    options,
+  );
 }
