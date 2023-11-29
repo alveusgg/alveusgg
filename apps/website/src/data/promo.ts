dd95c2f45f46250dde0642fe4c40651731d66912
MattIPv4 <me@mattcowley.co.uk>
2023-11-29 20:46:22 +0000

    s/Christmas/Holiday/g

--- apps/website/src/data/promo.ts
+++ apps/website/src/data/promo.ts
@@ -8,10 +8,10 @@ interface Promo {
 }
 
 const promo: Promo | null = {
-  title: "Christmas Sweaters & 2024 Calendars",
+  title: "Holiday Sweaters & 2024 Calendars",
   subtitle: "Available from December 1st",
   content:
-    "Grab yourself an embroidered Christmas sweater and a 2024 calendar featuring a selection of our ambassadors, shipping worldwide and in time for Christmas.",
+    "Grab yourself an embroidered Holiday sweater and a 2024 calendar featuring a selection of our ambassadors, shipping worldwide and in time for Christmas.",
   link: "/apparel",
   external: true,
   excluded: [],
