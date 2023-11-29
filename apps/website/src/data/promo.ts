6b3b7f29bae515548bc67a84697d746a36da0b7c
MattIPv4 <me@mattcowley.co.uk>
2023-11-29 20:42:38 +0000

    Add shop promo as tophat

---
+++ apps/website/src/data/promo.ts
@@ -0,0 +1,20 @@
+interface Promo {
+  title: string;
+  subtitle: string;
+  content: string;
+  link: string;
+  external: boolean;
+  excluded: string[];
+}
+
+const promo: Promo | null = {
+  title: "Christmas Sweaters & 2024 Calendars",
+  subtitle: "Available from December 1st",
+  content:
+    "Grab yourself an embroidered Christmas sweater and a 2024 calendar featuring a selection of our ambassadors, shipping worldwide and in time for Christmas.",
+  link: "/apparel",
+  external: true,
+  excluded: [],
+};
+
+export default promo;
