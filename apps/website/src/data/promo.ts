a1bebffab4734ca435ba0b1f6cba7255e846ef74
MattIPv4 <me@mattcowley.co.uk>
2024-01-14 03:08:03 +0000

    Remove December 1st messaging for top-hat promo

--- apps/website/src/data/promo.ts
+++ apps/website/src/data/promo.ts
@@ -8,10 +8,10 @@ interface Promo {
 }
 
 const promo: Promo | null = {
-  title: "Holiday Sweaters & 2024 Calendars",
-  subtitle: "Available from December 1st",
+  title: "Holiday Sweaters, 2024 Calendars, and more",
+  subtitle: "Available Now",
   content:
-    "Grab yourself an embroidered Holiday sweater and a 2024 calendar featuring a selection of our ambassadors, shipping worldwide and in time for Christmas.",
+    "Grab yourself an embroidered Holiday sweater, a 2024 calendar featuring a selection of our ambassadors, or discover more Alveus apparel in the store.",
   link: "/apparel",
   external: true,
   excluded: [],
