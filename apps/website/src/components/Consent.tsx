a27d851cb723d5cf7f640382efdb95e1ff302631
Matt Cowley <me@mattcowley.co.uk>
2024-01-17 15:07:30 +0000

    Bypass consent for crawlers on AQ pages (#506)

--- apps/website/src/components/Consent.tsx
+++ apps/website/src/components/Consent.tsx
@@ -1,4 +1,9 @@
-import { useCallback, type MouseEventHandler, type ReactNode } from "react";
+import {
+  useCallback,
+  type MouseEventHandler,
+  type ReactNode,
+  useMemo,
+} from "react";
 
 import { classes } from "@/utils/classes";
 
@@ -14,12 +19,32 @@ import Link from "@/components/content/Link";
 type ConsentProps = {
   item: string;
   consent: ConsentKey;
+  indexable?: boolean;
   className?: string;
   children: ReactNode;
 };
 
-const Consent = ({ item, consent: key, className, children }: ConsentProps) => {
+const Consent = ({
+  item,
+  consent: key,
+  indexable,
+  className,
+  children,
+}: ConsentProps) => {
+  // Get the current user consent state
+  // If this is "indexable", we'll ignore consent for known crawlers
   const { consent, update, loaded } = useConsent();
+  const crawling = useMemo(
+    () =>
+      indexable &&
+      typeof navigator !== "undefined" &&
+      ["googlebot", "bingbot", "linkedinbot"].some((bot) =>
+        navigator.userAgent.toLowerCase().includes(bot),
+      ),
+    [indexable],
+  );
+
+  // When the user clicks the consent button, update the state
   const clicked = useCallback<MouseEventHandler<HTMLButtonElement>>(
     (e) => {
       e.preventDefault();
@@ -36,7 +61,7 @@ const Consent = ({ item, consent: key, className, children }: ConsentProps) => {
       )}
     >
       {loaded &&
-        (consent[key] ? (
+        (crawling || consent[key] ? (
           <>
             <p className="absolute -z-10 m-auto text-2xl" aria-hidden="true">
               Loading {item}...
