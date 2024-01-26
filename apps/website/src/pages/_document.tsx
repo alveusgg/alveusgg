44a68fe6339f5f99508dc9616b4590affe7035e2
Matt Cowley <me@mattcowley.co.uk>
2024-01-26 20:50:08 +0000

    Set HTML background to avoid white on overscroll (#513)

--- apps/website/src/pages/_document.tsx
+++ apps/website/src/pages/_document.tsx
@@ -2,7 +2,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 const Document = () => {
   return (
-    <Html lang="en">
+    <Html
+      lang="en"
+      className="bg-gradient-to-b from-alveus-green-900 from-45% to-gray-800 to-55% bg-fixed"
+    >
       <Head />
       <body>
         <Main />
