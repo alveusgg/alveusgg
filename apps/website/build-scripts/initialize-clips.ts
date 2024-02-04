e3d7bd1a05a7a74f756bb96b8914b5a79077f02c
trimbe <19672127+trimbe@users.noreply.github.com>
2024-02-04 12:06:55 -0800

    add build script to insert historical clips

---
+++ apps/website/build-scripts/initialize-clips.ts
@@ -0,0 +1,47 @@
+import { prisma } from "../src/server/db/client";
+import { getClipsByDate } from "../src/server/utils/twitch-api";
+
+async function populateClips() {
+  const latestClip = await prisma.clip.findFirst({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const startDate = new Date(0);
+  let belowMinViews = false;
+
+  let clips = undefined;
+  do {
+    clips = await getClipsByDate(
+      "636587384",
+      startDate,
+      new Date(),
+      clips ? clips.pagination.cursor : undefined,
+    );
+
+    const clipsToAdd = [];
+    for (const clip of clips.data) {
+      if (clip.view_count < 500) {
+        // api sorts by views, so once we hit a clip with < 500 views, we can stop
+        belowMinViews = true;
+        break;
+      }
+
+      clipsToAdd.push({
+        slug: clip.id,
+        title: clip.title,
+        createdAt: new Date(clip.created_at),
+        thumbnailUrl: clip.thumbnail_url,
+        creator: clip.creator_name,
+      });
+    }
+
+    await prisma.clip.createMany({
+      data: clipsToAdd,
+      skipDuplicates: true,
+    });
+  } while (clips.pagination.cursor && !belowMinViews);
+}
+
+populateClips();
