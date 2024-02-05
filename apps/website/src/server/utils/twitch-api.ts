eb4feed152270b3b0db3acfc002a64227c9e9a17
Jonas Franke <jonas.franke1@gmx.net>
2024-02-05 16:23:44 +0100

    remove unused constant

--- apps/website/src/server/utils/twitch-api.ts
+++ apps/website/src/server/utils/twitch-api.ts
@@ -74,28 +74,6 @@ const subscriptionsResponseSchema = z.object({
   pagination: paginationSchema,
 });
 
-const streamsResponseSchema = z.object({
-  data: z.array(
-    z.object({
-      id: z.string(),
-      user_id: z.string(),
-      user_login: z.string(),
-      user_name: z.string(),
-      game_id: z.string(),
-      game_name: z.string(),
-      type: z.string(),
-      title: z.string(),
-      viewer_count: z.number(),
-      started_at: z.string(),
-      language: z.string(),
-      thumbnail_url: z.string(),
-      tag_ids: z.array(z.string()),
-      is_mature: z.boolean(),
-    }),
-  ),
-  pagination: paginationSchema,
-});
-
 export async function getSubscriptions() {
   const response = await fetch(
     "https://api.twitch.tv/helix/eventsub/subscriptions",
