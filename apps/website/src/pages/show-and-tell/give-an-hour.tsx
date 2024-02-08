52402b40336aac7e6b50011283c299d43af54365
MattIPv4 <me@mattcowley.co.uk>
2024-02-08 19:57:45 +0000

    Add styling for action cards

--- apps/website/src/pages/show-and-tell/give-an-hour.tsx
+++ apps/website/src/pages/show-and-tell/give-an-hour.tsx
@@ -1,12 +1,69 @@
 import type { NextPage } from "next";
-import Image from "next/image";
+import Image, { type ImageProps } from "next/image";
+
+import { classes } from "@/utils/classes";
 
 import Meta from "@/components/content/Meta";
 import Section from "@/components/content/Section";
 import Heading from "@/components/content/Heading";
 import Link from "@/components/content/Link";
 
+// Artwork from https://undraw.co
+import leafLeftImage3 from "@/assets/floral/leaf-left-3.png";
+import leafRightImage2 from "@/assets/floral/leaf-right-2.png";
 import giveAnHourLogo from "@/assets/show-and-tell/give-an-hour/logo.svg";
+import giveAnHourArt from "@/assets/show-and-tell/give-an-hour/art.svg";
+import giveAnHourEntertainment from "@/assets/show-and-tell/give-an-hour/entertainment.svg";
+import giveAnHourFitness from "@/assets/show-and-tell/give-an-hour/fitness.svg";
+import giveAnHourShopping from "@/assets/show-and-tell/give-an-hour/shopping.svg";
+import giveAnHourWalk from "@/assets/show-and-tell/give-an-hour/walk.svg";
+import giveAnHourNature from "@/assets/show-and-tell/give-an-hour/nature.svg";
+
+const Card = ({
+  heading,
+  id,
+  image,
+  number,
+  children,
+}: {
+  heading: string;
+  id: string;
+  image: ImageProps["src"];
+  number?: number;
+  children: React.ReactNode;
+}) => (
+  <div className="flex h-full gap-6 rounded-xl bg-white/75 p-6 shadow-lg backdrop-blur">
+    <div
+      className={classes(
+        "flex flex-shrink-0 flex-col items-center justify-between gap-4",
+        number === undefined && "md:order-last",
+      )}
+    >
+      <Image src={image} alt="" className="w-24 md:w-32" />
+
+      {number !== undefined && (
+        <p className="text-5xl font-bold text-alveus-green opacity-50">
+          {number.toLocaleString(undefined, {
+            minimumIntegerDigits: 2,
+            useGrouping: false,
+          })}
+        </p>
+      )}
+    </div>
+
+    <div className="flex-grow text-lg">
+      <Heading
+        level={3}
+        id={id}
+        link
+        className="mt-0 scroll-mt-8 font-sans text-2xl"
+      >
+        {heading}
+      </Heading>
+      {children}
+    </div>
+  </div>
+);
 
 const GiveAnHourPage: NextPage = () => (
   <>
@@ -51,90 +108,135 @@ const GiveAnHourPage: NextPage = () => (
     </Section>
 
     {/* Grow the last section to cover the page */}
-    <Section className="flex-grow">
-      <Heading level={2} id="why-it-matters" link>
-        Why it matters?
-      </Heading>
+    <Section className="flex-grow" containerClassName="max-w-6xl">
+      <div className="mx-auto mb-8 max-w-2xl text-center">
+        <Heading level={2} id="why-it-matters" link className="text-4xl">
+          Why it matters?
+        </Heading>
 
-      <p>
-        <strong>An essential ally against the climate crisis is nature.</strong>
-        Yet, we are losing nature at an alarming and unprecedented rate. Each of
-        us can do something positive for our planet for 60 minutes and together,
-        we can create the #BiggestHourForEarth.
-      </p>
+        <p className="text-lg">
+          <strong>
+            An essential ally against the climate crisis is nature.
+          </strong>{" "}
+          Yet, we are losing nature at an alarming and unprecedented rate. Each
+          of us can do something positive for our planet for 60 minutes and
+          together, we can create the #BiggestHourForEarth.
+        </p>
+      </div>
 
-      <Heading level={2} id="actions" link>
+      <Heading level={2} id="actions" link className="text-center">
         Actions you can take
       </Heading>
-      {/* TODO: Style each of the sections below as a card (see Maya's graphic) */}
 
-      <Heading level={3} id="actions:food" link>
-        Food
-      </Heading>
-      <ul>
-        <li>Cook/order a plant-based meal.</li>
-        <li>Look for sustainable products at the supermarket.</li>
-        <li>Go to a farmer&apos;s market.</li>
-        <li>Swap staples with more sustainable ones.</li>
-      </ul>
+      <div className="my-8 grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
+        <Card
+          heading="Food"
+          id="actions:food"
+          image={giveAnHourShopping}
+          number={1}
+        >
+          <ul className="list-disc ps-4">
+            <li>Cook/order a plant-based meal.</li>
+            <li>Look for sustainable products at the supermarket.</li>
+            <li>Go to a farmer&apos;s market.</li>
+            <li>Swap staples with more sustainable ones.</li>
+          </ul>
+        </Card>
 
-      <Heading level={3} id="actions:entertainment" link>
-        Entertainment
-      </Heading>
-      <ul>
-        <li>Visit an art gallery/exhibition about our planet.</li>
-        <li>Watch an educational video or documentary.</li>
-      </ul>
+        <div className="relative">
+          <Image
+            src={leafRightImage2}
+            alt=""
+            className="pointer-events-none absolute -bottom-14 right-0 -z-10 h-auto w-1/2 max-w-[12rem] select-none lg:-bottom-32 lg:-left-20 lg:right-auto lg:max-w-[10rem]"
+          />
 
-      <Heading level={3} id="actions:fitness-wellness" link>
-        Fitness &amp; Wellness
-      </Heading>
-      <ul>
-        <li>Attend a kayaking tour to learn about local nature.</li>
-        <li>Join an outdoor yoga session.</li>
-        <li>
-          Attend a charity run and choose WWF or Alveus as your organization.
-        </li>
-        <li>Cycle or walk to work.</li>
-      </ul>
+          <Card
+            heading="Entertainment"
+            id="actions:entertainment"
+            image={giveAnHourEntertainment}
+            number={2}
+          >
+            <ul className="list-disc ps-4">
+              <li>Visit an art gallery/exhibition about our planet.</li>
+              <li>Watch an educational video or documentary.</li>
+            </ul>
+          </Card>
+        </div>
 
-      <Heading level={3} id="actions:arts-creativity" link>
-        Arts &amp; Creativity
-      </Heading>
-      <ul>
-        <li>Try wildlife photography.</li>
-        <li>Upcycle something.</li>
-        <li>Draw or paint a nature scene.</li>
-      </ul>
+        <Card
+          heading="Fitness & Wellness"
+          id="actions:fitness-wellness"
+          image={giveAnHourFitness}
+          number={3}
+        >
+          <ul className="list-disc ps-4">
+            <li>Attend a kayaking tour to learn about local nature.</li>
+            <li>Join an outdoor yoga session.</li>
+            <li>
+              Attend a charity run and choose WWF or Alveus as your
+              organization.
+            </li>
+            <li>Cycle or walk to work.</li>
+          </ul>
+        </Card>
 
-      <Heading level={3} id="actions:get-outside" link>
-        Get Outside
-      </Heading>
-      <ul>
-        <li>Do a trash clean-up.</li>
-        <li>Plant a pollinator garden.</li>
-        <li>Start a compost pile.</li>
-        <li>Go for a nature walk or bird watch.</li>
-      </ul>
+        <Card
+          heading="Arts & Creativity"
+          id="actions:arts-creativity"
+          image={giveAnHourArt}
+          number={4}
+        >
+          <ul className="list-disc ps-4">
+            <li>Try wildlife photography.</li>
+            <li>Upcycle something.</li>
+            <li>Draw or paint a nature scene.</li>
+          </ul>
+        </Card>
 
-      <Heading level={3} id="actions:be-creative" link>
-        Be creative and have fun!
-      </Heading>
-      <p>As long as your activity helps to:</p>
-      <ul>
-        <li>Reconnect with our planet.</li>
-        <li>Restore our planet.</li>
-        <li>Learn more about our planet.</li>
-        <li>Inspire others to care for our planet.</li>
-      </ul>
-      <p>...then we want to hear about it!</p>
+        <div className="relative">
+          <Image
+            src={leafLeftImage3}
+            alt=""
+            className="pointer-events-none absolute -top-20 right-0 -z-10 h-auto w-1/2 max-w-[12rem] -scale-x-100 -scale-y-100 select-none lg:-right-32 lg:max-w-[10rem] lg:scale-x-100"
+          />
+
+          <Card
+            heading="Get Outside"
+            id="actions:get-outside"
+            image={giveAnHourWalk}
+            number={5}
+          >
+            <ul className="list-disc ps-4">
+              <li>Do a trash clean-up.</li>
+              <li>Plant a pollinator garden.</li>
+              <li>Start a compost pile.</li>
+              <li>Go for a nature walk or bird watch.</li>
+            </ul>
+          </Card>
+        </div>
+
+        <Card
+          heading="Be creative and have fun!"
+          id="actions:be-creative"
+          image={giveAnHourNature}
+        >
+          <p>As long as your activity helps to:</p>
+          <ul className="my-2 list-disc ps-4">
+            <li>Reconnect with our planet.</li>
+            <li>Restore our planet.</li>
+            <li>Learn more about our planet.</li>
+            <li>Inspire others to care for our planet.</li>
+          </ul>
+          <p>...then we want to hear about it!</p>
+        </Card>
+      </div>
 
       <Heading level={2} id="share-your-activities" link>
         Share your activities
       </Heading>
       <p>
         Share your activities with the community and inspire others, via the{" "}
-        <Link href="/show-and-tell/submit-post" dark className="underline">
+        <Link href="/show-and-tell/submit-post" className="underline">
           Show and Tell
         </Link>{" "}
         page.
