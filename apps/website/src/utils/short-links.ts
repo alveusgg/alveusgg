edf0fddb5fc48e204ce354c03a377fcd69c2bb1f
Jonas Franke <jonas.franke1@gmx.net>
2024-02-10 13:22:08 +0100

    remove comments

--- apps/website/src/utils/short-links.ts
+++ apps/website/src/utils/short-links.ts
@@ -2,23 +2,5 @@ import { z } from "zod";
 
 export const shortLinkConfigSchema = z.object({
   checks: z.boolean().optional(),
-  requireShippingAddress: z.boolean().optional(),
-  intro: z.string().optional(),
   rules: z.string().optional(),
-  askMarketingEmails: z.boolean().optional(),
-  askMarketingEmailsLabel: z.string().optional(),
 });
-
-/*export function calcFormConfig(formConfig?: string) {
-  const config: CalculatedFormConfig = getDefaultFormConfig();
-  if (formConfig) {
-    const parsedConfig = formConfigSchema.safeParse(JSON.parse(formConfig));
-    if (parsedConfig.success) {
-      Object.assign(config, parsedConfig.data);
-    }
-  }
-
-  config.hasRules = (config.rules && config.rules.trim() !== "") || false;
-
-  return config;
-}*/
