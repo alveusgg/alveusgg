e19b639f8148fc7c8f00bdb40a1fe3751c04e8a6
Matt Cowley <me@mattcowley.co.uk>
2024-02-16 14:08:42 +0000

    Remove merch promo from tophat (#546)

--- apps/website/src/data/promo.ts
+++ apps/website/src/data/promo.ts
@@ -7,14 +7,6 @@ interface Promo {
   excluded: string[];
 }
 
-const promo: Promo | null = {
-  title: "Holiday Sweaters, 2024 Calendars, and more",
-  subtitle: "Available Now",
-  content:
-    "Grab yourself an embroidered Holiday sweater, a 2024 calendar featuring a selection of our ambassadors, or discover more Alveus apparel in the store.",
-  link: "/apparel",
-  external: true,
-  excluded: [],
-};
+const promo: Promo | null = null;
 
 export default promo;
