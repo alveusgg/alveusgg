ec323b0f2fb6c508cf18a63163dc2cf4143213a2
MattIPv4 <me@mattcowley.co.uk>
2024-02-21 05:04:35 +0000

    Use Grouped to render events

--- apps/website/src/pages/events/index.tsx
+++ apps/website/src/pages/events/index.tsx
@@ -1,16 +1,19 @@
 import { type NextPage } from "next";
 import Image from "next/image";
-import { type ReactNode } from "react";
+import { forwardRef, useMemo, type ReactNode } from "react";
+
+import useGrouped, { type GroupedItems, type Options } from "@/hooks/grouped";
 
 import { formatDateTime } from "@/utils/datetime";
-import { camelToKebab } from "@/utils/string-case";
 import { classes } from "@/utils/classes";
+import { convertToSlug } from "@/utils/slugs";
 
 import Section from "@/components/content/Section";
 import Heading from "@/components/content/Heading";
 import VideoPlayer from "@/components/content/Video";
 import Meta from "@/components/content/Meta";
 import Link from "@/components/content/Link";
+import Grouped, { type GroupedProps } from "@/components/content/Grouped";
 
 import valentines2024Video from "@/assets/events/valentines-2024.mp4";
 import fallCarnival2023Video from "@/assets/events/fall-carnival-2023.mp4";
@@ -34,8 +37,8 @@ type Event = {
   info: ReactNode;
 };
 
-const events = {
-  valentines2024: {
+const events: Event[] = [
+  {
     name: "Valentine's Day 2024",
     date: new Date("2024-02-14"),
     video: valentines2024Video,
@@ -82,7 +85,7 @@ const events = {
       </>
     ),
   },
-  fallCarnival2023: {
+  {
     name: "Fall Carnival 2023",
     date: new Date("2023-11-04"),
     video: fallCarnival2023Video,
@@ -134,7 +137,7 @@ const events = {
       </>
     ),
   },
-  summerCamp2023: {
+  {
     name: "Summer Camp 2023",
     date: new Date("2023-07-21"),
     video: summerCamp2023Video,
@@ -183,7 +186,7 @@ const events = {
       </>
     ),
   },
-  artAuction2023: {
+  {
     name: "Art Auction 2023",
     date: new Date("2023-04-22"),
     video: artAuction2023Video,
@@ -228,7 +231,7 @@ const events = {
       </>
     ),
   },
-  valentines2023: {
+  {
     name: "Valentine's Day 2023",
     date: new Date("2023-02-14"),
     video: valentines2023Video,
@@ -272,7 +275,7 @@ const events = {
       </>
     ),
   },
-  artAuction2022: {
+  {
     name: "Art Auction 2022",
     date: new Date("2022-04-22"),
     video: artAuction2022Video,
@@ -306,7 +309,7 @@ const events = {
       </p>
     ),
   },
-  halloween2021: {
+  {
     name: "Halloween 2021",
     date: new Date("2021-10-31"),
     video: halloween2021Video,
@@ -355,7 +358,7 @@ const events = {
       </>
     ),
   },
-  fundathon2021: {
+  {
     name: "Fund-a-thon 2021",
     date: new Date("2021-02-10"),
     video: fundathon2021Video,
@@ -391,9 +394,132 @@ const events = {
       </p>
     ),
   },
-} as const satisfies Record<string, Event>;
+];
+
+const sortByOptions = {
+  all: {
+    label: "All Events",
+    sort: (events) =>
+      [...events]
+        .sort((a, b) => b.date.getTime() - a.date.getTime())
+        .reduce<GroupedItems<Event>>((map, event) => {
+          const year = event.date.getUTCFullYear().toString();
+
+          map.set(year, {
+            name: year,
+            items: [...(map.get(year)?.items || []), event],
+          });
+          return map;
+        }, new Map()),
+  },
+} as const satisfies Options<Event>;
+
+const EventItems = forwardRef<HTMLDivElement, GroupedProps<Event>>(
+  ({ items, option, group, name, index }, ref) => {
+    const itemsWithSlugs = useMemo(
+      () =>
+        items.reduce(
+          (acc, item) => {
+            const slug = convertToSlug(item.name);
+            return { ...acc, [slug]: item };
+          },
+          {} as Record<string, Event>,
+        ),
+      [items],
+    );
+
+    return (
+      <>
+        {name && (
+          <Heading
+            level={-1}
+            className={classes(
+              "alveus-green-800 mb-6 mt-8 border-b-2 border-alveus-green-300/25 pb-2 text-4xl",
+              index === 0 && "sr-only",
+            )}
+            id={`${option}:${group}`}
+            link
+          >
+            {name}
+          </Heading>
+        )}
+        <div ref={ref}>
+          {Object.entries(itemsWithSlugs).map(([slug, event], idx, arr) => (
+            <div
+              key={slug}
+              className={classes(
+                "flex flex-wrap gap-y-8 pb-12 pt-8",
+                idx === 0 ? "lg:pb-16" : "lg:py-16",
+                idx !== arr.length - 1 &&
+                  "border-b-2 border-alveus-green-300/15",
+              )}
+            >
+              <div className="mx-auto flex basis-full flex-col px-8 lg:basis-1/2">
+                <Heading
+                  level={2}
+                  className="my-4 scroll-mt-8 text-center text-4xl"
+                  id={slug}
+                  link
+                  linkClassName="flex flex-wrap items-end justify-center gap-x-8 gap-y-2"
+                >
+                  {event.name}
+                  <small className="text-xl text-alveus-green-600">
+                    {formatDateTime(event.date, { style: "long" })}
+                  </small>
+                </Heading>
+
+                <div className="my-auto flex flex-wrap py-2">
+                  {Object.entries(event.stats).map(([key, stat]) => (
+                    <div
+                      key={key}
+                      className="mx-auto basis-full py-2 text-center sm:basis-1/2 lg:px-2"
+                    >
+                      <p className="text-3xl font-bold">{stat.stat}</p>
+                      <p className="text-xl text-alveus-green-700">
+                        {stat.title}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+
+              <div
+                className={classes(
+                  "mx-auto flex basis-full flex-col px-8 lg:basis-1/2",
+                  idx % 2 === 0 && "lg:order-first",
+                )}
+              >
+                <VideoPlayer
+                  className="my-auto aspect-video w-full rounded-xl"
+                  poster={event.video.poster}
+                  sources={event.video.sources}
+                  autoPlay
+                  loop
+                  muted
+                  playsInline
+                />
+              </div>
+
+              <div className="flex basis-full flex-col gap-3 px-8 text-lg text-gray-600">
+                {event.info}
+              </div>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  },
+);
+
+EventItems.displayName = "EventItems";
 
 const EventsPage: NextPage = () => {
+  const { option, group, result } = useGrouped({
+    items: events,
+    options: sortByOptions,
+    initial: "all",
+  });
+
   return (
     <>
       <Meta
@@ -442,67 +568,12 @@ const EventsPage: NextPage = () => {
         />
 
         <Section className="flex-grow">
-          {Object.entries(events).map(([key, event], idx, arr) => (
-            <div
-              key={key}
-              className={classes(
-                "flex flex-wrap gap-y-8 pb-12 pt-8",
-                idx === 0 ? "lg:pb-16" : "lg:py-16",
-                idx !== arr.length - 1 &&
-                  "border-b-2 border-alveus-green-300/25",
-              )}
-            >
-              <div className="mx-auto flex basis-full flex-col px-8 lg:basis-1/2">
-                <Heading
-                  level={2}
-                  className="my-4 scroll-mt-8 text-center text-4xl"
-                  id={camelToKebab(key).replace(/([a-z])([0-9])/gi, "$1-$2")}
-                  link
-                  linkClassName="flex flex-wrap items-end justify-center gap-x-8 gap-y-2"
-                >
-                  {event.name}
-                  <small className="text-xl text-alveus-green-600">
-                    {formatDateTime(event.date, { style: "long" })}
-                  </small>
-                </Heading>
-
-                <div className="my-auto flex flex-wrap py-2">
-                  {Object.entries(event.stats).map(([key, stat]) => (
-                    <div
-                      key={key}
-                      className="mx-auto basis-full py-2 text-center sm:basis-1/2 lg:px-2"
-                    >
-                      <p className="text-3xl font-bold">{stat.stat}</p>
-                      <p className="text-xl text-alveus-green-700">
-                        {stat.title}
-                      </p>
-                    </div>
-                  ))}
-                </div>
-              </div>
-
-              <div
-                className={classes(
-                  "mx-auto flex basis-full flex-col px-8 lg:basis-1/2",
-                  idx % 2 === 0 && "lg:order-first",
-                )}
-              >
-                <VideoPlayer
-                  className="my-auto aspect-video w-full rounded-xl"
-                  poster={event.video.poster}
-                  sources={event.video.sources}
-                  autoPlay
-                  loop
-                  muted
-                  playsInline
-                />
-              </div>
-
-              <div className="flex basis-full flex-col gap-3 px-8 text-lg text-gray-600">
-                {event.info}
-              </div>
-            </div>
-          ))}
+          <Grouped
+            option={option}
+            group={group}
+            result={result}
+            component={EventItems}
+          />
         </Section>
       </div>
     </>
