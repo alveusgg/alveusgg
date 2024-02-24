503a1e05d15e5ebee24e64b5e176c2559801660c
Paul Golmann <mail@pje-web.de>
2024-02-24 18:05:05 +0100

    cleanup vapid.test.ts

--- apps/website/__tests__/vapid.test.ts
+++ apps/website/__tests__/vapid.test.ts
@@ -3,7 +3,7 @@ import { getVapidAuthorizationString } from "@/server/web-push/vapid";
 import { decodeBase64UrlString } from "@/utils/base64url";
 import { env } from "@/env";
 
-vi.mock("@/env/index.js", () => {
+vi.mock("@/env", () => {
   return {
     env: {
       WEB_PUSH_VAPID_SUBJECT: "mailto:admin@alveus.gg",
