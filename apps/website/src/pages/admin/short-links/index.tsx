536bfe5a16edc63b6be1bd8d1dfb9d061e0afaa4
Jonas Franke <90050350+JonasFranke@users.noreply.github.com>
2024-02-24 22:06:55 +0100

    config -> data
    Co-authored-by: Paul Golmann <mail@pje-web.de>

--- apps/website/src/pages/admin/short-links/index.tsx
+++ apps/website/src/pages/admin/short-links/index.tsx
@@ -1,6 +1,6 @@
 import type { InferGetStaticPropsType, NextPage, NextPageContext } from "next";
 import { getAdminSSP } from "@/server/utils/admin";
-import { permissions } from "@/config/permissions";
+import { permissions } from "@/data/permissions";
 import Meta from "@/components/content/Meta";
 import { AdminPageLayout } from "@/components/admin/AdminPageLayout";
 import { ShortLink } from "@/components/admin/short-links/ShortLink";
