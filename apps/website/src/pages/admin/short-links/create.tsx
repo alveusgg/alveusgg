bc471d92acafc7c5a9cce1a8dc1cb7dc042f4362
Jonas Franke <90050350+JonasFranke@users.noreply.github.com>
2024-02-24 22:15:11 +0100

    Update apps/website/src/pages/admin/short-links/create.tsx
    Co-authored-by: Paul Golmann <mail@pje-web.de>

--- apps/website/src/pages/admin/short-links/create.tsx
+++ apps/website/src/pages/admin/short-links/create.tsx
@@ -1,7 +1,7 @@
 import type { InferGetStaticPropsType, NextPageContext, NextPage } from "next";
 
 import { getAdminSSP } from "@/server/utils/admin";
-import { permissions } from "@/config/permissions";
+import { permissions } from "@/data/permissions";
 
 import { AdminPageLayout } from "@/components/admin/AdminPageLayout";
 import Meta from "@/components/content/Meta";
