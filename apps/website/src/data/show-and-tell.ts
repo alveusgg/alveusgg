4ac4a0192aea3a3b13f686bc65b2c13dac3bfa98
Paul Golmann <mail@pje-web.de>
2024-02-25 19:48:24 +0100

    !!! do not deploy to production !!! enable give an hour early

--- apps/website/src/data/show-and-tell.ts
+++ apps/website/src/data/show-and-tell.ts
@@ -1,5 +1,5 @@
 export const MAX_IMAGES = 16;
 export const MAX_VIDEOS = 6;
 
-export const giveAnHourStart = "2024-03-01T00:00:00";
+export const giveAnHourStart = "2024-02-01T00:00:00";
 export const giveAnHourEnd = "2024-03-22T23:59:59";
