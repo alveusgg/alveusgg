d7ebe0370c2af65859d3d385c72cfbb3706120a6
Loganrx_ <loganrx@proton.me>
2024-03-02 14:19:04 -0500

    New found animal guides, Cat, Squirrel, and Raccoon

--- apps/website/src/data/found-animal.ts
+++ apps/website/src/data/found-animal.ts
@@ -242,6 +242,150 @@ const data: FoundAnimalFlow = {
         ],
       },
     },
+    {
+      name: "Squirrel",
+      flow: {
+        prompt: [
+          "Does any of the following apply to the squirrel?",
+          "- It is bleeding, has an open wound, or has a broken bone",
+          "- It has been in a cat's or dog's mouth",
+          "- It is covered in fly eggs (looks like small grains of rice)",
+          "- It is cold, wet, or crying nonstop",
+        ],
+        options: [
+          {
+            name: "Yes",
+            flow: {
+              prompt: macros.general.rehab("the squirrel"),
+            },
+          },
+          {
+            name: "No",
+            flow: {
+              prompt: [
+                "If the squirrel is uninjured the next step is to identify its age to determine if intervention is needed",
+                "Does any of the following apply to the squirrel",
+                "- have a fluffed out tail",
+                "- a body longer than 6 inches (not including the tail)",
+                "- approach humans or pets",
+              ],
+              options: [
+                {
+                  name: "Yes",
+                  flow: {
+                    prompt: [
+                      "This is a juvenile squirrel, you do not need to intervene.",
+                      ...macros.general.leave("the squirrel"),
+                    ],
+                  },
+                },
+                {
+                  name: "No",
+                  flow: {
+                    prompt: [
+                      "This is an infant squirrel, You will need to guide the baby back to its mother",
+                      "Place uncooked rice or bird seed in a sock and warm in the microwave for 20-30 seconds, Wrap the sock in a soft towel, and place it with the baby squirrel in an open container (e.g. a box)",
+                      "Do NOT give the baby food or water",
+                      "Return the squirrel to its nesting tree (usually in the immediate area where the squirrel was found) If you dont know which tree the squirrels nest is in, or if the nest  was destroyed. Then choose a tree closest to where the squirrel was found",
+                      "Observe the baby for the next 6 to 8 hours, Reheat the rice every two hours. If the mother does return after 24 hours, you can...",
+                      ...macros.general.rehab("the squirrel"),
+                    ],
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+    {
+      name: "Raccoon",
+      flow: {
+        prompt: [
+          "Does the raccoon appear to be sick or injured?",
+        ],
+        options: [
+          {
+            name: "Yes",
+            flow: {
+              prompt: [
+                "Is the raccoon an adult or a baby?",
+              ],
+              options: [
+                {
+                  name: "Adult",
+                  flow: {
+                    prompt: [
+                      "If you have found an injured adult raccoon, although it may be hard to accept. Adult raccoons can almost never be caught and successfully treated. If you see an adult raccoon with an injury, leave the animal alone. Even though the injury may take a long time to heal, this is far preferable to the trauma of chase and capture.",
+                    ],
+                  },
+                },
+                {
+                  name: "Baby",
+                  flow: {
+                    prompt: macros.general.rehab("the baby raccoon"),
+                  },
+                },
+              ],
+            },
+          },
+          {
+            name: "No",
+            flow: {
+              prompt: [
+                "Have you found a baby raccoon that's alone with no mother in sight?",
+              ],
+              options: [
+                {
+                  name: "Yes",
+                  flow: {
+                    prompt: [
+                      "Be very careful not create a orphan raccoon on accident. When a baby raccoon is separated from its mother, the baby will stay where it is until the mother returns. It's best to observe the baby without disturbing it.",
+                      "If the mother does return after 24 hours you can...",
+                      ...macros.general.rehab("the baby raccoon"),
+                    ]
+                  },
+                },
+                {
+                  name: "No",
+                  flow: {
+                    prompt: macros.general.leave("the raccoon"),
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+    {
+      name: "Cat",
+      flow: {
+        prompt: [
+          "Does the cat appear to be sick, injured, in danger, or a nursing kitten with no mama in sight?",
+        ],
+        options: [
+          {
+            name: "Yes",
+            flow: {
+              prompt: [
+                "If the cat appears to be sick, injured, in danger, or if you've found a nursing kitten with no mama in sight please contact an animal welfare organization or shelter that can help you assess the situation",
+              ],
+            },
+          },
+          {
+            name: "No",
+            flow: {
+              prompt: [
+                "Many cats that people spot in neighborhoods aren't in need of immediate help. Instead, they are pets who spend some or all of their time outdoors.",
+                "Check for a collar, if the cat has one try and get in contact with the owner, If the cat has no collar you can take the cat to the nearest animal shelter to check for a microchip",
+                "If the cat does not have a microchip, assess whether the cat seems healthy (not injured or visibly ill) and safe (in a secure location, away from any danger) We want a healthy and safe cat to stay where it is while you attempt to find the owner",
+              ],
+            },
+          },
+        ],
+      },
+    },
   ],
 } as const;
 
