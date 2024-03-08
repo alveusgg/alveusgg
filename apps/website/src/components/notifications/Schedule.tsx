1b0464498a8f94a327d43944ce36aab2e820f68c
MattIPv4 <me@mattcowley.co.uk>
2024-03-08 04:04:48 +0000

    Use semantic table markup for calendar

--- apps/website/src/components/notifications/Schedule.tsx
+++ apps/website/src/components/notifications/Schedule.tsx
@@ -42,14 +42,14 @@ function Day({
   className?: string;
 }) {
   return (
-    <div
+    <td
       className={classes(
         "flex min-h-8 flex-col gap-1 border border-alveus-green-900 p-1 md:min-h-24",
         className,
       )}
     >
       {children}
-    </div>
+    </td>
   );
 }
 
@@ -92,8 +92,8 @@ export function Schedule() {
   if (!events.data) return <p>No schedule is available currently.</p>;
 
   const startDay = 1; // 1 = Monday, 0 = Sunday
-  const startOffset = startOfMonth.getDay() - startDay;
-  const endOffset = (7 - ((startOffset + daysInMonth) % 7)) % 7;
+  const startOffset = (7 + startOfMonth.getDay() - startDay) % 7;
+  const weeks = Math.ceil((startOffset + daysInMonth) / 7);
 
   return (
     <>
@@ -104,88 +104,99 @@ export function Schedule() {
         <p className="text-2xl font-medium">{startOfMonth.getFullYear()}</p>
       </div>
 
-      <div className="mt-2 grid grid-cols-1 overflow-hidden rounded-md border border-alveus-green-900 bg-alveus-green-100 shadow-lg md:mt-8 md:grid-cols-7">
-        {/* Render the days of the week for desktop */}
-        {Array.from({ length: 7 }, (_, i) => (
-          <div
-            key={i}
-            className="hidden border border-alveus-green-900 bg-alveus-green-900 px-2 py-1 text-center font-bold uppercase text-alveus-tan md:block"
-          >
-            {days[(i + startDay) % 7]}
-          </div>
-        ))}
-
-        {/* Render empty cells for days in the previous month */}
-        {Array.from({ length: startOffset }, (_, i) => (
-          <Day key={i} className="hidden bg-alveus-green-200 md:block" />
-        ))}
-
-        {/* Render a cell for each day in this month */}
-        {Array.from({ length: daysInMonth }, (_, i) => {
-          const date = i + 1;
-          const fullDate = new Date(
-            startOfMonth.getFullYear(),
-            startOfMonth.getMonth(),
-            date,
-          );
-          const day = fullDate.getDay();
-          const events =
-            byDay[fullDate.toISOString().split("T")[0] as string] || [];
-
-          return (
-            <Day
-              key={date}
-              className={classes(
-                // On mobile, we'll position absolute the date + day of week
-                "relative pr-10 md:pr-1",
-                // On mobile, make the weekends have a darker background
-                (day === 0 || day === 6) &&
-                  "bg-alveus-green-400 md:bg-transparent",
-              )}
-            >
-              <p
-                className={classes(
-                  "absolute right-1 top-1 mb-auto flex justify-end gap-1 font-mono text-sm leading-none md:relative",
-                  fullDate.getTime() < today.getTime() && "opacity-50",
-                )}
+      <table className="mt-2 grid grid-cols-1 overflow-hidden rounded-md border border-alveus-green-900 bg-alveus-green-100 shadow-lg md:mt-8 md:grid-cols-7">
+        <thead className="contents">
+          <tr className="contents">
+            {/* Render the days of the week for desktop */}
+            {Array.from({ length: 7 }, (_, i) => (
+              <th
+                key={i}
+                className="hidden border border-alveus-green-900 bg-alveus-green-900 px-2 py-1 text-center font-bold uppercase text-alveus-tan md:block"
               >
-                {date.toLocaleString("en-US", { minimumIntegerDigits: 2 })}
+                {days[(i + startDay) % 7]}
+              </th>
+            ))}
+          </tr>
+        </thead>
 
-                {/* Render the day of the week for mobile */}
-                <span className="md:hidden">
-                  {days[fullDate.getDay()]?.slice(0, 1)}
-                </span>
-              </p>
+        <tbody className="contents">
+          {Array.from({ length: weeks }, (_, week) => (
+            <tr key={week} className="contents">
+              {Array.from({ length: 7 }, (_, i) => {
+                const date = i + 1 + week * 7 - startOffset;
 
-              {events.map((event) => (
-                <Link
-                  key={event.id}
-                  className={classes(
-                    getColor(event.link),
-                    "mb-auto block font-medium leading-none transition-colors text-stroke text-stroke-black text-stroke-4 paint-sfm",
-                    fullDate.getTime() < today.getTime() && "opacity-50",
-                  )}
-                  href={event.link}
-                >
-                  <p>{event.title}</p>
-                  <p className="text-sm tabular-nums">
-                    {event.startAt.toLocaleTimeString("en-US", {
-                      hour: "numeric",
-                      minute: "2-digit",
-                      timeZoneName: "short",
-                    })}
-                  </p>
-                </Link>
-              ))}
-            </Day>
-          );
-        })}
+                // Render empty cells for days outside of the month
+                if (date < 1 || date > daysInMonth)
+                  return (
+                    <Day
+                      key={date}
+                      className="hidden bg-alveus-green-200 md:block"
+                    />
+                  );
 
-        {/* Render empty cells for days in the next month */}
-        {Array.from({ length: endOffset }, (_, i) => (
-          <Day key={i} className="hidden bg-alveus-green-200 md:block" />
-        ))}
-      </div>
+                const fullDate = new Date(
+                  startOfMonth.getFullYear(),
+                  startOfMonth.getMonth(),
+                  date,
+                );
+                const day = fullDate.getDay();
+                const events =
+                  byDay[fullDate.toISOString().split("T")[0] as string] || [];
+
+                return (
+                  <Day
+                    key={date}
+                    className={classes(
+                      // On mobile, we'll position absolute the date + day of week
+                      "relative pr-10 md:pr-1",
+                      // On mobile, make the weekends have a darker background
+                      (day === 0 || day === 6) &&
+                        "bg-alveus-green-400 md:bg-transparent",
+                    )}
+                  >
+                    <p
+                      className={classes(
+                        "absolute right-1 top-1 mb-auto flex justify-end gap-1 font-mono text-sm leading-none md:relative",
+                        fullDate.getTime() < today.getTime() && "opacity-50",
+                      )}
+                    >
+                      {date.toLocaleString("en-US", {
+                        minimumIntegerDigits: 2,
+                      })}
+
+                      {/* Render the day of the week for mobile */}
+                      <span className="md:hidden">
+                        {days[fullDate.getDay()]?.slice(0, 1)}
+                      </span>
+                    </p>
+
+                    {events.map((event) => (
+                      <Link
+                        key={event.id}
+                        className={classes(
+                          getColor(event.link),
+                          "mb-auto block font-medium leading-none transition-colors text-stroke text-stroke-black text-stroke-4 paint-sfm",
+                          fullDate.getTime() < today.getTime() && "opacity-50",
+                        )}
+                        href={event.link}
+                      >
+                        <p>{event.title}</p>
+                        <p className="text-sm tabular-nums">
+                          {event.startAt.toLocaleTimeString("en-US", {
+                            hour: "numeric",
+                            minute: "2-digit",
+                            timeZoneName: "short",
+                          })}
+                        </p>
+                      </Link>
+                    ))}
+                  </Day>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
