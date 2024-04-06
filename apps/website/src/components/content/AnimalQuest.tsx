90d07c85fe3b6f1c1b72a47e4cfff6ec65bd8713
MattIPv4 <me@mattcowley.co.uk>
2024-04-06 17:40:48 +0100

    Adjust homepage to handle dark mode

--- apps/website/src/components/content/AnimalQuest.tsx
+++ apps/website/src/components/content/AnimalQuest.tsx
@@ -34,7 +34,7 @@ const AnimalQuest = ({
   <Link
     href={`/animal-quest/${sentenceToKebab(episode.edition)}`}
     className={classes(
-      "group relative z-0 flex flex-wrap items-center justify-between gap-x-8 gap-y-2 rounded-2xl bg-alveus-tan px-6 py-4 shadow-xl transition hover:scale-102 hover:shadow-2xl sm:flex-nowrap md:flex-wrap xl:flex-nowrap",
+      "group relative z-0 flex flex-wrap items-center justify-between gap-x-8 gap-y-2 rounded-2xl bg-alveus-tan px-6 py-4 shadow-xl transition hover:scale-102 hover:shadow-2xl sm:flex-nowrap md:flex-wrap xl:flex-nowrap dark:bg-alveus-green",
       className,
     )}
     custom
@@ -43,21 +43,18 @@ const AnimalQuest = ({
       src={animalQuestImage}
       alt=""
       width={688}
-      className="absolute inset-0 -z-10 h-full w-full rounded-2xl bg-alveus-tan object-cover opacity-10"
+      className="absolute inset-0 -z-10 h-full w-full rounded-2xl bg-alveus-tan object-cover opacity-10 dark:bg-alveus-green"
     />
 
-    <div>
-      <Heading
-        level={heading}
-        className="text-alveus-green-900 transition-colors group-hover:text-alveus-green-800"
-      >
+    <div className="text-alveus-green-900 transition-colors group-hover:text-alveus-green-800 dark:text-alveus-tan dark:group-hover:text-alveus-green-200">
+      <Heading level={heading}>
         <span className="inline-block">
           <span className="inline-block">Animal Quest</span>{" "}
           <span className="inline-block">#{episode.episode}:</span>
         </span>{" "}
         <span className="inline-block">{episode.edition}</span>
       </Heading>
-      <p className="text-balance text-xl text-alveus-green-800">
+      <p className="text-balance text-xl opacity-90">
         <span className="inline-block">
           Learn more {relation === "featured" && `about ${ambassador.name} `}
           on
@@ -68,7 +65,7 @@ const AnimalQuest = ({
 
     <IconYouTube
       size={48}
-      className="shrink-0 text-alveus-green-900 transition-colors group-hover:text-alveus-green-600"
+      className="shrink-0 text-alveus-green-900 transition-colors group-hover:text-alveus-green-600 dark:text-alveus-tan dark:group-hover:text-alveus-green-400"
     />
   </Link>
 );
