e62606fbbcf6bb1be698d2f4408a6b7339c44f3c
Matt Cowley <me@mattcowley.co.uk>
2024-04-06 23:38:56 +0100

    Add creator selector to collaborations page (#645)
    * Add creators to collaborations
    
    * Add creator selector to collaborations page
    
    * Smooth scroll on creator click
    
    * Clean up UI
    
    * Use PNG consistently, not JPG

--- apps/website/src/data/collaborations.ts
+++ apps/website/src/data/collaborations.ts
@@ -1,4 +1,43 @@
+import { type StaticImageData } from "next/image";
+
 import { convertToSlug } from "@/utils/slugs";
+
+import jackManifold from "@/assets/collaborations/jack-manifold.png";
+import emilyWang from "@/assets/collaborations/emily-wang.png";
+import jinny from "@/assets/collaborations/jinny.png";
+import jaidenAnimations from "@/assets/collaborations/jaiden-animations.png";
+import alpharad from "@/assets/collaborations/alpharad.png";
+import cinna from "@/assets/collaborations/cinna.png";
+import hasanAbi from "@/assets/collaborations/hasan-abi.png";
+import fanfan from "@/assets/collaborations/fanfan.png";
+import alluux from "@/assets/collaborations/alluux.png";
+import filian from "@/assets/collaborations/filian.png";
+import trihex from "@/assets/collaborations/trihex.png";
+import esfandtv from "@/assets/collaborations/esfandtv.png";
+import extraEmily from "@/assets/collaborations/extra-emily.png";
+import zoil from "@/assets/collaborations/zoil.png";
+import peachJars from "@/assets/collaborations/peach-jars.png";
+import jessicaNigri from "@/assets/collaborations/jessica-nigri.png";
+import squeex from "@/assets/collaborations/squeex.png";
+import carolineKwan from "@/assets/collaborations/caroline-kwan.png";
+import lacari from "@/assets/collaborations/lacari.png";
+import dailyDose from "@/assets/collaborations/daily-dose.png";
+import graycen from "@/assets/collaborations/graycen.png";
+import dareon from "@/assets/collaborations/dareon.png";
+import yungJeff from "@/assets/collaborations/yung-jeff.png";
+import pointCrow from "@/assets/collaborations/point-crow.png";
+import russel from "@/assets/collaborations/russel.png";
+import ludwig from "@/assets/collaborations/ludwig.png";
+import alinity from "@/assets/collaborations/alinity.png";
+import connorEatsPants from "@/assets/collaborations/connor-eats-pants.png";
+import botzSisters from "@/assets/collaborations/botez-sisters.png";
+import knut from "@/assets/collaborations/knut.png";
+import moistCr1TiKaL from "@/assets/collaborations/moist-cr1tikal.png";
+
+export type Creator = {
+  name: string;
+  image: StaticImageData;
+};
 
 export type Collaboration = {
   name: string;
@@ -7,6 +46,7 @@ export type Collaboration = {
   date: Date;
   videoId: string;
   vodId?: string;
+  creators: Creator[];
 };
 
 const collaborations: Collaboration[] = (
@@ -17,12 +57,24 @@ const collaborations: Collaboration[] = (
       date: new Date("2024-03-25"),
       videoId: "GfAxJsrymEo",
       vodId: "3lnIyCsyipE",
+      creators: [
+        {
+          name: "Jack Manifold",
+          image: jackManifold,
+        },
+      ],
     },
     {
       name: "Emily Wang",
       link: "https://www.twitch.tv/emilyywang",
       date: new Date("2024-02-13"),
       videoId: "CE-mNI_5eEI",
+      creators: [
+        {
+          name: "Emily Wang",
+          image: emilyWang,
+        },
+      ],
     },
     {
       name: "Jinny",
@@ -30,6 +82,12 @@ const collaborations: Collaboration[] = (
       date: new Date("2024-02-08"),
       videoId: "lpXtuG87BUM",
       vodId: "c-EoBOEr1_g",
+      creators: [
+        {
+          name: "Jinny",
+          image: jinny,
+        },
+      ],
     },
     {
       name: "JaidenAnimations & Alpharad",
@@ -37,6 +95,16 @@ const collaborations: Collaboration[] = (
       date: new Date("2024-02-02"),
       videoId: "W74Ub2HR1Y4",
       vodId: "U2HqCEr5_e4",
+      creators: [
+        {
+          name: "JaidenAnimations",
+          image: jaidenAnimations,
+        },
+        {
+          name: "Alpharad",
+          image: alpharad,
+        },
+      ],
     },
     {
       name: "Cinna",
@@ -44,6 +112,12 @@ const collaborations: Collaboration[] = (
       date: new Date("2024-01-28"),
       videoId: "5YRrm5mDb6o",
       vodId: "URSbZwAqLNE",
+      creators: [
+        {
+          name: "Cinna",
+          image: cinna,
+        },
+      ],
     },
     {
       name: "Hasan",
@@ -51,6 +125,12 @@ const collaborations: Collaboration[] = (
       date: new Date("2024-01-26"),
       videoId: "E_iO1ZKlHyM",
       vodId: "MnPhxGBoY-I",
+      creators: [
+        {
+          name: "HasanAbi",
+          image: hasanAbi,
+        },
+      ],
     },
     {
       name: "Fanfan",
@@ -58,6 +138,12 @@ const collaborations: Collaboration[] = (
       date: new Date("2023-11-13"),
       videoId: "qHcy_sBJXmU",
       vodId: "OCf7356WlNo",
+      creators: [
+        {
+          name: "Fanfan",
+          image: fanfan,
+        },
+      ],
     },
     {
       name: "Alluux",
@@ -65,12 +151,24 @@ const collaborations: Collaboration[] = (
       date: new Date("2023-11-05"),
       videoId: "L5r1isGll2I",
       vodId: "x1KMHDgmsR0",
+      creators: [
+        {
+          name: "Alluux",
+          image: alluux,
+        },
+      ],
     },
     {
       name: "Filian",
       link: "https://www.twitch.tv/filian",
       date: new Date("2023-10-11"),
       videoId: "WaQcdQBrz0I",
+      creators: [
+        {
+          name: "Filian",
+          image: filian,
+        },
+      ],
     },
     {
       name: "Trihex",
@@ -78,6 +176,12 @@ const collaborations: Collaboration[] = (
       date: new Date("2023-09-21"),
       videoId: "lxks3skYeO4",
       vodId: "H0gaJrLY5X0",
+      creators: [
+        {
+          name: "Trihex",
+          image: trihex,
+        },
+      ],
     },
     {
       name: "Esfand",
@@ -85,6 +189,12 @@ const collaborations: Collaboration[] = (
       date: new Date("2023-08-26"),
       videoId: "9aXebCBRonA",
       vodId: "o7nyVU62rf8",
+      creators: [
+        {
+          name: "EsfandTV",
+          image: esfandtv,
+        },
+      ],
     },
     {
       name: "ExtraEmily",
@@ -92,6 +202,12 @@ const collaborations: Collaboration[] = (
       date: new Date("2023-06-22"),
       videoId: "maJTBSkgA4Y",
       vodId: "qbxV8xgA7Vg",
+      creators: [
+        {
+          name: "ExtraEmily",
+          image: extraEmily,
+        },
+      ],
     },
     {
       name: "Zoil",
@@ -99,6 +215,12 @@ const collaborations: Collaboration[] = (
       date: new Date("2023-06-13"),
       videoId: "8M_qtYuSrys",
       vodId: "n4KpVaUmrSE",
+      creators: [
+        {
+          name: "Zoil",
+          image: zoil,
+        },
+      ],
     },
     {
       name: "PeachJars and Jessica Nigri",
@@ -106,6 +228,16 @@ const collaborations: Collaboration[] = (
       date: new Date("2023-05-21"),
       videoId: "-HuS6wbpqzQ",
       vodId: "KF4HIypXiyc",
+      creators: [
+        {
+          name: "PeachJars",
+          image: peachJars,
+        },
+        {
+          name: "Jessica Nigri",
+          image: jessicaNigri,
+        },
+      ],
     },
     {
       name: "Squeex",
@@ -113,6 +245,12 @@ const collaborations: Collaboration[] = (
       date: new Date("2023-05-19"),
       videoId: "oUJxCDGQDlY",
       vodId: "dCV8b8LcG-0",
+      creators: [
+        {
+          name: "Squeex",
+          image: squeex,
+        },
+      ],
     },
     {
       name: "Caroline Kwan",
@@ -120,6 +258,12 @@ const collaborations: Collaboration[] = (
       date: new Date("2023-05-09"),
       videoId: "H5nV79y-Prg",
       vodId: "4MmCFJadSg8",
+      creators: [
+        {
+          name: "Caroline Kwan",
+          image: carolineKwan,
+        },
+      ],
     },
     {
       name: "Lacari",
@@ -127,6 +271,12 @@ const collaborations: Collaboration[] = (
       date: new Date("2023-05-02"),
       videoId: "IssAfvy_bmo",
       vodId: "mVOypbE5YNs",
+      creators: [
+        {
+          name: "Lacari",
+          image: lacari,
+        },
+      ],
     },
     {
       name: "Daily Dose",
@@ -134,12 +284,24 @@ const collaborations: Collaboration[] = (
       date: new Date("2023-04-25"),
       videoId: "M9BBwvR3i-E",
       vodId: "j8GxD4adNyk",
+      creators: [
+        {
+          name: "Daily Dose",
+          image: dailyDose,
+        },
+      ],
     },
     {
       name: "Graycen",
       link: "https://www.twitch.tv/graycen",
       date: new Date("2023-04-18"),
       videoId: "M3WBnThqRkg",
+      creators: [
+        {
+          name: "Graycen",
+          image: graycen,
+        },
+      ],
     },
     {
       name: "Dareon",
@@ -147,12 +309,24 @@ const collaborations: Collaboration[] = (
       date: new Date("2023-04-11"),
       videoId: "kqKnhu5zBIY",
       vodId: "RCzMJs9uDSI",
+      creators: [
+        {
+          name: "Dareon",
+          image: dareon,
+        },
+      ],
     },
     {
       name: "YungJeff",
       link: "https://twitter.com/YUNGJEFF",
       date: new Date("2023-04-04"),
       videoId: "BXOEWQZ8C_Y",
+      creators: [
+        {
+          name: "YungJeff",
+          image: yungJeff,
+        },
+      ],
     },
     {
       name: "PointCrow",
@@ -160,6 +334,12 @@ const collaborations: Collaboration[] = (
       date: new Date("2023-03-24"),
       videoId: "FhmkQkbV42U",
       vodId: "Qz1Iniho9-g",
+      creators: [
+        {
+          name: "PointCrow",
+          image: pointCrow,
+        },
+      ],
     },
     {
       name: "Russel",
@@ -167,6 +347,12 @@ const collaborations: Collaboration[] = (
       date: new Date("2023-03-20"),
       videoId: "Xizgus_phn4",
       vodId: "OUaYjkkLeFQ",
+      creators: [
+        {
+          name: "Russel",
+          image: russel,
+        },
+      ],
     },
     {
       name: "Ludwig",
@@ -174,6 +360,12 @@ const collaborations: Collaboration[] = (
       date: new Date("2023-02-25"),
       videoId: "po1jytjDu4E",
       vodId: "vuLMTU8QHAU",
+      creators: [
+        {
+          name: "Ludwig",
+          image: ludwig,
+        },
+      ],
     },
     {
       name: "Alinity",
@@ -181,6 +373,12 @@ const collaborations: Collaboration[] = (
       date: new Date("2023-02-09"),
       videoId: "bak3RqjCzE0",
       vodId: "XHTEs94Cf4s",
+      creators: [
+        {
+          name: "Alinity",
+          image: alinity,
+        },
+      ],
     },
     {
       name: "ConnorEatsPants",
@@ -188,18 +386,36 @@ const collaborations: Collaboration[] = (
       date: new Date("2023-01-25"),
       videoId: "nC8qlK3k96Q",
       vodId: "SMEyEfVlzlM",
+      creators: [
+        {
+          name: "ConnorEatsPants",
+          image: connorEatsPants,
+        },
+      ],
     },
     {
       name: "The Botez Sisters",
       link: "https://www.twitch.tv/botezlive",
       date: new Date("2022-08-30"),
       videoId: "QgvNy11kU6E",
+      creators: [
+        {
+          name: "The Botez Sisters",
+          image: botzSisters,
+        },
+      ],
     },
     {
       name: "Knut",
       link: "https://www.twitch.tv/knut",
       date: new Date("2022-08-09"),
       videoId: "lFhFx6kf2E4",
+      creators: [
+        {
+          name: "Knut",
+          image: knut,
+        },
+      ],
     },
     {
       name: "MoistCr1TiKaL",
@@ -207,14 +423,21 @@ const collaborations: Collaboration[] = (
       date: new Date("2022-04-30"),
       videoId: "pb7MR59s1Z0",
       vodId: "x-OPvwjGHEU",
+      creators: [
+        {
+          name: "MoistCr1TiKaL",
+          image: moistCr1TiKaL,
+        },
+      ],
     },
     {
       name: "Jack Manifold",
       link: "https://www.twitch.tv/jackmanifoldtv",
       date: new Date("2022-04-21"),
       videoId: "jzyxhnODe2g",
+      creators: [], // Jack has a more recent collaboration
     },
-  ] as const
+  ] as const satisfies Omit<Collaboration, "slug">[]
 )
   .toSorted((a, b) => b.date.getTime() - a.date.getTime())
   .reduce(
--- apps/website/src/pages/collaborations.tsx
+++ apps/website/src/pages/collaborations.tsx
@@ -2,12 +2,16 @@ import { forwardRef, useEffect, useState } from "react";
 import { type NextPage } from "next";
 import Image from "next/image";
 
+import { useRouter } from "next/router";
 import useGrouped, { type GroupedItems, type Options } from "@/hooks/grouped";
 
 import { formatDateTime } from "@/utils/datetime";
 import { classes } from "@/utils/classes";
 
-import collaborations, { type Collaboration } from "@/data/collaborations";
+import collaborations, {
+  type Creator,
+  type Collaboration,
+} from "@/data/collaborations";
 
 import Section from "@/components/content/Section";
 import Heading from "@/components/content/Heading";
@@ -20,6 +24,20 @@ import leafRightImage1 from "@/assets/floral/leaf-right-1.png";
 import leafRightImage2 from "@/assets/floral/leaf-right-2.png";
 import leafLeftImage3 from "@/assets/floral/leaf-left-3.png";
 import leafLeftImage1 from "@/assets/floral/leaf-left-1.png";
+
+type CreatorWithSlug = Creator & { slug: string };
+
+const creators = collaborations.reduce(
+  (acc, { slug, creators }) => [
+    ...acc,
+    ...creators.map(({ name, image }) => ({
+      name,
+      image,
+      slug,
+    })),
+  ],
+  [] as CreatorWithSlug[],
+);
 
 const sortByOptions = {
   all: {
@@ -162,12 +180,12 @@ const CollaborationsPage: NextPage = () => {
         <Image
           src={leafRightImage1}
           alt=""
-          className="pointer-events-none absolute -top-8 right-0 z-10 hidden h-auto w-1/2 max-w-sm select-none lg:block"
+          className="pointer-events-none absolute -bottom-4 right-0 z-10 hidden h-auto w-1/2 max-w-xs select-none lg:block xl:-bottom-8"
         />
         <Image
           src={leafLeftImage3}
           alt=""
-          className="pointer-events-none absolute -bottom-20 left-0 z-10 hidden h-auto w-1/2 max-w-[12rem] select-none lg:block"
+          className="pointer-events-none absolute -bottom-16 left-0 z-10 hidden h-auto w-1/2 max-w-[12rem] select-none lg:block"
         />
 
         <Section dark className="py-24">
@@ -196,6 +214,47 @@ const CollaborationsPage: NextPage = () => {
         />
 
         <Section className="flex-grow">
+          <div className="flex justify-center">
+            <div className="relative isolate max-w-full">
+              <ul className="scrollbar-none group/creators -mt-6 flex max-w-full flex-row gap-y-4 overflow-x-auto pb-2 pl-12 pr-8 pt-6">
+                {creators.map(({ name, image, slug }, idx) => (
+                  <li key={slug} style={{ zIndex: creators.length - idx }}>
+                    <Link
+                      href={`#${slug}`}
+                      title={name}
+                      custom
+                      className="group/creator -ml-4 block rounded-full transition-all hover:-mt-4 hover:scale-105 hover:px-2 hover:pb-4"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        history.pushState(null, "", `#${slug}`);
+                        document.getElementById(slug)?.scrollIntoView({
+                          behavior: "smooth",
+                        });
+                      }}
+                    >
+                      <div className="h-16 w-16 rounded-full bg-alveus-green shadow-md ring-4 ring-alveus-tan transition-shadow group-hover/creator:shadow-lg">
+                        <Image
+                          src={image}
+                          alt=""
+                          className="h-full w-full rounded-full object-cover transition-all group-hover/creator:!brightness-105 group-hover/creator:contrast-115 group-hover/creator:!saturate-110 group-has-[:hover]/creators:brightness-75 group-has-[:hover]/creators:saturate-50"
+                        />
+                      </div>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+
+              <div
+                className="pointer-events-none absolute -top-6 bottom-0 left-0 w-10 bg-gradient-to-l from-transparent to-alveus-tan"
+                style={{ zIndex: creators.length + 1 }}
+              />
+              <div
+                className="pointer-events-none absolute -top-6 bottom-0 right-0 w-10 bg-gradient-to-r from-transparent to-alveus-tan"
+                style={{ zIndex: creators.length + 1 }}
+              />
+            </div>
+          </div>
+
           <Grouped
             option={option}
             group={group}
