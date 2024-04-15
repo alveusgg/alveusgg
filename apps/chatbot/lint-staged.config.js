3680964c471b520b96967a78eba53c0862f2a327
Jonas Franke <info@jonasfranke.xyz>
2024-04-15 15:18:28 +0000

    Use Biome's staged flag

--- apps/chatbot/lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-/** @param {string[]} filenames */
-const buildBiomeCommand = (filenames) => `pnpm biome check --apply --vcs-client-kind=git --vcs-enabled=true --vcs-use-ignore-file=true --vcs-default-branch=master --files-ignore-unknown=true ${filenames.join(" ")}`;
-
-const config = {
-  "*.{mjs,cjs,js,ts}": buildBiomeCommand,
-};
-
-export default config;
--- apps/website/lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-/** @param {string[]} filenames */
-const buildBiomeCommand = (filenames) => `pnpm biome check --apply --vcs-client-kind=git --vcs-enabled=true --vcs-use-ignore-file=true --vcs-default-branch=master --files-ignore-unknown=true ${filenames.join(" ")}`;
-
-const config = {
-  "*.{js,jsx,ts,tsx,cjs}": buildBiomeCommand,
-};
-
-export default config;
