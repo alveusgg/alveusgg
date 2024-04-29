94259a600b1fd20e644b506b5980e9c18b551eb3
Paul Golmann <mail@pje-web.de>
2024-04-29 21:34:17 +0200

    Update time when shortened show and tell text was actually deployed

--- apps/website/src/data/show-and-tell.ts
+++ apps/website/src/data/show-and-tell.ts
@@ -3,7 +3,7 @@ export const MAX_VIDEOS = 6;
 
 export const MAX_TEXT_HTML_LENGTH = 1_000; // We allow more total html length than characters (ignoring tags)
 
-const dateWhenTextShortened = new Date(2024, 3, 26);
+const dateWhenTextShortened = new Date(2024, 3, 27, 15, 19, 0, 0);
 
 export const getMaxTextLengthForCreatedAt = (createdAt?: Date) =>
   createdAt && createdAt < dateWhenTextShortened ? 700 : 300;
