04b75e039b72b15f6180f784892bc80a3412f58d
bl4nkfce <bl4nkfce@gmail.com>
2024-05-06 22:01:13 +0200

    fix: consent button z-index on md screens

--- apps/website/src/hooks/consent.tsx
+++ apps/website/src/hooks/consent.tsx
@@ -303,7 +303,7 @@ const ConsentDialog = ({ context }: { context: ConsentContext }) => {
 
       <button
         type="button"
-        className="group fixed bottom-4 left-4 flex h-12 w-12 items-center justify-center rounded-full bg-alveus-tan shadow transition-shadow hover:shadow-lg md:z-50"
+        className="group fixed bottom-4 left-4 flex h-12 w-12 items-center justify-center rounded-full bg-alveus-tan shadow transition-shadow hover:shadow-lg lg:z-50"
         onClick={() => setOpen(true)}
         title="Manage consent"
       >
