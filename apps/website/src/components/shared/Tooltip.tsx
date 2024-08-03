2cf4bf69109f6415521b770289191228e385cd62
ACA30 <me@nateb.ca>
2024-08-03 06:18:13 -0400

    Fix Prettier issue

--- apps/website/src/components/shared/Tooltip.tsx
+++ apps/website/src/components/shared/Tooltip.tsx
@@ -1,5 +1,5 @@
 import { Transition } from "@headlessui/react";
-import type { ReactNode} from "react";
+import type { ReactNode } from "react";
 import { Fragment, useState } from "react";
 
 export type TooltipProps = {
