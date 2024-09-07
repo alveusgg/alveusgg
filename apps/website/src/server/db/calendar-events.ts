0b9371d5a49b4eacfb6a9a4fe7b3546ab67f31fb
Paul Golmann <mail@pje-web.de>
2024-09-07 21:02:46 +0200

    Add start times to some regular events (#768)
    * cleanup code, remove async/await when not necessary
    
    * add start times to some regular events

--- apps/website/src/server/db/calendar-events.ts
+++ apps/website/src/server/db/calendar-events.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
-import { DateTime } from "luxon";
+import { type DateObjectUnits, DateTime } from "luxon";
+
 import { prisma } from "@/server/db/client";
 import { DATETIME_ALVEUS_ZONE } from "@/utils/datetime";
 
@@ -20,25 +21,25 @@ export const existingCalendarEventSchema = calendarEventSchema.and(
   }),
 );
 
-export async function createCalendarEvent(
+export function createCalendarEvent(
   input: z.infer<typeof calendarEventSchema>,
 ) {
-  return await prisma.calendarEvent.create({
+  return prisma.calendarEvent.create({
     data: input,
   });
 }
 
-export async function editCalendarEvent(
+export function editCalendarEvent(
   input: z.infer<typeof existingCalendarEventSchema>,
 ) {
   const { id, ...data } = input;
-  return await prisma.calendarEvent.update({
+  return prisma.calendarEvent.update({
     where: { id },
     data,
   });
 }
 
-export async function getCalendarEvents({
+export function getCalendarEvents({
   start,
   end,
 }: { start?: Date; end?: Date } = {}) {
@@ -49,7 +50,7 @@ export async function getCalendarEvents({
   const endAt =
     end ?? new Date(new Date(startAt).setMonth(startAt.getMonth() + 1));
 
-  return await prisma.calendarEvent.findMany({
+  return prisma.calendarEvent.findMany({
     where: {
       startAt: {
         gte: startAt,
@@ -60,60 +61,70 @@ export async function getCalendarEvents({
   });
 }
 
+type RegularEvent = Pick<
+  CalendarEventSchema,
+  "title" | "description" | "category" | "link"
+> & {
+  startTime?: Pick<DateObjectUnits, "hour" | "minute">;
+};
+
+const animalCareChats: RegularEvent = {
+  title: "Animal Care Chats",
+  description:
+    "Join animal care staff as they carry out their daily tasks and answer your questions.",
+  category: "Alveus Regular Stream",
+  link: "https://twitch.tv/AlveusSanctuary",
+  startTime: { hour: 14, minute: 30 },
+};
+
+export const eventsByWeekDay = [
+  // Monday
+  [animalCareChats],
+  // Tuesday
+  [animalCareChats],
+  // Wednesday
+  [
+    animalCareChats,
+    {
+      title: "WAI New Episode",
+      description: "Watch the latest Wine About It episode.",
+      category: "Maya YouTube Video",
+      link: "https://www.youtube.com/@WineAboutItPodcast",
+    },
+  ],
+  // Thursday
+  [
+    {
+      title: "Connor Stream",
+      description: "Join Connor as he gets up to his usual antics.",
+      category: "Alveus Regular Stream",
+      link: "https://twitch.tv/AlveusSanctuary",
+      startTime: { hour: 14, minute: 30 },
+    },
+    {
+      title: "WW New Episode",
+      description: "Watch the latest World's Wildest episode.",
+      category: "Maya YouTube Video",
+      link: "https://www.youtube.com/@worldswildestpodcast",
+    },
+  ],
+  // Friday
+  [animalCareChats],
+  // Saturday
+  [
+    {
+      title: "Nick Stream",
+      description: "Join Nick as he gets work done around the sanctuary.",
+      category: "Alveus Regular Stream",
+      link: "https://twitch.tv/AlveusSanctuary",
+      startTime: { hour: 14, minute: 30 },
+    },
+  ],
+  // Sunday
+  [],
+] as RegularEvent[][];
+
 export async function createRegularCalendarEvents(date: Date) {
-  const animalCareChats = {
-    title: "Animal Care Chats",
-    description:
-      "Join animal care staff as they carry out their daily tasks and answer your questions.",
-    category: "Alveus Regular Stream",
-    link: "https://twitch.tv/AlveusSanctuary",
-  };
-
-  const eventsByWeekDay = [
-    // Monday
-    [animalCareChats],
-    // Tuesday
-    [animalCareChats],
-    // Wednesday
-    [
-      animalCareChats,
-      {
-        title: "WAI New Episode",
-        description: "Watch the latest Wine About It episode.",
-        category: "Maya YouTube Video",
-        link: "https://www.youtube.com/@WineAboutItPodcast",
-      },
-    ],
-    // Thursday
-    [
-      {
-        title: "Connor Stream",
-        description: "Join Connor as he gets up to his usual antics.",
-        category: "Alveus Regular Stream",
-        link: "https://twitch.tv/AlveusSanctuary",
-      },
-      {
-        title: "WW New Episode",
-        description: "Watch the latest World's Wildest episode.",
-        category: "Maya YouTube Video",
-        link: "https://www.youtube.com/@worldswildestpodcast",
-      },
-    ],
-    // Friday
-    [animalCareChats],
-    // Saturday
-    [
-      {
-        title: "Nick Stream",
-        description: "Join Nick as he gets work done around the sanctuary.",
-        category: "Alveus Regular Stream",
-        link: "https://twitch.tv/AlveusSanctuary",
-      },
-    ],
-    // Sunday
-    [],
-  ] as Omit<CalendarEventSchema, "startAt" | "hasTime">[][];
-
   // Walk through each day of the next month and create any events
   const nextMonth = DateTime.fromJSDate(date)
     .plus({ months: 1 })
@@ -123,10 +134,20 @@ export async function createRegularCalendarEvents(date: Date) {
     const date = nextMonth.set({ day });
     const events = eventsByWeekDay[date.weekday - 1];
     for (const event of events || []) {
+      let hasTime = false;
+      let eventDate = date;
+      if (event.startTime !== undefined) {
+        hasTime = true;
+        eventDate = eventDate.set(event.startTime);
+      }
+
       await createCalendarEvent({
-        ...event,
-        startAt: date.toJSDate(),
-        hasTime: false,
+        title: event.title,
+        category: event.category,
+        description: event.description,
+        link: event.link,
+        startAt: eventDate.toJSDate(),
+        hasTime: hasTime,
       });
     }
   }
