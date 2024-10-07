5d9a31781a14d8af90d02dc76ae5ac1233fe31e1
alvaroMorillas <alvaromori95@gmail.com>
2024-10-07 21:30:39 +0200

    See post content on a modal when clicking a marker on the Community Map.

--- apps/website/src/components/show-and-tell/CommunityMap.tsx
+++ apps/website/src/components/show-and-tell/CommunityMap.tsx
@@ -7,18 +7,27 @@ import type {
 import "maplibre-gl/dist/maplibre-gl.css"; // Import the MapLibre CSS
 import IconArrowUp from "@/icons/IconArrowUp";
 import IconArrowDown from "@/icons/IconArrowDown";
-import config from "../../../tailwind.config";
+import { trpc } from "@/utils/trpc";
+import { ShowAndTellEntry } from "@/components/show-and-tell/ShowAndTellEntry";
 import Heading from "../content/Heading";
 import { Button } from "../shared/form/Button";
+import { ModalDialog } from "../shared/ModalDialog";
+import config from "../../../tailwind.config";
+import Section from "../content/Section";
 
 export const CommunityMap = () => {
   const [showMap, setShowMap] = useState(false);
   const [locations, setLocations] = useState<LocationResponse["features"]>([]);
+
+  // Some fast, easy to digest data
   const [uniqueLocationsCount, setUniqueLocationsCount] = useState(0);
   const [uniqueCountriesCount, setUniqueCountriesCount] = useState(0);
-  const [calculatingData, setCalculatingData] = useState(true);
+  const [isCalculatingData, setIsCalculatingData] = useState(true);
 
-  const MAX_ZOOM = 10;
+  // To show post info on marker click
+  const [selectedMarkerId, setSelectedMarkerId] = useState<string | null>(null);
+
+  const MAX_ZOOM = 10; // Totally arbitrary, I think this level of zoom looks good.
 
   useEffect(() => {
     const fetchLocations = async () => {
@@ -32,8 +41,6 @@ export const CommunityMap = () => {
         );
         setUniqueLocationsCount(uniqueLocations.size);
 
-        // FIXME: current locations are not generated with nominatim, so the last part of the location may not always be the country.
-        // Maybe check /apps/website/src/utils/countries.ts
         const uniqueCountries = new Set(
           data.features.map((l: LocationFeature) => {
             return l.properties.location
@@ -44,7 +51,7 @@ export const CommunityMap = () => {
         );
         setUniqueCountriesCount(uniqueCountries.size);
       }
-      setCalculatingData(false);
+      setIsCalculatingData(false);
     };
     fetchLocations();
   }, []);
@@ -77,9 +84,7 @@ export const CommunityMap = () => {
         ],
       },
       antialias: true,
-    });
-
-    map.on("zoom", () => {
+    }).on("zoom", () => {
       if (map.getZoom() > MAX_ZOOM) {
         map.setZoom(MAX_ZOOM);
       }
@@ -89,22 +94,18 @@ export const CommunityMap = () => {
     // .addControl(new FullscreenControl());
 
     locations.forEach((p: LocationFeature) => {
-      const postUrl = `/admin/show-and-tell/review/${p.properties.id}/preview`;
-      const popupHtml = `<strong>${p.properties.displayName ?? "A chatter"}</strong> was here!<br>Check their <a target="_blank" href="${postUrl}"><u>post</u></a>.`;
-
       // Marker on hover popout S&T entry post relevant info (entry.displayName from entry.location)
-      new Marker({
+      const marker = new Marker({
         color: config.theme.colors["alveus-green"].DEFAULT,
         draggable: false,
       })
         .setLngLat([p.geometry.coordinates[0], p.geometry.coordinates[1]])
-        .setPopup(
-          new Popup({
-            offset: 24,
-            className: "text-alveus-green flex", // FIXME: add alveus' classes
-          }).setHTML(popupHtml),
-        )
         .addTo(map);
+
+      marker.getElement().setAttribute("data-id", p.properties.id);
+      marker
+        .getElement()
+        .addEventListener("click", () => setSelectedMarkerId(p.properties.id));
     });
 
     // Clean up on component unmount
@@ -117,13 +118,20 @@ export const CommunityMap = () => {
     setShowMap(!showMap);
   };
 
+  const { data } = trpc.adminShowAndTell.getEntry.useQuery(
+    String(selectedMarkerId),
+    {
+      enabled: !!selectedMarkerId,
+    },
+  );
+
   return (
     <>
       <div className="w-full md:w-3/5">
         <Heading>Community Map</Heading>
         <p className="pb-4 text-lg">Reaching every part of the Globe!</p>
 
-        {!calculatingData &&
+        {!isCalculatingData &&
           uniqueLocationsCount > 0 &&
           uniqueCountriesCount > 0 && (
             <p className="pb-4">
@@ -138,11 +146,24 @@ export const CommunityMap = () => {
         {showMap ? <IconArrowUp /> : <IconArrowDown />}
       </Button>
       {showMap && (
-        <div className="h-[600px] w-full overflow-hidden rounded-lg">
+        <div className="h-[800px] w-full overflow-hidden rounded-lg">
           <div
             id="mapVisualizerContainer"
-            style={{ width: "100%", height: "600px" }}
+            style={{ width: "100%", height: "800px" }}
           />
+          <ModalDialog
+            title=""
+            panelClassName="max-w-fit"
+            closeLabel="Return to Community Map"
+            isOpen={!!selectedMarkerId}
+            closeModal={() => setSelectedMarkerId(null)}
+          >
+            <Section>
+              {data && (
+                <ShowAndTellEntry entry={data} isPresentationView={false} />
+              )}
+            </Section>
+          </ModalDialog>
         </div>
       )}
     </>
