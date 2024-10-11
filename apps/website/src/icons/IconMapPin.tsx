af33786a8ac077620b688ee130cbcf409afdd756
Paul Golmann <mail@pje-web.de>
2024-10-11 16:24:11 +0200

    fix svg attributes in jsx

--- apps/website/src/icons/IconMapPin.tsx
+++ apps/website/src/icons/IconMapPin.tsx
@@ -8,16 +8,16 @@ export default function IconMapPin(props: IconProps) {
       <path
         fill="none"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         strokeWidth={1.5}
         d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
       />
       <path
         fill="none"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         strokeWidth={1.5}
         d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
       />
