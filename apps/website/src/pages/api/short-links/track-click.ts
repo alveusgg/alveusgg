b0b3d8a37b981ed995ffed0820ff6d9193406423
Matt Cowley <me@mattcowley.co.uk>
2024-10-19 20:33:47 +0100

    Add Vercel track call to short links (#803)
    * Add Vercel track call to short links
    
    * Include headers in Vercel track call

--- apps/website/src/pages/api/short-links/track-click.ts
+++ apps/website/src/pages/api/short-links/track-click.ts
@@ -1,20 +1,28 @@
 import { z } from "zod";
+import { track } from "@vercel/analytics/server";
+
 import { prisma } from "@/server/db/client";
 import { createTokenProtectedApiHandler } from "@/server/utils/api";
 
-const trackClickSchema = z.object({ id: z.string().cuid() });
+const trackClickSchema = z.object({
+  id: z.string().cuid(),
+  slug: z.string(),
+  link: z.string(),
+  headers: z.record(z.union([z.string(), z.array(z.string())])),
+});
 export type TrackClickSchema = z.infer<typeof trackClickSchema>;
 
 //API for short links tracking
 export default createTokenProtectedApiHandler(
   trackClickSchema,
-  async (options) => {
+  async ({ id, slug, link, headers }) => {
     try {
       await prisma.shortLinksTracking.upsert({
-        where: { id: options.id },
-        create: { id: options.id, clicks: 1 },
+        where: { id },
+        create: { id, clicks: 1 },
         update: { clicks: { increment: 1 } },
       });
+      await track("Short link clicked", { slug, link }, { headers });
       return true;
     } catch (e) {
       console.error("Failed to track click", e);
