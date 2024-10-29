1004ed75c1e9d8908c3ec7fc29a1a1fcd429c236
MattIPv4 <me@mattcowley.co.uk>
2024-10-29 16:05:14 +0000

    Make collab event category specific to Alveus

--- apps/website/src/data/calendar-events.ts
+++ apps/website/src/data/calendar-events.ts
@@ -6,10 +6,13 @@ type StandardCategory = {
 export const standardCategories: StandardCategory[] = [
   { name: "Alveus Regular Stream", color: "bg-yellow-400 hover:bg-yellow-600" },
   { name: "Alveus Special Stream", color: "bg-green-300 hover:bg-green-500" },
+  {
+    name: "Alveus Collaboration Stream",
+    color: "bg-blue-300 hover:bg-blue-500",
+  },
   { name: "Alveus YouTube Video", color: "bg-red-300 hover:bg-red-500" },
   { name: "Maya Regular Stream", color: "bg-gray-200 hover:bg-gray-400" },
   { name: "Maya YouTube Video", color: "bg-red-200 hover:bg-red-400" },
-  { name: "Collaboration Stream", color: "bg-blue-300 hover:bg-blue-500" },
 ] as const;
 
 export const getStandardCategoryColor = (category: string) =>
