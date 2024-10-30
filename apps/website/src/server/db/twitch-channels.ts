7b27e60d8e077bf4c70e300ffccd53b928670bd3
MattIPv4 <me@mattcowley.co.uk>
2024-10-30 22:36:19 +0000

    Add background job for proactive refresh of channel auth

--- apps/website/src/server/db/twitch-channels.ts
+++ apps/website/src/server/db/twitch-channels.ts
@@ -1,4 +1,6 @@
+import { env } from "@/env";
 import { prisma } from "@/server/db/client";
+import { refreshAccessToken } from "@/server/utils/oauth2";
 
 export async function getTwitchChannels() {
   return prisma.twitchChannel.findMany({
@@ -49,4 +51,47 @@ export async function deleteTwitchChannel(channelId: string) {
   return prisma.twitchChannel.delete({
     where: { channelId },
   });
+}
+
+export async function refreshTwitchChannels() {
+  const channels = await getTwitchChannels();
+  const accounts = channels
+    .map((channel) => [channel.broadcasterAccount, channel.moderatorAccount])
+    .flat();
+
+  const seen = new Set<string>();
+  for (const account of accounts) {
+    // Handle a channel not having a linked account
+    if (!account) continue;
+
+    // Handle a tied account not having tokens for some reason
+    if (!account.access_token || !account.refresh_token) {
+      console.log(
+        `Skipping refresh for ${account.id} - missing access token or refresh token`,
+      );
+      continue;
+    }
+
+    // Handle the same account being tied to multiple channels
+    if (seen.has(account.id)) continue;
+    seen.add(account.id);
+
+    await refreshAccessToken(
+      "twitch",
+      env.TWITCH_CLIENT_ID,
+      env.TWITCH_CLIENT_SECRET,
+      account.id,
+      account.access_token,
+      account.refresh_token,
+      // Force the refresh if we're within 1hr of expiry
+      !!(
+        account.expires_at && account.expires_at - Date.now() / 1000 < 60 * 60
+      ),
+    ).catch((err) => {
+      console.error(
+        `Error refreshing access token for account ${account.id}:`,
+        err,
+      );
+    });
+  }
 }
