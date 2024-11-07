62cefb0cfc339d948213ce76fea3f96535b2e13d
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2024-11-07 14:53:44 -0600

    Add extractInfoFromMapFeatures

--- apps/website/src/utils/locations.ts
+++ apps/website/src/utils/locations.ts
@@ -10,3 +10,28 @@ export function extractCountriesFromLocations(locations: string[]) {
     .map(extractCountryFromLocationString)
     .filter((country) => !["", "OTHER"].includes(country));
 }
+
+export function extractInfoFromMapFeatures(
+  mapFeatures: { id: string; location: string }[],
+) {
+  const locations = new Set<string>();
+  const countries = new Set<string>();
+  const postsFromANewLocation = new Set<string>();
+  for (let i = 0; i < mapFeatures.length; i++) {
+    const { id, location } = mapFeatures[i]!;
+
+    locations.add(location);
+
+    const country = location
+      .substring(location.lastIndexOf(",") + 1)
+      .trim()
+      .toUpperCase();
+
+    if (country !== "" && country !== "OTHER" && !countries.has(country)) {
+      countries.add(country);
+      postsFromANewLocation.add(id);
+    }
+  }
+
+  return { locations, countries, postsFromANewLocation };
+}
