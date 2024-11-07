513e37230d0ae43a7cac0baa38a011291bca8ce4
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2024-11-07 14:56:08 -0600

    Remove other location utils
    extractInfoFromMapFeatures does it all in one loop

--- apps/website/src/utils/locations.ts
+++ apps/website/src/utils/locations.ts
@@ -1,16 +1,3 @@
-export function extractCountryFromLocationString(location: string) {
-  return location
-    ?.substring(location.lastIndexOf(",") + 1)
-    .trim()
-    .toUpperCase();
-}
-
-export function extractCountriesFromLocations(locations: string[]) {
-  return locations
-    .map(extractCountryFromLocationString)
-    .filter((country) => !["", "OTHER"].includes(country));
-}
-
 export function extractInfoFromMapFeatures(
   mapFeatures: { id: string; location: string }[],
 ) {
