668c204094c0fdd60c7bae6f9bdc3be4df201f7b
Paul Golmann <mail@pje-web.de>
2024-11-14 15:53:29 +0100

    Filter events by URL instead of category

--- apps/website/src/server/db/calendar-events.ts
+++ apps/website/src/server/db/calendar-events.ts
@@ -217,7 +217,11 @@ export async function syncTwitchSchedule() {
   const events = await getCalendarEvents({
     start: new Date(),
     hasTime: true,
-  }).then((events) => events.filter(isAlveusEvent));
+  }).then((events) =>
+    events.filter((event) =>
+      /https:\/\/(www\.)?twitch\.tv\/AlveusSanctuary/i.test(event.link),
+    ),
+  );
 
   // Get all the existing segments in the future from Twitch
   const segments = await getTwitchSchedule(
