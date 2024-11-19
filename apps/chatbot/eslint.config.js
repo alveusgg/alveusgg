65085ea30e1eaa4e71fb9ceb68a6cba66052445c
MattIPv4 <me@mattcowley.co.uk>
2024-11-19 01:40:34 +0000

    chatbot: update eslint config for v9

---
+++ apps/chatbot/eslint.config.js
@@ -0,0 +1,83 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettiereslint from "eslint-config-prettier";
+
+import importXPlugin from "eslint-plugin-import-x";
+
+export default tseslint.config(
+  {
+    name: "eslint/recommended",
+    ...eslint.configs.recommended,
+  },
+  ...tseslint.configs.recommended,
+  {
+    name: "typescript-eslint/tsconfig",
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+  },
+  importXPlugin.flatConfigs.recommended,
+  importXPlugin.flatConfigs.typescript,
+  {
+    name: "import-x/order",
+    rules: {
+      "import-x/order": [
+        "warn",
+        {
+          pathGroups: [
+            {
+              pattern: "@/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    name: "prettier/config",
+    ...prettiereslint,
+  },
+  {
+    name: "custom/rules",
+    rules: {
+      "no-empty": [
+        "error",
+        {
+          allowEmptyCatch: true,
+        },
+      ],
+      "@typescript-eslint/consistent-type-imports": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "warn", // or "error"
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  {
+    name: "custom/cjs",
+    files: ["**/*.cjs"],
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+    },
+    languageOptions: {
+      parserOptions: {
+        sourceType: "commonjs",
+      },
+    },
+  },
+);
