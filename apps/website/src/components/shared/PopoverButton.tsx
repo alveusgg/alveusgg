95d87e5c60913a15a11861cbe7d8d9d46fb17a29
alisondy <alison@alisondowdney.com>
2024-12-04 21:07:54 +0000

    Remove additional div from popover

--- apps/website/src/components/shared/PopoverButton.tsx
+++ apps/website/src/components/shared/PopoverButton.tsx
@@ -16,34 +16,32 @@ export function PopoverButton({
   onClick?: MouseEventHandler;
 }) {
   return (
-    <div className="relative">
-      <Popover>
-        {({ open }) => (
-          <>
-            <PopeoverButtonHeadless as={Fragment}>
-              <Button
-                width="auto"
-                size="small"
-                onClick={onClick}
-                className={
-                  open
-                    ? defaultButtonClasses
-                    : "bg-gray-700/10 hover:bg-gray-700/20"
-                }
-              >
-                {label}
-              </Button>
-            </PopeoverButtonHeadless>
-
-            <PopoverPanel
-              className="absolute right-0 z-20 mt-0.5 rounded bg-gray-800 p-2 text-white shadow-xl"
-              modal={false}
+    <Popover className="relative">
+      {({ open }) => (
+        <>
+          <PopeoverButtonHeadless as={Fragment}>
+            <Button
+              width="auto"
+              size="small"
+              onClick={onClick}
+              className={
+                open
+                  ? defaultButtonClasses
+                  : "bg-gray-700/10 hover:bg-gray-700/20"
+              }
             >
-              {children}
-            </PopoverPanel>
-          </>
-        )}
-      </Popover>
-    </div>
+              {label}
+            </Button>
+          </PopeoverButtonHeadless>
+
+          <PopoverPanel
+            className="absolute right-0 z-20 mt-0.5 rounded bg-gray-800 p-2 text-white shadow-xl"
+            modal={false}
+          >
+            {children}
+          </PopoverPanel>
+        </>
+      )}
+    </Popover>
   );
 }
