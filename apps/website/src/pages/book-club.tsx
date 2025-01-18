05674e83950f3bc84ab2cfa6ff59915e0df7af57
MattIPv4 <me@mattcowley.co.uk>
2025-01-18 21:11:17 +0000

    Clickable 3D book buttons

--- apps/website/src/pages/book-club.tsx
+++ apps/website/src/pages/book-club.tsx
@@ -1,8 +1,14 @@
 import { type NextPage } from "next";
 import Image, { type StaticImageData } from "next/image";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
 import type { PartialDateString } from "@alveusgg/data/src/types";
 
 import { formatPartialDateString } from "@/utils/datetime";
+import { classes } from "@/utils/classes";
 
 import Section from "@/components/content/Section";
 import Heading from "@/components/content/Heading";
@@ -15,12 +21,22 @@ import leafRightImage2 from "@/assets/floral/leaf-right-2.png";
 import leafLeftImage3 from "@/assets/floral/leaf-left-3.png";
 import leafLeftImage1 from "@/assets/floral/leaf-left-1.png";
 
+const thickness = {
+  xs: "h-6 -translate-z-6 -mb-6",
+  sm: "h-8 -translate-z-8 -mb-8",
+  md: "h-10 -translate-z-10 -mb-10",
+  lg: "h-12 -translate-z-12 -mb-12",
+  xl: "h-14 -translate-z-14 -mb-14",
+};
+
 type BookInfo = {
   title: string;
   author: string;
   image: StaticImageData;
   month: PartialDateString & `${number}-${number}`;
   link: string;
+  thickness: (typeof thickness)[keyof typeof thickness];
+  color: `border-${string}`;
 };
 
 const books: BookInfo[] = [
@@ -30,22 +46,65 @@ const books: BookInfo[] = [
     image: hIsForHawk,
     month: "2025-01",
     link: "https://amzn.to/4a2ByGQ",
+    thickness: thickness.xs,
+    color: "border-black",
+  },
+  {
+    title: "H is for Hawk",
+    author: "Helen Macdonald",
+    image: hIsForHawk,
+    month: "2025-01",
+    link: "https://amzn.to/4a2ByGQ",
+    thickness: thickness.md,
+    color: "border-black",
+  },
+  {
+    title: "H is for Hawk",
+    author: "Helen Macdonald",
+    image: hIsForHawk,
+    month: "2025-01",
+    link: "https://amzn.to/4a2ByGQ",
+    thickness: thickness.xl,
+    color: "border-black",
   },
 ];
 
-const Book = ({ title, author, image, month, link }: BookInfo) => (
-  <div className="w-64">
-    <Image
-      src={image}
-      alt=""
-      className="aspect-book h-auto w-full rounded-xl object-cover"
-    />
+const Book = ({
+  title,
+  author,
+  image,
+  month,
+  link,
+  thickness,
+  color,
+}: BookInfo) => (
+  <Disclosure as="div" className="w-64">
+    <DisclosureButton className="group overflow-visible perspective-500 focus:outline-none">
+      <div className="origin-[50%_40%] transition-all duration-1000 transform-style-3d group-data-[open]:mb-[-100%] group-data-[open]:-translate-y-1/4 group-data-[open]:translate-z-2 group-data-[open]:rotate-x-[85deg] group-data-[open]:scale3d-[0.70]">
+        <Image
+          src={image}
+          alt={`${title} by ${author}`}
+          className="aspect-book h-auto w-full rounded-xl object-cover transition-transform duration-1000 group-data-[open]:translate-x-0.5 group-data-[open]:-translate-z-0.5"
+        />
+        <div
+          className={classes(
+            "w-full origin-top rounded-l-xl rounded-r-sm border-4 border-r-0 border-solid bg-white transition-transform duration-1000 -translate-y-0.5 rotate-x-90 scale-x-95 group-data-[open]:scale-x-100",
+            thickness,
+            color,
+          )}
+        />
+      </div>
+    </DisclosureButton>
 
-    <Heading>{title}</Heading>
-    <p>{author}</p>
     <p>{formatPartialDateString(month)}</p>
-    <Link href={link}>Buy</Link>
-  </div>
+
+    <DisclosurePanel>
+      <Heading>{title}</Heading>
+      <p>{author}</p>
+
+      <Link href={link}>Buy</Link>
+    </DisclosurePanel>
+  </Disclosure>
 );
 
 const BookClubPage: NextPage = () => {
@@ -102,7 +161,10 @@ const BookClubPage: NextPage = () => {
           className="pointer-events-none absolute -bottom-60 right-0 z-10 hidden h-auto w-1/2 max-w-40 select-none drop-shadow-md lg:block 2xl:-bottom-64 2xl:max-w-48"
         />
 
-        <Section className="grow pt-8">
+        <Section
+          className="grow pt-8"
+          containerClassName="flex flex-row gap-8 overflow-x-auto overflow-y-clip"
+        >
           {books.map((book) => (
             <Book key={book.title} {...book} />
           ))}
