e2c5442d9eb4d7b8cd21925415d1f089105b2478
Matt Cowley <me@mattcowley.co.uk>
2025-01-20 22:40:43 +0000

    Switch weather + youtube stream API endpoints to text (#967)
    * Abstract + export underlying methods for stream API routes
    
    * Add trpc endpoint for weather overlay
    
    * Switch weather API endpoint to text response
    
    * Switch youtube API endpoint to text response
    
    * Remove explicit weather type
    
    * Remove unused request arg

--- apps/website/src/app/api/stream/weather/route.ts
+++ apps/website/src/app/api/stream/weather/route.ts
@@ -1,165 +1,20 @@
-import { env } from "@/env";
-import { getCurrentObservation } from "@/server/apis/weather";
-import { rounded } from "@/utils/math";
+import { getWeather } from "@/server/apis/weather";
 
-const getFeelsLike = (
-  temperature: number | null,
-  heatIndex: number | null,
-  windChill: number | null,
-) => {
-  if (temperature !== null) {
-    if (temperature >= 70 && heatIndex !== null) return heatIndex;
-    if (temperature <= 61 && windChill !== null) return windChill;
-  }
-  return temperature;
-};
+// API for chat bot
+export async function GET() {
+  try {
+    const data = await getWeather();
+    const resp = `Alveus Weather: ${data.temperature.fahrenheit} °F (${data.temperature.celsius} °C). Feels like ${data.temperature.feelsLike.fahrenheit} °F (${data.temperature.feelsLike.celsius} °C). ${data.humidity}% humidity. ${data.wind.speed.miles} mph winds. ${data.precipitation.total.inches} in of rain.`;
 
-export type WeatherResponse = {
-  time: {
-    local: string | null;
-    utc: string | null;
-  };
-  uvIndex: number | null;
-  humidity: number | null;
-  temperature: {
-    fahrenheit: number | null;
-    celsius: number | null;
-    heatIndex: {
-      fahrenheit: number | null;
-      celsius: number | null;
-    };
-    windChill: {
-      fahrenheit: number | null;
-      celsius: number | null;
-    };
-    feelsLike: {
-      fahrenheit: number | null;
-      celsius: number | null;
-    };
-  };
-  pressure: {
-    inches: number | null;
-    millibars: number | null;
-  };
-  wind: {
-    speed: {
-      miles: number | null;
-      kilometers: number | null;
-    };
-    gusts: {
-      miles: number | null;
-      kilometers: number | null;
-    };
-    direction: number | null;
-  };
-  precipitation: {
-    rate: {
-      inches: number | null;
-      millimeters: number | null;
-    };
-    total: {
-      inches: number | null;
-      millimeters: number | null;
-    };
-  };
-};
-
-// API for overlay + chat bot
-export async function GET(request: Request) {
-  if (env.WEATHER_STATION_ID && env.WEATHER_API_KEY) {
-    try {
-      const weather = await getCurrentObservation(env.WEATHER_STATION_ID, true);
-      const feelsLike = getFeelsLike(
-        weather.imperial.temp,
-        weather.imperial.heatIndex,
-        weather.imperial.windChill,
-      );
-
-      const resp: WeatherResponse = {
-        time: {
-          local: weather.obsTimeLocal,
-          utc: weather.obsTimeUtc,
-        },
-        uvIndex: weather.uv,
-        humidity: weather.humidity,
-        temperature: {
-          fahrenheit: weather.imperial.temp,
-          celsius:
-            weather.imperial.temp !== null
-              ? rounded(((weather.imperial.temp - 32) * 5) / 9, 2)
-              : null,
-          heatIndex: {
-            fahrenheit: weather.imperial.heatIndex,
-            celsius:
-              weather.imperial.heatIndex !== null
-                ? rounded(((weather.imperial.heatIndex - 32) * 5) / 9, 2)
-                : null,
-          },
-          windChill: {
-            fahrenheit: weather.imperial.windChill,
-            celsius:
-              weather.imperial.windChill !== null
-                ? rounded(((weather.imperial.windChill - 32) * 5) / 9, 2)
-                : null,
-          },
-          feelsLike: {
-            fahrenheit: feelsLike,
-            celsius: feelsLike ? rounded(((feelsLike - 32) * 5) / 9, 2) : null,
-          },
-        },
-        pressure: {
-          inches: weather.imperial.pressure,
-          millibars:
-            weather.imperial.pressure !== null
-              ? rounded(weather.imperial.pressure * 33.86375, 2)
-              : null,
-        },
-        wind: {
-          speed: {
-            miles: weather.imperial.windSpeed,
-            kilometers:
-              weather.imperial.windSpeed !== null
-                ? rounded(weather.imperial.windSpeed * 1.60934, 2)
-                : null,
-          },
-          gusts: {
-            miles: weather.imperial.windGust,
-            kilometers:
-              weather.imperial.windGust !== null
-                ? rounded(weather.imperial.windGust * 1.60934, 2)
-                : null,
-          },
-          direction: weather.winddir,
-        },
-        precipitation: {
-          rate: {
-            inches: weather.imperial.precipRate,
-            millimeters:
-              weather.imperial.precipRate !== null
-                ? rounded(weather.imperial.precipRate * 25.4, 2)
-                : null,
-          },
-          total: {
-            inches: weather.imperial.precipTotal,
-            millimeters:
-              weather.imperial.precipTotal !== null
-                ? rounded(weather.imperial.precipTotal * 25.4, 2)
-                : null,
-          },
-        },
-      };
-
-      return Response.json(resp, {
-        headers: {
-          // Response can be cached for 1 minute
-          // And can be stale for 5 minutes while revalidating
-          "Cache-Control":
-            "max-age=60, s-maxage=60, stale-while-revalidate=300",
-        },
-      });
-    } catch (err) {
-      console.error("Error getting weather", err);
-    }
+    return new Response(resp, {
+      headers: {
+        // Response can be cached for 1 minute
+        // And can be stale for 5 minutes while revalidating
+        "Cache-Control": "max-age=60, s-maxage=60, stale-while-revalidate=300",
+      },
+    });
+  } catch (err) {
+    console.error("Error getting weather", err);
   }
 
   return new Response("Weather data not available", { status: 500 });
