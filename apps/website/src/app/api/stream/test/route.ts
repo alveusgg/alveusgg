0f01541c08604e597f5a25a411b084bf0135effd
MattIPv4 <me@mattcowley.co.uk>
2025-01-22 16:19:22 +0000

    Reduce test route logic

--- apps/website/src/app/api/stream/test/route.ts
+++ apps/website/src/app/api/stream/test/route.ts
@@ -1,30 +1,6 @@
-const headers = {
-  // Response can be cached for 30 minutes
-  // And can be stale for 5 minutes while revalidating
-  "Cache-Control": "max-age=1800, s-maxage=1800, stale-while-revalidate=300",
-
-  // Vercel doesn't respect Vary so we allow all origins to use this
-  // Ideally we'd just allow specifically localhost + *.ext-twitch.tv
-  "Access-Control-Allow-Origin": "*",
-};
-
-// API for extension
 export async function GET(request: Request) {
-  // Handle preflight requests
-  if (request.method === "OPTIONS") {
-    return new Response(null, {
-      status: 204,
-      headers: {
-        ...headers,
-        "Access-Control-Allow-Methods": "GET",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Max-Age": "86400",
-      },
-    });
-  }
-
-  // Otherwise, return the data
-  return Response.json({ ok: true }, { headers });
+  const opt = request.method === "OPTIONS";
+  return Response.json({ opt });
 }
 
 // Cache the response for 30 minutes
