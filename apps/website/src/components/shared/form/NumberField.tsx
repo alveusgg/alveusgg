c5c6d6e864c7db2db09dc6db6f0ce236f82e0ebd
MattIPv4 <me@mattcowley.co.uk>
2025-02-01 23:28:14 +0000

    Migrate outline-none usage to v4

--- apps/website/src/components/shared/form/NumberField.tsx
+++ apps/website/src/components/shared/form/NumberField.tsx
@@ -101,7 +101,7 @@ export function NumberField(props: NumberFieldProps) {
             {props.prefix}
             <input
               className={classes(
-                "w-full flex-1 bg-white p-1 px-2 text-black outline-none",
+                "w-full flex-1 bg-white p-1 px-2 text-black outline-hidden",
                 props.inputClassName,
               )}
               {...inputProps}
