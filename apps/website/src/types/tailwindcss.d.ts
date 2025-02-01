f9813a3849613808ecabeb3a333a7a54d2d2cdab
MattIPv4 <me@mattcowley.co.uk>
2025-02-01 20:11:05 +0000

    Remove old config

--- apps/website/src/types/tailwindcss.d.ts
+++
@@ -1,6 +0,0 @@
-// declare module "tailwindcss/lib/util/flattenColorPalette" {
-//   const flattenColorPalette: (
-//     obj: Record<string, Record<string, string | Record<string, string>>>,
-//   ) => Record<string, string>;
-//   export default flattenColorPalette;
-// }
