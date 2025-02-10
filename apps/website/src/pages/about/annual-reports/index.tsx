31d5c4a6165b4e5a8dc748fae71f79f3c5876b8c
MattIPv4 <me@mattcowley.co.uk>
2025-02-10 02:23:29 +0000

    Use single section for annual reports + 990s

--- apps/website/src/pages/about/annual-reports/index.tsx
+++ apps/website/src/pages/about/annual-reports/index.tsx
@@ -34,84 +34,82 @@ const AnnualReportsPage: NextPage = () => {
         <Heading className="text-center">Annual Reports</Heading>
       </Section>
 
-      <Section containerClassName="flex flex-col md:flex-row gap-8 xl:gap-16">
-        <div className="text-balance md:basis-1/2 xl:basis-3/5">
-          <p className="text-lg text-alveus-green-900">
-            Each year we publish a report that covers key stats on the growth
-            and current status of Alveus, the impact we&apos;ve made toward our
-            continued mission, and an in-depth breakdown of the financials
-            behind the sanctuary.
-          </p>
-        </div>
-        <div className="flex flex-col items-start gap-4 md:basis-1/2 xl:basis-2/5">
-          {reportYears.map((year) => (
-            <Button
-              key={year}
-              href={`/about/annual-reports/${year}`}
-              className="group inline-flex items-center sm:shrink-0"
-            >
-              <Heading
-                level={2}
-                className="my-0 font-sans text-2xl font-semibold"
+      <Section containerClassName="space-y-16">
+        <div className="flex flex-col gap-8 md:flex-row xl:gap-16">
+          <div className="text-balance md:basis-1/2 xl:basis-3/5">
+            <p className="text-lg text-alveus-green-900">
+              Each year we publish a report that covers key stats on the growth
+              and current status of Alveus, the impact we&apos;ve made toward
+              our continued mission, and an in-depth breakdown of the financials
+              behind the sanctuary.
+            </p>
+          </div>
+          <div className="flex flex-col items-start gap-4 md:basis-1/2 xl:basis-2/5">
+            {reportYears.map((year) => (
+              <Button
+                key={year}
+                href={`/about/annual-reports/${year}`}
+                className="group inline-flex items-center sm:shrink-0"
               >
-                {year} Annual Report
-              </Heading>
-              <IconChevronRight
-                size={24}
-                className="-mr-1 ml-2 mt-1 shrink-0 transition-all group-hover:-mr-2 group-hover:ml-3"
-              />
-            </Button>
-          ))}
+                <Heading
+                  level={2}
+                  className="my-0 font-sans text-2xl font-semibold"
+                >
+                  {year} Annual Report
+                </Heading>
+                <IconChevronRight
+                  size={24}
+                  className="-mr-1 ml-2 mt-1 shrink-0 transition-all group-hover:-mr-2 group-hover:ml-3"
+                />
+              </Button>
+            ))}
+          </div>
         </div>
-      </Section>
 
-      <Section
-        dark
-        containerClassName="flex flex-col md:flex-row gap-8 xl:gap-16"
-      >
-        <div className="text-balance md:basis-1/2 xl:basis-3/5">
-          <p className="text-lg">
-            As a 501(c)(3) nonprofit, we are required to file a 990 form each
-            year with the IRS. These forms are public record, available to
-            download here, and give a detailed look at our financials.
-          </p>
-        </div>
-        <div className="md:basis-1/2 xl:basis-2/5">
-          <Menu>
-            <MenuButton
-              as={DropdownButton}
-              dark
-              className="group/button inline-flex items-center gap-1 py-2 pr-4"
-            >
-              Download 990 Filings
-              <IconChevronDown
-                size={16}
-                className="transition-transform translate-y-0.5 group-data-[open]/button:-scale-y-100"
-              />
-            </MenuButton>
+        <div className="flex flex-col gap-8 md:flex-row xl:gap-16">
+          <div className="text-balance md:basis-1/2 xl:basis-3/5">
+            <p>
+              As a 501(c)(3) nonprofit, we are required to file a 990 form each
+              year with the IRS. These forms are public record, available to
+              download here, and give a detailed look at our financials.
+            </p>
+          </div>
+          <div className="md:basis-1/2 xl:basis-2/5">
+            <Menu>
+              <MenuButton
+                as={DropdownButton}
+                className="group/button inline-flex items-center gap-1 py-2 pr-4"
+              >
+                Download 990 Filings
+                <IconChevronDown
+                  size={16}
+                  className="transition-transform translate-y-0.5 group-data-[open]/button:-scale-y-100"
+                />
+              </MenuButton>
 
-            <MenuItems
-              anchor="bottom"
-              transition
-              className="group/items absolute left-0 top-full z-30 -ml-4 mt-1.5 flex flex-col rounded bg-alveus-tan text-alveus-green shadow-lg outline outline-1 outline-black/20 transition ease-in-out data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[closed]:scale-95"
-              as="ul"
-              modal={false}
-            >
-              {filings.map(({ year, link }) => (
-                <MenuItem key={year} as="li" className="group/item">
-                  <a
-                    href={link}
-                    target="_blank"
-                    download={`alveus-sanctuary-990-${year}.pdf`}
-                    rel="noreferrer"
-                    className="block rounded px-4 py-1 transition-colors hover:bg-alveus-green hover:text-alveus-tan group-data-[focus]/item:outline-blue-500 group-data-[focus]/item:group-focus-visible/items:outline"
-                  >
-                    {`alveus-sanctuary-990-${year}.pdf`}
-                  </a>
-                </MenuItem>
-              ))}
-            </MenuItems>
-          </Menu>
+              <MenuItems
+                anchor="bottom"
+                transition
+                className="group/items absolute left-0 top-full z-30 -ml-4 mt-1.5 flex flex-col rounded bg-alveus-tan text-alveus-green shadow-lg outline outline-1 outline-black/20 transition ease-in-out data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[closed]:scale-95"
+                as="ul"
+                modal={false}
+              >
+                {filings.map(({ year, link }) => (
+                  <MenuItem key={year} as="li" className="group/item">
+                    <a
+                      href={link}
+                      target="_blank"
+                      download={`alveus-sanctuary-990-${year}.pdf`}
+                      rel="noreferrer"
+                      className="block rounded px-4 py-1 transition-colors hover:bg-alveus-green hover:text-alveus-tan group-data-[focus]/item:outline-blue-500 group-data-[focus]/item:group-focus-visible/items:outline"
+                    >
+                      {`alveus-sanctuary-990-${year}.pdf`}
+                    </a>
+                  </MenuItem>
+                ))}
+              </MenuItems>
+            </Menu>
+          </div>
         </div>
       </Section>
 
