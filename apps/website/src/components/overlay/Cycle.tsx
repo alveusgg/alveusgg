3a90c2b13b805c3724ab2aa66a6af81ac9eaf39f
Matt Cowley <me@mattcowley.co.uk>
2025-02-17 22:33:28 +0000

    Add QR code to stream overlay (#1032)
    * Render QR code with socials link in overlay
    
    * Cycle the QR code and logo
    
    * Use Cycle component for the upcoming event itself
    
    * Fix instability of cloned Cycle elements
    
    * Move Cycle component into dedicated file

---
+++ apps/website/src/components/overlay/Cycle.tsx
@@ -0,0 +1,84 @@
+import {
+  Children,
+  cloneElement,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+  type Ref,
+  type ReactElement,
+} from "react";
+import { Transition } from "@headlessui/react";
+
+import { classes } from "@/utils/classes";
+
+const Cycle = ({
+  items,
+  interval = 60,
+}: {
+  items: ReactElement<{ className: string; ref: Ref<HTMLElement> }>[];
+  interval?: number;
+}) => {
+  const refs = useRef<(HTMLElement | null)[]>([]);
+  const cloned = useMemo(
+    () =>
+      items.map((item, idx) =>
+        cloneElement(item, {
+          className: classes(
+            item.props.className,
+            "transition-opacity data-[closed]:opacity-0 data-[enter]:duration-700 data-[leave]:duration-300",
+          ),
+          ref: (el) => {
+            refs.current[idx] = el;
+          },
+        }),
+      ),
+    [items],
+  );
+
+  const [index, setIndex] = useState(0);
+  useEffect(() => {
+    const nextIndex = (index + 1) % items.length;
+    const timeout = setTimeout(() => setIndex(nextIndex), interval * 1000);
+    return () => clearTimeout(timeout);
+  }, [index, items, interval]);
+
+  return Children.map(cloned, (item, idx) => (
+    <Transition
+      show={idx === index}
+      beforeEnter={() => {
+        const ref = refs.current[idx];
+        if (!ref) return;
+
+        ref.style.zIndex = "1";
+      }}
+      beforeLeave={() => {
+        const ref = refs.current[idx];
+        if (!ref) return;
+
+        // Only offset if not absolute positioned
+        if (window.getComputedStyle(ref).position !== "absolute") {
+          // Offset by the width of the element and any gap between elements
+          const { width } = ref.getBoundingClientRect();
+          const gap = ref.parentElement
+            ? Number(
+                window
+                  .getComputedStyle(ref.parentElement)
+                  .rowGap.replace(/px$/, ""),
+              )
+            : 0;
+
+          // If we're the last element, we need to offset an element that will be to the left of us
+          ref.style[idx === items.length - 1 ? "marginLeft" : "marginRight"] =
+            `-${width + gap}px`;
+        }
+
+        ref.style.zIndex = "0";
+      }}
+    >
+      {item}
+    </Transition>
+  ));
+};
+
+export default Cycle;
--- apps/website/src/components/overlay/Event.tsx
+++ apps/website/src/components/overlay/Event.tsx
@@ -1,16 +1,53 @@
 import Image from "next/image";
-import { useEffect, useMemo, useRef, useState } from "react";
-import { Transition } from "@headlessui/react";
+import { useEffect, useMemo, useRef, useState, type HTMLProps } from "react";
 import { keepPreviousData } from "@tanstack/react-query";
+import type { CalendarEvent } from "@prisma/client";
 
 import { trpc } from "@/utils/trpc";
 import { DATETIME_ALVEUS_ZONE, formatDateTimeRelative } from "@/utils/datetime";
 import { classes } from "@/utils/classes";
+import { getShortBaseUrl } from "@/utils/short-url";
 import { getFormattedTitle, twitchChannels } from "@/data/calendar-events";
+
+import { QRCode } from "@/components/QrCode";
+import Cycle from "@/components/overlay/Cycle";
 
 import logoImage from "@/assets/logo.png";
 
-const Event = ({ className }: { className?: string }) => {
+const cycleTime = 60;
+
+const Socials = ({ className, ...props }: HTMLProps<HTMLDivElement>) => (
+  <div className={classes(className, "flex items-center gap-2")} {...props}>
+    <Cycle
+      items={useMemo(
+        () => [
+          <Image
+            key="logo"
+            src={logoImage}
+            alt=""
+            height={64}
+            className="size-16 object-contain opacity-75 brightness-150 contrast-125 drop-shadow-sm grayscale"
+          />,
+          <QRCode
+            key="qr"
+            className="size-16 rounded-lg border-2 border-black bg-white p-1 opacity-75 drop-shadow-sm"
+            value={`${getShortBaseUrl()}/socials`}
+          />,
+        ],
+        [],
+      )}
+      // We want to be back on the logo before the parent cycle switches
+      interval={cycleTime / 3}
+    />
+
+    <div className="text-xl font-bold text-white text-stroke">
+      <p>alveussanctuary.org</p>
+      <p>@alveussanctuary</p>
+    </div>
+  </div>
+);
+
+const useUpcoming = () => {
   // Set the range for upcoming events to the next 3 days
   // Refresh every 60s
   const [upcomingRange, setUpcomingRange] = useState<[Date, Date]>();
@@ -34,87 +71,85 @@ const Event = ({ className }: { className?: string }) => {
     { start: upcomingRange?.[0], end: upcomingRange?.[1] },
     { enabled: upcomingRange !== undefined, placeholderData: keepPreviousData },
   );
-  const firstEventId = useMemo(
-    () => events?.find(twitchChannels.alveus.filter)?.id,
-    [events],
-  );
 
-  // If we have an upcoming event, swap socials with it
-  // Swap every 60s
-  const [visibleEventId, setVisibleEventId] = useState<string>();
-  const eventInterval = useRef<NodeJS.Timeout>(null);
+  // Ensure we hold a stable reference to the event
+  const [event, setEvent] = useState<CalendarEvent>();
   useEffect(() => {
-    if (!firstEventId) {
-      setVisibleEventId(undefined);
+    if (!events) return;
+
+    const upcoming = events.find(twitchChannels.alveus.filter);
+    if (!upcoming) {
+      setEvent(undefined);
       return;
     }
 
-    const swapEvent = () =>
-      setVisibleEventId((prev) => (prev ? undefined : firstEventId));
-    swapEvent();
-    eventInterval.current = setInterval(swapEvent, 60 * 1000);
-    return () => clearInterval(eventInterval.current ?? undefined);
-  }, [firstEventId]);
-  const event = useMemo(
+    const hasChanged =
+      !event ||
+      Object.entries(upcoming).some(([key, value]) => {
+        if (!(key in event)) return true;
+        const val = event[key as keyof CalendarEvent];
+
+        // Handle dates as they are objects
+        if (value instanceof Date && val instanceof Date) {
+          return value.getTime() !== val.getTime();
+        }
+        return value !== val;
+      });
+    if (hasChanged) {
+      setEvent(upcoming);
+    }
+  }, [events, event]);
+
+  return event;
+};
+
+const Upcoming = ({
+  event,
+  className,
+  ...props
+}: { event: CalendarEvent } & HTMLProps<HTMLDivElement>) => (
+  <div
+    className={classes(className, "font-bold text-white text-stroke")}
+    {...props}
+  >
+    <p>Upcoming:</p>
+
+    {event && (
+      <>
+        <p className="text-xl">
+          {getFormattedTitle(event, twitchChannels.alveus.username, 30)}
+        </p>
+        <p className="text-xl">
+          {formatDateTimeRelative(
+            event.startAt,
+            {
+              style: "long",
+              time: event.hasTime ? "minutes" : undefined,
+              timezone: event.hasTime,
+            },
+            { zone: DATETIME_ALVEUS_ZONE },
+          )}
+        </p>
+      </>
+    )}
+  </div>
+);
+
+const Event = ({ className }: { className?: string }) => {
+  const upcoming = useUpcoming();
+
+  const items = useMemo(
     () =>
-      visibleEventId && events?.find((event) => event.id === visibleEventId),
-    [visibleEventId, events],
+      [
+        <Socials key="socials" className={className} />,
+        upcoming && (
+          <Upcoming key="upcoming" event={upcoming} className={className} />
+        ),
+      ].filter((x) => !!x),
+    [upcoming, className],
   );
 
-  return (
-    <>
-      <Transition show={event !== undefined}>
-        <div
-          className={classes(
-            className,
-            "font-bold text-white transition-opacity text-stroke data-[closed]:opacity-0 data-[enter]:duration-700 data-[leave]:duration-300",
-          )}
-        >
-          <p>Upcoming:</p>
-
-          {event && (
-            <>
-              <p className="text-xl">
-                {getFormattedTitle(event, twitchChannels.alveus.username, 30)}
-              </p>
-              <p className="text-xl">
-                {formatDateTimeRelative(
-                  event.startAt,
-                  {
-                    style: "long",
-                    time: event.hasTime ? "minutes" : undefined,
-                    timezone: event.hasTime,
-                  },
-                  { zone: DATETIME_ALVEUS_ZONE },
-                )}
-              </p>
-            </>
-          )}
-        </div>
-      </Transition>
-
-      <Transition show={event === undefined}>
-        <div
-          className={classes(
-            className,
-            "flex items-center gap-2 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-700 data-[leave]:duration-300",
-          )}
-        >
-          <Image
-            src={logoImage}
-            alt=""
-            height={64}
-            className="h-16 w-auto opacity-75 brightness-150 contrast-125 drop-shadow-sm grayscale"
-          />
-
-          <div className="text-xl font-bold text-white text-stroke">
-            <p>alveussanctuary.org</p>
-            <p>@alveussanctuary</p>
-          </div>
-        </div>
-      </Transition>
-    </>
-  );
+  return <Cycle items={items} interval={cycleTime} />;
 };
 
 export default Event;
