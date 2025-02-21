2d3c9079a9fe5a407551a2d83b06d6d64f91b425
MattIPv4 <me@mattcowley.co.uk>
2025-02-21 23:57:08 +0000

    Remove old merch from carousel

--- apps/website/src/components/content/MerchCarousel.tsx
+++ apps/website/src/components/content/MerchCarousel.tsx
@@ -3,15 +3,6 @@ import Image, { type ImageProps } from "next/image";
 
 import { classes } from "@/utils/classes";
 
-import tshirtFront from "@/assets/merch/tshirt-front.png";
-import tshirtDemoGrinder from "@/assets/merch/tshirt-demo-grinder.png";
-import tshirtBack from "@/assets/merch/tshirt-back.png";
-import tshirtDemoAppa from "@/assets/merch/tshirt-demo-appa.png";
-import tshirtDemoMia from "@/assets/merch/tshirt-demo-mia.png";
-import hoodieFront from "@/assets/merch/hoodie-front.png";
-import hoodieBack from "@/assets/merch/hoodie-back.png";
-import hoodieDemoGeorgie from "@/assets/merch/hoodie-demo-georgie.png";
-
 import sirenImage from "@/assets/plushies/siren.png";
 import winnieImage from "@/assets/plushies/winnie.png";
 import georgieImage from "@/assets/plushies/georgie.png";
@@ -30,56 +21,7 @@ interface Item {
 }
 
 const allItems = {
-  merch: {
-    tshirtFront: {
-      src: tshirtFront,
-      alt: "Alveus T-Shirt (Front)",
-      pip: {
-        src: tshirtBack,
-        alt: "Alveus T-Shirt (Back)",
-      },
-    },
-    tshirtDemoGrinder: {
-      src: tshirtDemoGrinder,
-      alt: "Alveus T-Shirt (Front) - Demo w/ Grinder",
-    },
-    hoodieFront: {
-      src: hoodieFront,
-      alt: "Alveus Hoodie (Front)",
-      pip: {
-        src: hoodieBack,
-        alt: "Alveus Hoodie (Back)",
-      },
-    },
-    hoodieDemoGeorgie: {
-      src: hoodieDemoGeorgie,
-      alt: "Alveus Hoodie (Back) - Demo w/ Georgie",
-    },
-    tshirtBack: {
-      src: tshirtBack,
-      alt: "Alveus T-Shirt (Back)",
-      pip: {
-        src: tshirtFront,
-        alt: "Alveus T-Shirt (Front)",
-      },
-    },
-    tshirtDemoAppa: {
-      src: tshirtDemoAppa,
-      alt: "Alveus T-Shirt (Back) - Demo w/ Appa",
-    },
-    hoodieBack: {
-      src: hoodieBack,
-      alt: "Alveus Hoodie (Back)",
-      pip: {
-        src: hoodieFront,
-        alt: "Alveus Hoodie (Front)",
-      },
-    },
-    tshirtDemoMia: {
-      src: tshirtDemoMia,
-      alt: "Alveus T-Shirt (Back) - Demo w/ Mia",
-    },
-  },
+  merch: {},
   plushies: {
     siren: {
       src: sirenImage,
