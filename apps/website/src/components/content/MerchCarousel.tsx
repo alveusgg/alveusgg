d76a3b5a8ddc99208fd91d34fe3e0495cab70b6a
MattIPv4 <me@mattcowley.co.uk>
2025-02-22 18:10:55 +0000

    Add new merch to carousel

--- apps/website/src/components/content/MerchCarousel.tsx
+++ apps/website/src/components/content/MerchCarousel.tsx
@@ -3,6 +3,31 @@ import Image, { type ImageProps } from "next/image";
 
 import { classes } from "@/utils/classes";
 
+import beanieDoodleImage from "@/assets/merch/beanie-doodle.png";
+import beanieNoodleImage from "@/assets/merch/beanie-noodle.png";
+import bottleStickersImage from "@/assets/merch/bottle-stickers.png";
+// import capDoodleImage from "@/assets/merch/cap-doodle.png";
+// import capNoodleImage from "@/assets/merch/cap-noodle.png";
+import hoodieAdventureImage from "@/assets/merch/hoodie-adventure.png";
+import hoodieMagazineImage from "@/assets/merch/hoodie-magazine.png";
+import hoodiePanelsImage from "@/assets/merch/hoodie-panels.png";
+import mugBarbaraImage from "@/assets/merch/mug-barbara.png";
+import mugDoodleImage from "@/assets/merch/mug-doodle.png";
+import mugFennReedImage from "@/assets/merch/mug-fenn-reed.png";
+import mugSirenImage from "@/assets/merch/mug-siren.png";
+import sweaterBugsImage from "@/assets/merch/sweater-bugs.png";
+import sweaterWinnieImage from "@/assets/merch/sweater-winnie.png";
+// import toteFramesImage from "@/assets/merch/tote-frames.png";
+// import toteGeometricImage from "@/assets/merch/tote-geometric.png";
+import tshirtBubbleImage from "@/assets/merch/tshirt-bubble.png";
+import tshirtBugsImage from "@/assets/merch/tshirt-bugs.png";
+import tshirtPatchyImage from "@/assets/merch/tshirt-patchy.png";
+
+import tshirtPatchyMayaImage from "@/assets/merch/tshirt-patchy-maya.png";
+import hoodieAdventureConnorImage from "@/assets/merch/hoodie-adventure-connor.png";
+import sweaterBugsAmandaImage from "@/assets/merch/sweater-bugs-amanda.png";
+import sweaterWinnieLindsayImage from "@/assets/merch/sweater-winnie-lindsay.png";
+
 import sirenImage from "@/assets/plushies/siren.png";
 import winnieImage from "@/assets/plushies/winnie.png";
 import georgieImage from "@/assets/plushies/georgie.png";
@@ -14,14 +39,95 @@ import Carousel from "./Carousel";
 interface Item {
   src: ImageProps["src"];
   alt: string;
+  size?: `w-${number}/${number}`;
   pip?: {
     src: ImageProps["src"];
     alt: string;
+    position?: "bottom" | "top";
   };
 }
 
 const allItems = {
-  merch: {},
+  merch: {
+    tshirtPatchyMaya: {
+      src: tshirtPatchyMayaImage,
+      alt: "Maya wearing the Patchy t-shirt",
+    },
+    tshirtBubblePatchy: {
+      src: tshirtBubbleImage,
+      alt: "Bubble t-shirt",
+      pip: {
+        src: tshirtPatchyImage,
+        alt: "Patchy t-shirt",
+      },
+    },
+    mugSirenBarbara: {
+      src: mugSirenImage,
+      alt: "Siren mug",
+      pip: {
+        src: mugBarbaraImage,
+        alt: "Barbara mug",
+      },
+    },
+    hoodieAdventureConnor: {
+      src: hoodieAdventureConnorImage,
+      alt: "Connor wearing the Adventure hoodie",
+    },
+    hoodiePanelsAdventure: {
+      src: hoodiePanelsImage,
+      alt: "Panels hoodie",
+      pip: {
+        src: hoodieAdventureImage,
+        alt: "Adventure hoodie",
+      },
+    },
+    mugFeenReedDoodle: {
+      src: mugFennReedImage,
+      alt: "Fenn and Reed mug",
+      pip: {
+        src: mugDoodleImage,
+        alt: "Doodle mug",
+      },
+    },
+    tshirtBugsSweater: {
+      src: tshirtBugsImage,
+      alt: "Bugs t-shirt",
+      pip: {
+        src: sweaterBugsImage,
+        alt: "Bugs sweater",
+      },
+    },
+    sweaterBugsAmanda: {
+      src: sweaterBugsAmandaImage,
+      alt: "Amanda wearing the Bugs sweater",
+    },
+    bottleStickers: {
+      src: bottleStickersImage,
+      alt: "Sticker bottle",
+      size: "w-2/3",
+    },
+    beanieDoodleNoodle: {
+      src: beanieDoodleImage,
+      alt: "Doodle beanie",
+      pip: {
+        src: beanieNoodleImage,
+        alt: "Noodle beanie",
+        position: "top",
+      },
+    },
+    sweaterWinnieHoodieMagazine: {
+      src: sweaterWinnieImage,
+      alt: "Winnie sweater",
+      pip: {
+        src: hoodieMagazineImage,
+        alt: "Magazine hoodie",
+      },
+    },
+    sweaterWinnieLindsay: {
+      src: sweaterWinnieLindsayImage,
+      alt: "Lindsay wearing the Winnie sweater",
+    },
+  },
   plushies: {
     siren: {
       src: sirenImage,
@@ -87,7 +193,7 @@ const MerchCarousel = ({
       Object.entries<Item>(
         type === "all" ? zipAllItems() : allItems[type],
       ).reduce<Record<string, ReactNode>>(
-        (obj, [key, { src, alt, pip }]) => ({
+        (obj, [key, { src, alt, size, pip }]) => ({
           ...obj,
           [key]: (
             <div className="relative mx-auto h-auto w-full max-w-40">
@@ -97,7 +203,11 @@ const MerchCarousel = ({
                 title={alt}
                 draggable={false}
                 width={200}
-                className="h-auto w-full drop-shadow-sm"
+                className={classes(
+                  "h-auto drop-shadow-sm",
+                  size || "w-full",
+                  pip?.position === "top" && "relative -right-2 -bottom-2",
+                )}
               />
 
               {pip && (
@@ -107,7 +217,12 @@ const MerchCarousel = ({
                   title={pip.alt}
                   draggable={false}
                   width={80}
-                  className="absolute -right-2 -bottom-2 h-auto w-1/2 max-w-20 drop-shadow-sm"
+                  className={classes(
+                    "absolute h-auto drop-shadow-sm",
+                    pip.position === "top"
+                      ? "-top-2 -left-2"
+                      : "-right-2 -bottom-2",
+                  )}
                 />
               )}
             </div>
