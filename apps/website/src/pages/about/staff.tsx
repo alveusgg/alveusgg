33840dcf58d2fd4be756edfaa4522d36d84bbd9d
MattIPv4 <me@mattcowley.co.uk>
2025-03-13 01:47:45 +0000

    Expand paragraph + add quote for Maya on staff page

--- apps/website/src/pages/about/staff.tsx
+++ apps/website/src/pages/about/staff.tsx
@@ -6,9 +6,6 @@ import Heading from "@/components/content/Heading";
 import People, { type Person } from "@/components/content/People";
 import Meta from "@/components/content/Meta";
 import { Lightbox, Preview } from "@/components/content/YouTube";
-import Link from "@/components/content/Link";
-
-import IconArrowRight from "@/icons/IconArrowRight";
 
 import mayaHigaImage from "@/assets/people/maya-higa.jpg";
 import mayaHigaFunImage from "@/assets/people/maya-higa-fun.jpg";
@@ -36,16 +33,25 @@ const staff: Record<string, Person> = {
       <>
         <p>
           Maya Higa is one of the top female streamers on Twitch and has amassed
-          a large following on YouTube and other social platforms. Maya has
-          experience as a licensed falconer, wildlife rehabilitator, zookeeper,
-          and conservation outreach educator. In 2021, Maya founded Alveus
+          a large following on YouTube and other social platforms. She
+          integrates her passion for wildlife conservation and education into
+          her content regularly, creating some of the most unique content on
+          Twitch. Maya has experience as a licensed falconer, wildlife
+          rehabilitator, zookeeper, and conservation outreach educator.
+        </p>
+        <p>
+          Her livestreams and videos feature conservation education and charity
+          fundraising. She created a conservation podcast in 2019 which aired
+          more than 60 episodes and raised more than $92,000 for conservation
+          organizations around the globe. In 2021, Maya founded Alveus
           Sanctuary, a non-profit wildlife sanctuary and virtual education
           center in central Texas.
         </p>
-        <Link href="/about/maya" className="flex items-center">
-          Read more about Maya
-          <IconArrowRight size={16} className="mt-0.5 ml-1" />
-        </Link>
+        <p className="font-serif text-lg font-bold text-alveus-green">
+          <span>&ldquo;</span>I love the natural world with my whole heart and
+          entire being. All I want is to inspire that same love in others.
+          <span>&rdquo;</span>
+        </p>
       </>
     ),
   },
