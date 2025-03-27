930a33c621ed93a81a5dd0d2812dfdf4eaccc684
MattIPv4 <me@mattcowley.co.uk>
2025-03-27 16:34:39 +0000

    Use ambassador icon/badge/emotes for Animal Quest page icons

--- apps/website/src/utils/array.ts
+++ apps/website/src/utils/array.ts
@@ -1,3 +1,15 @@
 export function countUnique(array: unknown[]) {
   return new Set(array).size;
 }
+
+export function mapFirst<T, U>(
+  array: T[],
+  fn: (item: T) => U | undefined,
+): U | undefined {
+  for (const item of array) {
+    const result = fn(item);
+    if (result !== undefined) {
+      return result;
+    }
+  }
+}
