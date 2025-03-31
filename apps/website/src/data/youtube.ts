ce0e2a59cfd1de31e93d81df953f27baef7e76a1
MattIPv4 <me@mattcowley.co.uk>
2025-03-31 17:25:25 +0100

    Associate a name with each YouTube channel

--- apps/website/src/data/youtube.ts
+++ apps/website/src/data/youtube.ts
@@ -1,8 +1,15 @@
+interface Channel {
+  id: string;
+  name: string;
+}
+
 export const channels = {
   alveus: {
     id: "UCbJ-1yM55NHrR1GS9hhPuvg",
+    name: "Alveus Sanctuary",
   },
   highlights: {
     id: "UCfisf6HxiQr8_4mctNBm9cQ",
+    name: "Alveus Highlights",
   },
-};
+} as const satisfies Record<string, Channel>;
