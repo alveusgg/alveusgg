73faab4283bdc5d1e1c74bc53e312c8920aac0fd
MattIPv4 <me@mattcowley.co.uk>
2025-04-16 19:08:50 +0100

    Remove global Prisma definition

--- apps/website/src/server/db/client.ts
+++ apps/website/src/server/db/client.ts
@@ -2,18 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 import { env } from "@/env";
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
-
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
-
-if (env.NODE_ENV !== "production") {
-  global.prisma = prisma;
-}
+export const prisma = new PrismaClient({
+  log: env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+});
