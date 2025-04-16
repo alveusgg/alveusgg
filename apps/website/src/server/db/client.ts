25c75280df281b4470c441efc9392dfd2fcbec8e
MattIPv4 <me@mattcowley.co.uk>
2025-04-16 19:14:13 +0100

    Explicitly set Prisma client output location

--- apps/website/src/server/db/client.ts
+++ apps/website/src/server/db/client.ts
@@ -1,9 +1,9 @@
-import { PrismaClient } from "@prisma/client";
-
 import { env } from "@/env";
+
+import { PrismaClient } from "../../../prisma/client";
 
 export const prisma = new PrismaClient({
   log: env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
 });
 
-export * from "@prisma/client";
+export * from "../../../prisma/client";
