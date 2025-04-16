05585ec8b76785f8a63cc18ceb01ac9e09004d14
MattIPv4 <me@mattcowley.co.uk>
2025-04-16 19:10:20 +0100

    Re-export entire generated Prisma client

--- apps/website/src/server/db/client.ts
+++ apps/website/src/server/db/client.ts
@@ -5,3 +5,5 @@ import { env } from "@/env";
 export const prisma = new PrismaClient({
   log: env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
 });
+
+export * from "@prisma/client";
