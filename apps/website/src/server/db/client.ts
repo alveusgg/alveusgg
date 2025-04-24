7d5dc17ac3a17ec02c4b9a6248a8e277b670596f
MattIPv4 <me@mattcowley.co.uk>
2025-04-25 00:11:06 +0100

    Expose edge client for Prisma

--- apps/website/src/server/db/client.ts
+++ apps/website/src/server/db/client.ts
@@ -1,9 +1,27 @@
+import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
+
 import { env } from "@/env";
 
 import { PrismaClient } from "../../../prisma/client";
 
-export const prisma = new PrismaClient({
+const client = new PrismaClient({
   log: env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
 });
+
+const edge = new PrismaClient({
+  adapter: new PrismaPlanetScale({ url: env.DATABASE_URL }),
+});
+
+Object.defineProperty(client, "edge", {
+  get() {
+    return edge;
+  },
+});
+
+type PrismaClientWithEdge = typeof client & {
+  get edge(): typeof edge;
+};
+
+export const prisma = client as PrismaClientWithEdge;
 
 export * from "../../../prisma/client";
