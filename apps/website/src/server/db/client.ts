fb2f461477738552dbd00cabdb129ab92666e774
MattIPv4 <me@mattcowley.co.uk>
2025-04-25 01:02:52 +0100

    Expose Prisma as proxy to lazily initialize clients

--- apps/website/src/server/db/client.ts
+++ apps/website/src/server/db/client.ts
@@ -2,26 +2,48 @@ import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 
 import { env } from "@/env";
 
-import { PrismaClient } from "../../../prisma/client";
+import { type Prisma, PrismaClient } from "../../../prisma/client";
 
-const client = new PrismaClient({
+const opts = {
   log: env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-});
+} as const satisfies Prisma.PrismaClientOptions;
 
-const edge = new PrismaClient({
-  adapter: new PrismaPlanetScale({ url: env.DATABASE_URL }),
-});
-
-Object.defineProperty(client, "edge", {
-  get() {
-    return edge;
-  },
-});
-
-type PrismaClientWithEdge = typeof client & {
-  get edge(): typeof edge;
+const createClient = () => new PrismaClient({ ...opts });
+let client: ReturnType<typeof createClient>;
+const getClient = () => {
+  client ??= createClient();
+  return client;
 };
 
-export const prisma = client as PrismaClientWithEdge;
+const createEdge = () =>
+  new PrismaClient({
+    ...opts,
+    adapter: new PrismaPlanetScale({ url: env.DATABASE_URL }),
+  });
+let edge: ReturnType<typeof createEdge>;
+const getEdge = () => {
+  edge ??= createEdge();
+  return edge;
+};
+
+// Readonly to reflect that the Proxy only exposes a getter
+type PrismaClientWithEdge = Readonly<
+  typeof client & {
+    get edge(): typeof edge;
+  }
+>;
+
+// Use a Proxy to allow lazy initialization of both clients
+export const prisma = new Proxy(
+  {},
+  {
+    get: (_, prop: string) => {
+      if (prop === "edge") {
+        return getEdge();
+      }
+      return Reflect.get(getClient(), prop);
+    },
+  },
+) as PrismaClientWithEdge;
 
 export * from "../../../prisma/client";
