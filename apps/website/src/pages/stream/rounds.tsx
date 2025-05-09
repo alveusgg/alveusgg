8530b8002bde1b0fc86a33d75ab6583b84de24a9
MattIPv4 <me@mattcowley.co.uk>
2025-05-09 22:29:16 +0100

    Move checks into dedicated component

--- apps/website/src/pages/stream/rounds.tsx
+++ apps/website/src/pages/stream/rounds.tsx
@@ -1,25 +1,16 @@
 import { DateTime } from "luxon";
 import { type NextPage } from "next";
-import Image from "next/image";
-import { type CSSProperties, useEffect, useId, useState } from "react";
+import { useEffect, useState } from "react";
 
-import {
-  type AmbassadorImage,
-  getAmbassadorImages,
-} from "@alveusgg/data/build/ambassadors/images";
+import { getAmbassadorImages } from "@alveusgg/data/build/ambassadors/images";
 
-import { classes, objToCss } from "@/utils/classes";
 import { DATETIME_ALVEUS_ZONE } from "@/utils/datetime";
 
 import Video from "@/components/content/Video";
+import Checks, { type Check } from "@/components/overlay/Checks";
 
 import roundsDayBackground from "@/assets/rounds/day.webm";
 import roundsNightBackground from "@/assets/rounds/night.webm";
-
-interface Check {
-  name: string;
-  icon: AmbassadorImage;
-}
 
 const checks: Record<string, Check> = {
   foxes: {
@@ -68,68 +59,7 @@ const checks: Record<string, Check> = {
   },
 };
 
-const Check = ({
-  check,
-  className,
-  style,
-}: {
-  check: Check;
-  className?: string;
-  style?: CSSProperties;
-}) => {
-  return (
-    <div
-      className={classes(
-        "flex items-center gap-2 drop-shadow-md origin-center",
-        className,
-      )}
-      style={style}
-    >
-      <Image
-        src={check.icon.src}
-        alt={check.icon.alt}
-        className="rounded-full border-4 border-white size-20 object-cover"
-        style={{ objectPosition: check.icon.position }}
-      />
-      <span className="w-3 h-1.5 bg-white rounded-xs" />
-      <span className="text-5xl font-bold text-white">{check.name}</span>
-    </div>
-  );
-};
-
-const timing = {
-  duration: 750,
-  delay: { item: -600, before: 1000, after: 9000 },
-};
-const scale = { from: 1, to: 1.15 };
-
-const itemDuration = timing.duration + timing.delay.item;
-const totalDuration =
-  itemDuration * Object.keys(checks).length +
-  timing.delay.before +
-  timing.delay.after;
-
-const keyframes = objToCss({
-  "0%": {
-    scale: scale.from,
-  },
-  [`${(timing.delay.before / totalDuration) * 100}%`]: {
-    scale: scale.from,
-  },
-  [`${((timing.delay.before + timing.duration / 2) / totalDuration) * 100}%`]: {
-    scale: scale.to,
-  },
-  [`${((timing.delay.before + timing.duration) / totalDuration) * 100}%`]: {
-    scale: scale.from,
-  },
-  "100%": {
-    scale: scale.from,
-  },
-});
-
 const RoundsPage: NextPage = () => {
-  const id = useId();
-
   const [night, setNight] = useState(false);
   useEffect(() => {
     const update = () => {
@@ -145,44 +75,25 @@ const RoundsPage: NextPage = () => {
   const background = night ? roundsNightBackground : roundsDayBackground;
 
   return (
-    <>
-      <style
-        dangerouslySetInnerHTML={{
-          __html: [
-            `@keyframes rounds-${id}-item { ${keyframes} }`,
-            `.rounds-${id}-item { animation: ${totalDuration}ms rounds-${id}-item infinite; will-change: scale; }`,
-            `@media (prefers-reduced-motion) { .rounds-${id}-item { animation: none !important; } }`,
-          ].join("\n"),
-        }}
+    <div className="h-screen w-full">
+      <Video
+        sources={background.sources}
+        poster={background.poster}
+        className="h-full w-full object-cover absolute inset-0 object-top-left"
+        width={1920}
+        height={1080}
+        autoPlay
+        loop
+        muted
+        playsInline
+        disablePictureInPicture
       />
-      <div className="h-screen w-full">
-        <Video
-          sources={background.sources}
-          poster={background.poster}
-          className="h-full w-full object-cover absolute inset-0 object-top-left"
-          width={1920}
-          height={1080}
-          autoPlay
-          loop
-          muted
-          playsInline
-          disablePictureInPicture
-        />
 
-        <div className="flex flex-col h-full max-h-full overflow-hidden justify-center items-start px-16 gap-4">
-          {Object.entries(checks).map(([key, check], idx) => (
-            <Check
-              key={key}
-              check={check}
-              className={`rounds-${id}-item`}
-              style={{
-                animationDelay: `${idx * itemDuration}ms`,
-              }}
-            />
-          ))}
-        </div>
-      </div>
-    </>
+      <Checks
+        checks={checks}
+        className="h-full max-h-full overflow-hidden px-16"
+      />
+    </div>
   );
 };
 
