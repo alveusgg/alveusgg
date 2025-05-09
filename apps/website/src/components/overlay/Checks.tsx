e975d64eddf432a427c4ba7ad8f0a79f574c69d0
MattIPv4 <me@mattcowley.co.uk>
2025-05-09 22:29:16 +0100

    Move checks into dedicated component

---
+++ apps/website/src/components/overlay/Checks.tsx
@@ -0,0 +1,129 @@
+import Image from "next/image";
+import { type CSSProperties, useId, useMemo } from "react";
+
+import { type AmbassadorImage } from "@alveusgg/data/build/ambassadors/images";
+
+import { classes, objToCss } from "@/utils/classes";
+
+export interface Check {
+  name: string;
+  icon: AmbassadorImage;
+}
+
+const Check = ({
+  check,
+  className,
+  style,
+}: {
+  check: Check;
+  className?: string;
+  style?: CSSProperties;
+}) => {
+  return (
+    <div
+      className={classes(
+        "flex items-center gap-2 drop-shadow-md origin-center",
+        className,
+      )}
+      style={style}
+    >
+      <Image
+        src={check.icon.src}
+        alt={check.icon.alt}
+        className="rounded-full border-4 border-white size-20 object-cover"
+        style={{ objectPosition: check.icon.position }}
+      />
+      <span className="w-3 h-1.5 bg-white rounded-xs" />
+      <span className="text-5xl font-bold text-white">{check.name}</span>
+    </div>
+  );
+};
+
+const Checks = ({
+  checks,
+  timing = {
+    duration: 750,
+    delay: { item: -600, before: 1000, after: 9000 },
+  },
+  scale = { from: 1, to: 1.15 },
+  className,
+}: {
+  checks: Record<string, Check>;
+  timing?: {
+    duration: number;
+    delay: { item: number; before: number; after: number };
+  };
+  scale?: { from: number; to: number };
+  className?: string;
+}) => {
+  // Define the animation keyframes
+  const id = useId().replace(/:/g, "");
+  const length = Object.keys(checks).length;
+  const animation = useMemo<{
+    duration: { total: number; item: number };
+    keyframes: string;
+  }>(() => {
+    const itemDuration = timing.duration + timing.delay.item;
+    const totalDuration =
+      itemDuration * length + timing.delay.before + timing.delay.after;
+
+    return {
+      duration: {
+        item: itemDuration,
+        total: totalDuration,
+      },
+      keyframes: objToCss({
+        "0%": {
+          scale: scale.from,
+        },
+        [`${(timing.delay.before / totalDuration) * 100}%`]: {
+          scale: scale.from,
+        },
+        [`${((timing.delay.before + timing.duration / 2) / totalDuration) * 100}%`]:
+          {
+            scale: scale.to,
+          },
+        [`${((timing.delay.before + timing.duration) / totalDuration) * 100}%`]:
+          {
+            scale: scale.from,
+          },
+        "100%": {
+          scale: scale.from,
+        },
+      }),
+    };
+  }, [timing, length, scale]);
+
+  return (
+    <>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: [
+            `@keyframes checks-${id}-item { ${animation.keyframes} }`,
+            `.checks-${id}-item { animation: ${animation.duration.total}ms checks-${id}-item infinite; will-change: scale; }`,
+            `@media (prefers-reduced-motion) { .checks-${id}-item { animation: none !important; } }`,
+          ].join("\n"),
+        }}
+      />
+      <div
+        className={classes(
+          "flex flex-col justify-center items-start gap-4",
+          className,
+        )}
+      >
+        {Object.entries(checks).map(([key, check], idx) => (
+          <Check
+            key={key}
+            check={check}
+            className={`checks-${id}-item`}
+            style={{
+              animationDelay: `${idx * animation.duration.item}ms`,
+            }}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Checks;
