bcb08732260012b5c0d139002d7db697d3e75e08
MattIPv4 <me@mattcowley.co.uk>
2025-05-13 23:46:05 +0100

    Initial run command + cam layout

--- apps/website/src/components/admin/ptz/LiveCamControls.tsx
+++ apps/website/src/components/admin/ptz/LiveCamControls.tsx
@@ -1,5 +1,109 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+import { classes } from "@/utils/classes";
+import { trpc } from "@/utils/trpc";
+
+import useChat from "@/hooks/chat";
+
+const channels = ["AlveusSanctuary", "AlveusGG"];
+
+const positions = [
+  {
+    scene: "customcamsbig",
+    positions: [
+      "col-start-2 col-span-2 row-start-1 row-span-2",
+      "col-start-1 col-span-1 row-start-1 row-span-1",
+      "col-start-1 col-span-1 row-start-2 row-span-1",
+      "col-start-1 col-span-1 row-start-3 row-span-1",
+      "col-start-2 col-span-1 row-start-3 row-span-1",
+      "col-start-3 col-span-1 row-start-3 row-span-1",
+    ],
+  },
+];
+
+const useCamLayout = () => {
+  const [layout, setLayout] = useState<string | null>(null);
+  const [cams, setCams] = useState<string[]>([]);
+
+  const camLayout = useMemo(() => {
+    const matching = positions.find(
+      (p) => p.scene === layout && p.positions.length === cams.length,
+    );
+    if (matching) {
+      return cams.map((cam, idx) => ({
+        cam,
+        position: matching.positions[idx]!,
+      }));
+    }
+    return null;
+  }, [layout, cams]);
+
+  return {
+    setLayout,
+    setCams,
+    cams: camLayout,
+  };
+};
+
 const LiveCamControls = () => {
-  return <></>;
+  const { mutateAsync: runCommand } = trpc.stream.runCommand.useMutation();
+  const { setLayout, setCams, cams } = useCamLayout();
+  const [active, setActive] = useState<string | null>(null);
+
+  useChat(
+    channels,
+    useCallback(
+      (message) => {
+        const { text } = message;
+
+        // Look for !scenecams response
+        const scenecams = text.match(
+          /^Scene: (.+) Current Scene: (.+) Cams: ((?:\d+: .+(?:,|$))+)$/,
+        );
+        if (scenecams) {
+          const [_, scene, _currentScene, cams] = scenecams;
+          if (scene && cams) {
+            const camList = cams
+              ?.split(",")
+              .map((cam) => cam.match(/(\d+): (.+)/)?.[2])
+              .filter((cam) => cam !== undefined);
+
+            console.log("!scenecams", { scene, cams: camList });
+            setLayout(scene);
+            setCams(camList);
+          }
+        }
+      },
+      [setLayout, setCams],
+    ),
+  );
+
+  useEffect(() => {
+    runCommand({
+      command: "scenecams",
+    });
+  }, [runCommand]);
+
+  return (
+    <div className="grid aspect-video grid-cols-3 grid-rows-3">
+      {cams?.map(({ cam, position }) => (
+        <div
+          key={cam}
+          title={cam}
+          className={classes(
+            position,
+            "relative border-2",
+            active === cam
+              ? "cursor-crosshair border-red"
+              : "cursor-pointer border-blue bg-black/25 backdrop-blur-xs",
+          )}
+          onClick={() => {
+            setActive(cam);
+          }}
+        ></div>
+      ))}
+    </div>
+  );
 };
 
 export default LiveCamControls;
--- apps/website/src/server/trpc/router/stream.ts
+++ apps/website/src/server/trpc/router/stream.ts
@@ -1,6 +1,55 @@
+import { z } from "zod";
+
+import { prisma } from "@alveusgg/database";
+
+import { sendChatMessage } from "@/server/apis/twitch";
 import { getWeather } from "@/server/apis/weather";
-import { publicProcedure, router } from "@/server/trpc/trpc";
+import {
+  createCheckPermissionMiddleware,
+  protectedProcedure,
+  publicProcedure,
+  router,
+} from "@/server/trpc/trpc";
+
+import { permissions } from "@/data/permissions";
+
+const ptzProcedure = protectedProcedure.use(
+  createCheckPermissionMiddleware(permissions.managePTZControls),
+);
 
 export const streamRouter = router({
   getWeather: publicProcedure.query(getWeather),
+
+  runCommand: ptzProcedure
+    .input(
+      z.object({
+        command: z.string(), // TODO: This needs to be an enum of known commands
+        args: z.array(z.string()).optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { command, args } = input;
+
+      // Get the user's Twitch access token
+      const account = await prisma.account.findFirst({
+        where: {
+          userId: ctx.session.user.id,
+          provider: "twitch",
+        },
+        select: {
+          providerAccountId: true,
+          access_token: true,
+        },
+      });
+      if (!account || !account.access_token) {
+        throw new Error("No Twitch account found for user");
+      }
+
+      await sendChatMessage(
+        account.access_token,
+        account.providerAccountId,
+        "858050963",
+        `!${command} ${args?.join(" ") || ""}`,
+      );
+    }),
 });
