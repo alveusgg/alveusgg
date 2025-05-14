f2fd03eba6e7c30b54b92610cd55c801386f4acb
James Williams <james-williams@softwareimaging.com>
2025-05-14 18:41:11 +0100

    Memoised the ref callback to only be changed (and therefore triggered/cleaned up) when the url changes

--- apps/website/src/components/admin/ptz/LiveCamFeed.tsx
+++ apps/website/src/components/admin/ptz/LiveCamFeed.tsx
@@ -1,30 +1,35 @@
 import { WebRTCPlayer } from "@eyevinn/webrtc-player";
+import { useMemo } from "react";
 
 interface LiveCamFeedProps {
   url: string;
 }
 
 const LiveCamFeed = ({ url }: LiveCamFeedProps) => {
+  const videoRefCallback = useMemo(() => {
+    return (video: HTMLVideoElement) => {
+      if (!video) return;
+      const player = new WebRTCPlayer({
+        type: "whep",
+        video: video,
+      });
+
+      player
+        .load(new URL(url))
+        .then(() => console.log(`[LOLA] Loaded`))
+        .catch((error) => console.error(`[LOLA] Error`, error));
+
+      return () => {
+        player.destroy();
+      };
+    };
+  }, [url]);
+
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-black">
       <video
         className="aspect-video max-h-full max-w-full"
-        ref={(video) => {
-          if (!video) return;
-          const player = new WebRTCPlayer({
-            type: "whep",
-            video: video,
-          });
-
-          player
-            .load(new URL(url))
-            .then(() => console.log(`[LOLA] Loaded`))
-            .catch((error) => console.error(`[LOLA] Error`, error));
-
-          return () => {
-            player.destroy();
-          };
-        }}
+        ref={videoRefCallback}
         autoPlay
         muted
       />
