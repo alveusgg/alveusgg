680ef16ddfb56598efd454e4902f53768096bd09
MattIPv4 <me@mattcowley.co.uk>
2025-05-18 20:24:47 +0100

    Add tRPC method to check Alveus subscription

--- apps/website/src/server/trpc/router/stream.ts
+++ apps/website/src/server/trpc/router/stream.ts
@@ -2,7 +2,10 @@ import { z } from "zod";
 
 import { prisma } from "@alveusgg/database";
 
-import { sendChatMessage } from "@/server/apis/twitch";
+import {
+  getUserSubscribedToBroadcaster,
+  sendChatMessage,
+} from "@/server/apis/twitch";
 import { getWeather } from "@/server/apis/weather";
 import {
   protectedProcedure,
@@ -12,6 +15,24 @@ import {
 
 import { channels } from "@/data/twitch";
 
+const getUserTwitchAccount = async (userId: string) => {
+  const { access_token: token, providerAccountId: id } =
+    (await prisma.account.findFirst({
+      where: {
+        userId,
+        provider: "twitch",
+      },
+      select: {
+        providerAccountId: true,
+        access_token: true,
+      },
+    })) ?? {};
+  if (!token || !id) {
+    throw new Error("No Twitch account found for user");
+  }
+  return { token, id };
+};
+
 export const runCommandSchema = z.object({
   command: z.enum(["ptzload"]),
   args: z.array(z.string()).optional(),
@@ -20,29 +41,23 @@ export const runCommandSchema = z.object({
 export const streamRouter = router({
   getWeather: publicProcedure.query(getWeather),
 
+  getSubscription: protectedProcedure.query(async ({ ctx }) => {
+    const twitchAccount = await getUserTwitchAccount(ctx.session.user.id);
+    return getUserSubscribedToBroadcaster(
+      twitchAccount.token,
+      twitchAccount.id,
+      channels.alveus.id,
+    );
+  }),
+
   runCommand: protectedProcedure
     .input(runCommandSchema)
     .mutation(async ({ ctx, input }) => {
+      const twitchAccount = await getUserTwitchAccount(ctx.session.user.id);
       const { command, args } = input;
-
-      // Get the user's Twitch access token
-      const account = await prisma.account.findFirst({
-        where: {
-          userId: ctx.session.user.id,
-          provider: "twitch",
-        },
-        select: {
-          providerAccountId: true,
-          access_token: true,
-        },
-      });
-      if (!account || !account.access_token) {
-        throw new Error("No Twitch account found for user");
-      }
-
       await sendChatMessage(
-        account.access_token,
-        account.providerAccountId,
+        twitchAccount.token,
+        twitchAccount.id,
         channels.alveusgg.id,
         `!${command} ${args?.join(" ") || ""}`,
       );
