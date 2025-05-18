565313eaa17833972b6877b7b61053a678f528d7
MattIPv4 <me@mattcowley.co.uk>
2025-05-18 20:31:42 +0100

    Reuse getUserTwitchAccount method in forms tRPC method

--- apps/website/src/server/trpc/router/stream.ts
+++ apps/website/src/server/trpc/router/stream.ts
@@ -1,7 +1,5 @@
 import { z } from "zod";
 
-import { prisma } from "@alveusgg/database";
-
 import {
   getUserSubscribedToBroadcaster,
   sendChatMessage,
@@ -12,26 +10,9 @@ import {
   publicProcedure,
   router,
 } from "@/server/trpc/trpc";
+import { getUserTwitchAccount } from "@/server/utils/auth";
 
 import { channels } from "@/data/twitch";
-
-const getUserTwitchAccount = async (userId: string) => {
-  const { access_token: token, providerAccountId: id } =
-    (await prisma.account.findFirst({
-      where: {
-        userId,
-        provider: "twitch",
-      },
-      select: {
-        providerAccountId: true,
-        access_token: true,
-      },
-    })) ?? {};
-  if (!token || !id) {
-    throw new Error("No Twitch account found for user");
-  }
-  return { token, id };
-};
 
 export const runCommandSchema = z.object({
   command: z.enum(["ptzload"]),
@@ -42,7 +23,7 @@ export const streamRouter = router({
   getWeather: publicProcedure.query(getWeather),
 
   getSubscription: protectedProcedure.query(async ({ ctx }) => {
-    const twitchAccount = await getUserTwitchAccount(ctx.session.user.id);
+    const twitchAccount = await getUserTwitchAccount(ctx.session.user.id, true);
     return getUserSubscribedToBroadcaster(
       twitchAccount.token,
       twitchAccount.id,
@@ -53,7 +34,10 @@ export const streamRouter = router({
   runCommand: protectedProcedure
     .input(runCommandSchema)
     .mutation(async ({ ctx, input }) => {
-      const twitchAccount = await getUserTwitchAccount(ctx.session.user.id);
+      const twitchAccount = await getUserTwitchAccount(
+        ctx.session.user.id,
+        true,
+      );
       const { command, args } = input;
       await sendChatMessage(
         twitchAccount.token,
