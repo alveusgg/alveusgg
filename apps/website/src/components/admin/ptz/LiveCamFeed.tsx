13ae4a1402dcc58a882c20a66e39e8a8eb770498
MattIPv4 <me@mattcowley.co.uk>
2025-05-19 04:31:39 +0100

    Move lola feed URL to tRPC method

--- apps/website/src/components/admin/ptz/LiveCamFeed.tsx
+++ apps/website/src/components/admin/ptz/LiveCamFeed.tsx
@@ -1,36 +1,90 @@
 import { WebRTCPlayer } from "@eyevinn/webrtc-player";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 
-interface LiveCamFeedProps {
-  url: string;
-}
+import { trpc } from "@/utils/trpc";
 
-const LiveCamFeed = ({ url }: LiveCamFeedProps) => {
-  const videoRefCallback = useCallback(
-    (video: HTMLVideoElement) => {
-      if (!video) return;
-      const player = new WebRTCPlayer({
+const LiveCamFeed = () => {
+  const feedUrl = trpc.stream.getFeedUrl.useQuery(undefined, {
+    refetchInterval: 1000 * 60 * 5,
+  });
+  const [url, setUrl] = useState<string>();
+
+  // Continue to use the first URL we're given until we get an error
+  // We do this to avoid the player being continually reloaded,
+  //  as the first URL will keep working as long as it isn't reloaded
+  useEffect(() => {
+    if (feedUrl.isError) {
+      setUrl(undefined);
+      return;
+    }
+
+    setUrl((prev) => prev ?? feedUrl.data?.url);
+  }, [feedUrl.isError, feedUrl.data?.url]);
+
+  const [player, setPlayer] = useState<WebRTCPlayer & { loaded: boolean }>();
+
+  useEffect(() => {
+    if (!player) return;
+
+    (async () => {
+      // Always unload the player if the URL changes
+      // This ensures that the underlying connection doesn't remain open
+      if (player.loaded) {
+        await player
+          .unload()
+          .then(() => {
+            console.log(`[LOLA] Unloaded`);
+            player.loaded = false;
+          })
+          .catch((error) => {
+            console.error(`[LOLA] Error unloading`, error);
+          });
+      }
+
+      if (!url) return;
+
+      await player
+        .load(new URL(url))
+        .then(() => {
+          console.log(`[LOLA] Loaded`);
+          player.loaded = true;
+        })
+        .catch((error) => console.error(`[LOLA] Error loading`, error));
+    })();
+  }, [url, player]);
+
+  const ref = useCallback((video: HTMLVideoElement) => {
+    if (!video) return;
+    const player = Object.assign(
+      new WebRTCPlayer({
         type: "whep",
         video: video,
-      });
+      }),
+      { loaded: false },
+    );
+    setPlayer(player);
 
-      player
-        .load(new URL(url))
-        .then(() => console.log(`[LOLA] Loaded`))
-        .catch((error) => console.error(`[LOLA] Error`, error));
-
-      return () => {
-        player.destroy();
-      };
-    },
-    [url],
-  );
+    return () => {
+      setPlayer((prev) => (prev === player ? undefined : prev));
+      if (player.loaded) {
+        player
+          .unload()
+          .then(() => {
+            console.log(`[LOLA] Unloaded`);
+            player.destroy();
+          })
+          .catch((error) => {
+            console.error(`[LOLA] Error unloading`, error);
+          });
+      }
+    };
+  }, []);
 
   return (
     <div className="flex h-full w-full items-center justify-center bg-black">
       <video
         className="aspect-video max-h-full max-w-full"
-        ref={videoRefCallback}
+        ref={ref}
         autoPlay
         muted
       />
--- apps/website/src/server/trpc/router/stream.ts
+++ apps/website/src/server/trpc/router/stream.ts
@@ -2,20 +2,31 @@ import { z } from "zod";
 
 import { prisma } from "@alveusgg/database";
 
+import { env } from "@/env";
+
 import { sendChatMessage } from "@/server/apis/twitch";
 import { getWeather } from "@/server/apis/weather";
 import {
+  createCheckPermissionMiddleware,
   protectedProcedure,
   publicProcedure,
   router,
 } from "@/server/trpc/trpc";
 
+import { permissions } from "@/data/permissions";
 import { channels } from "@/data/twitch";
+
+import invariant from "@/utils/invariant";
+import { createJWT, signJWT } from "@/utils/jwt";
 
 export const runCommandSchema = z.object({
   command: z.enum(["ptzload"]),
   args: z.array(z.string()).optional(),
 });
+
+const ptzProcedure = protectedProcedure.use(
+  createCheckPermissionMiddleware(permissions.managePTZControls),
+);
 
 export const streamRouter = router({
   getWeather: publicProcedure.query(getWeather),
@@ -47,4 +58,39 @@ export const streamRouter = router({
         `!${command} ${args?.join(" ") || ""}`,
       );
     }),
+
+  getFeedUrl: ptzProcedure.query(async () => {
+    invariant(env.CF_STREAM_KEY_ID, "CF_STREAM_KEY_ID is not set");
+    invariant(env.CF_STREAM_KEY_JWK, "CF_STREAM_KEY_JWK is not set");
+    invariant(
+      env.CF_STREAM_LOLA_VIDEO_ID,
+      "CF_STREAM_LOLA_VIDEO_ID is not set",
+    );
+    invariant(env.CF_STREAM_HOST, "CF_STREAM_HOST is not set");
+
+    const expiresTimeInSeconds = 60 * 5;
+    const expiresAt = Math.floor(Date.now() / 1000) + expiresTimeInSeconds;
+    const token = createJWT(
+      {
+        alg: "RS256",
+        kid: env.CF_STREAM_KEY_ID,
+      },
+      {
+        sub: env.CF_STREAM_LOLA_VIDEO_ID,
+        kid: env.CF_STREAM_KEY_ID,
+        exp: expiresAt,
+        accessRules: [
+          {
+            type: "any",
+            action: "allow",
+          },
+        ],
+      },
+    );
+
+    const jwt = await signJWT(token, env.CF_STREAM_KEY_JWK);
+    return {
+      url: `https://${env.CF_STREAM_HOST}/${jwt}/webRTC/play`,
+    };
+  }),
 });
