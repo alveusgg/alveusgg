4ca4f108525a9fc8a3388aa89895459c739639e3
MattIPv4 <me@mattcowley.co.uk>
2025-05-19 21:54:19 +0100

    Remove lola feed URL from server side props

--- apps/website/src/pages/stream/lola.tsx
+++ apps/website/src/pages/stream/lola.tsx
@@ -5,63 +5,21 @@ import type {
 } from "next";
 import { getSession } from "next-auth/react";
 
-import { env } from "@/env";
-
-import invariant from "@/utils/invariant";
-import { createJWT, signJWT } from "@/utils/jwt";
-
 import LiveCamFeed from "@/components/admin/ptz/LiveCamFeed";
 import Meta from "@/components/content/Meta";
 import { LoginWithTwitchButton } from "@/components/shared/LoginWithTwitchButton";
 
 export async function getServerSideProps(context: NextPageContext) {
-  invariant(env.CF_STREAM_KEY_ID, "CF_STREAM_KEY_ID is not set");
-  invariant(env.CF_STREAM_KEY_JWK, "CF_STREAM_KEY_JWK is not set");
-  invariant(env.CF_STREAM_LOLA_VIDEO_ID, "CF_STREAM_LOLA_VIDEO_ID is not set");
-  invariant(env.CF_STREAM_HOST, "CF_STREAM_HOST is not set");
-
   const session = await getSession(context);
   const isAuthed = session?.user?.id !== undefined;
   const hasRole = session?.user?.roles.includes("ptzControl");
 
-  if (!isAuthed || !hasRole) {
-    return { props: { isAuthed, hasRole } };
-  }
-
-  const expiresTimeInSeconds = 60 * 5;
-  const expiresIn = Math.floor(Date.now() / 1000) + expiresTimeInSeconds;
-  const token = createJWT(
-    {
-      alg: "RS256",
-      kid: env.CF_STREAM_KEY_ID,
-    },
-    {
-      sub: env.CF_STREAM_LOLA_VIDEO_ID,
-      kid: env.CF_STREAM_KEY_ID,
-      exp: expiresIn,
-      accessRules: [
-        {
-          type: "any",
-          action: "allow",
-        },
-      ],
-    },
-  );
-
-  const jwt = await signJWT(token, env.CF_STREAM_KEY_JWK);
-
-  return {
-    props: {
-      isAuthed,
-      hasRole,
-      url: `https://${env.CF_STREAM_HOST}/${jwt}/webRTC/play`,
-    },
-  };
+  return { props: { isAuthed, hasRole } };
 }
 
 const LolaPage: NextPage<
   InferGetServerSidePropsType<typeof getServerSideProps>
-> = ({ hasRole, isAuthed, url }) => {
+> = ({ hasRole, isAuthed }) => {
   return (
     <>
       <Meta title="Low-Latency Video Stream" description="" />
@@ -83,11 +41,9 @@ const LolaPage: NextPage<
               </p>
             </div>
           ) : (
-            url && (
-              <div className="h-screen w-screen">
-                <LiveCamFeed url={url} />
-              </div>
-            )
+            <div className="h-screen w-screen">
+              <LiveCamFeed url="" />
+            </div>
           ))}
       </main>
     </>
