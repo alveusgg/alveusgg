3c7db0c7b36c70de8244671098826e74f9a60038
MattIPv4 <me@mattcowley.co.uk>
2025-05-27 16:58:54 +0100

    Add custom styling for swap button

--- apps/website/src/components/shared/actions/ActionButton.tsx
+++ apps/website/src/components/shared/actions/ActionButton.tsx
@@ -16,22 +16,39 @@ type ActionButtonProps = {
     }) => ReactNode;
     force?: boolean;
   };
+  className?: string;
 };
 
-const ActionButton = ({ onClick, icon: Icon, tooltip }: ActionButtonProps) => {
+const ActionButton = ({
+  onClick,
+  icon: Icon,
+  tooltip,
+  className,
+}: ActionButtonProps) => {
   const Tooltip = tooltip.elm ?? "div";
   return (
-    <div className="group relative inline-block">
+    <div
+      className={classes(
+        "group relative",
+        !/\b((inline-)?(block|flex|grid|table)|inline|contents)\b/.test(
+          className || "",
+        ) && "inline-block",
+        !/\btext-/.test(className || "") &&
+          "text-alveus-green-400 hover:text-black",
+        className,
+      )}
+    >
       <Tooltip
         className={classes(
-          "pointer-events-none absolute top-1/2 left-0 z-10 -translate-x-full -translate-y-1/2 rounded-md bg-alveus-green-900 px-1 py-0.5 leading-tight text-nowrap text-white transition-opacity",
+          "pointer-events-none absolute top-1/2 z-10 -translate-x-full -translate-y-1/2 rounded-md bg-alveus-green-900 px-1 py-0.5 leading-tight text-nowrap text-white transition-opacity",
           tooltip.force ? "opacity-100" : "opacity-0 group-hover:opacity-100",
+          /\bbg-/.test(className || "") ? "-left-1" : "left-0",
         )}
       >
         {tooltip.text}
       </Tooltip>
       <button onClick={onClick} title={tooltip.text}>
-        <Icon className="m-1 inline size-5 cursor-pointer text-alveus-green-400 transition-colors group-hover:text-black" />
+        <Icon className="m-1 inline size-5 cursor-pointer transition-colors" />
       </button>
     </div>
   );
