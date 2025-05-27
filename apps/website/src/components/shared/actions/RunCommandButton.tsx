421e68b9c124b5463b6798e4cbed3c46b40a0c4c
MattIPv4 <me@mattcowley.co.uk>
2025-05-27 18:25:36 +0100

    Show preview in run command button tooltip

--- apps/website/src/components/shared/actions/RunCommandButton.tsx
+++ apps/website/src/components/shared/actions/RunCommandButton.tsx
@@ -1,16 +1,35 @@
 import { useSession } from "next-auth/react";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import type { z } from "zod";
 
 import type { runCommandSchema } from "@/server/trpc/router/stream";
 
 import { scopeGroups } from "@/data/twitch";
 
+import { classes } from "@/utils/classes";
 import { trpc } from "@/utils/trpc";
 
 import IconVideoCamera from "@/icons/IconVideoCamera";
 
 import ActionButton from "./ActionButton";
+
+const getCommandTooltip = (command: string, args?: string[]) => {
+  const CommandTooltip = ({
+    className,
+    children,
+  }: {
+    className: string;
+    children: string;
+  }) => (
+    <div className={classes(className, "flex flex-col")}>
+      <span>{children}</span>
+      <span className="font-mono text-xs text-alveus-green-300">
+        !{[command, ...(args ?? [])].join(" ")}
+      </span>
+    </div>
+  );
+  return CommandTooltip;
+};
 
 interface RunCommandButtonProps extends z.infer<typeof runCommandSchema> {
   subOnly?: boolean;
@@ -68,13 +87,22 @@ const RunCommandButton = ({
     }
   }, [status]);
 
+  const CommandTooltip = useMemo(
+    () => getCommandTooltip(command, args),
+    [command, args],
+  );
+
   if (!hasScopes || (subOnly && !subscription.data)) return null;
 
   return (
     <ActionButton
       onClick={onClick}
       icon={IconVideoCamera}
-      tooltip={{ text: statusText ?? "Run command", force: !!statusText }}
+      tooltip={{
+        text: statusText ?? "Run command",
+        elm: statusText ? undefined : CommandTooltip,
+        force: !!statusText,
+      }}
     />
   );
 };
