d95246c4ea7f7c7a655103297ffebbb70cb47813
MattIPv4 <me@mattcowley.co.uk>
2025-05-29 15:19:26 +0100

    Add hidden field to tRPC methods

--- apps/website/src/server/db/rounds-checks.ts
+++ apps/website/src/server/db/rounds-checks.ts
@@ -11,15 +11,16 @@ export const roundsCheckSchema = z.object({
   name: z.string().min(1),
   command: z.string().regex(SLUG_REGEX),
   ambassador: z.string(),
+  hidden: z.boolean(),
 });
 
 export type RoundsCheckSchema = z.infer<typeof roundsCheckSchema>;
 
-export const existingRoundsCheckSchema = roundsCheckSchema.and(
-  z.object({
+export const existingRoundsCheckSchema = z
+  .object({
     id: z.string().cuid(),
-  }),
-);
+  })
+  .merge(roundsCheckSchema.partial());
 
 export async function createRoundsCheck(
   input: z.infer<typeof roundsCheckSchema>,
@@ -54,14 +55,16 @@ export async function createRoundsCheck(
 export async function editRoundsCheck(
   input: z.infer<typeof existingRoundsCheckSchema>,
 ) {
-  const existingRoundsCheckWithCommand = await prisma.roundsCheck.findFirst({
-    where: { command: input.command, id: { not: input.id } },
-  });
-  if (existingRoundsCheckWithCommand)
-    throw new TRPCError({
-      code: "BAD_REQUEST",
-      message: "Command already exists",
+  if (typeof input.command === "string") {
+    const existingRoundsCheckWithCommand = await prisma.roundsCheck.findFirst({
+      where: { command: input.command, id: { not: input.id } },
     });
+    if (existingRoundsCheckWithCommand)
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "Command already exists",
+      });
+  }
 
   const { id, ...data } = input;
   return await prisma.roundsCheck.update({
