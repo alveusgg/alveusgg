9f1ddabb850a9dc40585666c06720b31682feed8
MattIPv4 <me@mattcowley.co.uk>
2025-06-15 16:28:10 +0100

    Use relative sizing for Storage component

--- apps/website/src/components/tech/servers/Storage.tsx
+++ apps/website/src/components/tech/servers/Storage.tsx
@@ -1,11 +1,12 @@
 import { classes } from "@/utils/classes";
 
-const led = "size-4 rounded-full inset-ring-2";
+const led =
+  "aspect-square rounded-full inset-ring-[0.1cqw] relative after:content-[''] after:absolute after:top-1/2 after:left-1/2 after:-translate-1/2 after:size-2/5 after:rounded-full after:brightness-150 after:opacity-75";
 
 const leds = {
-  green: "bg-green-500 inset-ring-green-700 saturate-200",
-  red: "bg-red-500 inset-ring-red-700 saturate-200",
-  blue: "bg-blue-500 inset-ring-blue-700 saturate-200",
+  green: "bg-green-500 inset-ring-green-700 saturate-200 after:bg-green-500",
+  red: "bg-red-500 inset-ring-red-700 saturate-200 after:bg-red-500",
+  blue: "bg-blue-500 inset-ring-blue-700 saturate-200 after:bg-blue-500",
   gray: "bg-gray-900 inset-ring-black",
 };
 
@@ -13,36 +14,46 @@ const Front = ({ size, drives }: { size: 1 | 2; drives?: boolean[] }) => {
   return (
     <div
       className={classes(
-        size === 1 ? "h-32" : "h-64",
-        "flex gap-1 rounded-sm bg-gray-400 p-1",
+        // Rack width is 19 inches, 1U is 1.75 inches, 2U is 3.5 inches
+        size === 1 ? "aspect-[19/1.75]" : "aspect-[19/3.5]",
+        "@container flex w-full gap-[0.25cqw] rounded-sm bg-gray-400 p-[0.25cqw]",
       )}
     >
-      <div className="flex w-12 flex-col">
-        <div className="mt-auto flex flex-col gap-2 p-1">
+      <div className="flex w-1/25 flex-col">
+        <div className="mt-auto flex w-2/5 flex-col gap-[0.5cqw] p-[0.25cqw]">
           <div className={classes(led, leds.green)} />
           <div className={classes(led, leds.red)} />
           <div className={classes(led, leds.gray)} />
         </div>
       </div>
-      <div className="grid grow grid-cols-4 gap-1">
+      <div
+        className={classes(
+          "grid grow grid-cols-4 gap-1",
+          size === 1 ? "grid-rows-[1fr_min-content]" : "grid-rows-3",
+        )}
+      >
         {size === 1 && (
-          <div className="col-span-full h-9 rounded-sm bg-gray-800" />
+          <div className="col-span-full h-full rounded-sm bg-gray-800" />
         )}
         {Array.from({ length: size === 1 ? 4 : 12 }, (_, i) => (
-          <div key={i} className="flex h-20 gap-2 rounded-sm bg-gray-800 p-2">
+          <div
+            key={i}
+            className="flex aspect-[4/1] gap-[0.5cqw] rounded-sm bg-gray-800 p-[0.5cqw]"
+          >
             <div className="grow rounded-sm bg-gray-900" />
-            <div className="flex flex-col gap-2">
-              {!drives || drives[i] ? (
-                <div className={classes(led, leds.blue)} />
-              ) : (
-                <div className={classes(led, leds.gray)} />
-              )}
+            <div className="flex w-1/20 flex-col gap-[0.5cqw]">
+              <div
+                className={classes(
+                  led,
+                  !drives || drives[i] ? leds.blue : leds.gray,
+                )}
+              />
               <div className={classes(led, leds.gray)} />
             </div>
           </div>
         ))}
       </div>
-      <div className="w-12" />
+      <div className="w-1/25" />
     </div>
   );
 };
