b8c9ad9872f0f037973e64edcf7096b879a82f1a
MattIPv4 <me@mattcowley.co.uk>
2025-07-01 18:09:05 +0100

    Add util for partial date diff

--- apps/website/src/hooks/today.ts
+++ apps/website/src/hooks/today.ts
@@ -1,11 +1,13 @@
-import { DateTime } from "luxon";
+import type { DateTime } from "luxon";
 import { useEffect, useState } from "react";
+
+import { getToday } from "@/utils/datetime";
 
 export default function useToday(timeZone?: string) {
   const [today, setToday] = useState<DateTime>();
 
   useEffect(() => {
-    setToday(DateTime.local({ zone: timeZone }).startOf("day"));
+    setToday(getToday(timeZone));
   }, [timeZone]);
 
   return today;
