8ff1e5c8bdcbd3834237f55e5144c1ec134ec477
MattIPv4 <me@mattcowley.co.uk>
2025-07-05 15:18:07 +0100

    Fix schema for fetching YouTube live

--- apps/website/src/server/apis/youtube.ts
+++ apps/website/src/server/apis/youtube.ts
@@ -105,24 +105,16 @@ const SearchSchema = z.object({
         channelTitle: z.string(),
         publishedAt: z.iso.datetime(),
         thumbnails: z.record(
-          z.union([
-            z.literal("default"),
-            z.literal("medium"),
-            z.literal("high"),
-            z.literal("standard"),
-            z.literal("maxres"),
-          ]),
-          z.object({
-            url: z.url(),
-            width: z.number(),
-            height: z.number(),
-          }),
+          z.literal(["default", "medium", "high", "standard", "maxres"]),
+          z
+            .object({
+              url: z.url(),
+              width: z.number(),
+              height: z.number(),
+            })
+            .optional(),
         ),
-        liveBroadcastContent: z.union([
-          z.literal("none"),
-          z.literal("upcoming"),
-          z.literal("live"),
-        ]),
+        liveBroadcastContent: z.literal(["none", "upcoming", "live"]),
       }),
     }),
   ),
