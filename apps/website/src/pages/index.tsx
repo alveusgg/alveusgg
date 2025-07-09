b6b569479d97fa6fa5ecb89813743ca1b417eb0a
MattIPv4 <me@mattcowley.co.uk>
2025-07-09 22:22:27 +0100

    Add news clips to about page w/ Lightbox + StreamEmbed

--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -425,64 +425,62 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
       </div>
 
       <Section dark>
-        <div className="flex flex-wrap items-center gap-y-8">
-          <Heading level={2} id="recent-videos" link>
-            Recent Videos
-          </Heading>
+        <Heading level={2} id="recent-videos" link>
+          Recent Videos
+        </Heading>
 
-          <div className="flex w-full flex-wrap justify-around gap-y-4">
-            {videos.map((video) => (
-              <div
-                key={video.id}
-                className="mx-auto flex basis-full flex-col items-center justify-start p-2 md:basis-1/2 lg:basis-1/4"
+        <div className="mt-8 flex w-full flex-wrap justify-around gap-y-4">
+          {videos.map((video) => (
+            <div
+              key={video.id}
+              className="mx-auto flex basis-full flex-col items-center justify-start p-2 md:basis-1/2 lg:basis-1/4"
+            >
+              <Heading
+                level={2}
+                className="order-3 my-0 px-1 text-center text-2xl"
               >
-                <Heading
-                  level={2}
-                  className="order-3 my-0 px-1 text-center text-2xl"
-                >
-                  {video.title}
-                </Heading>
+                {video.title}
+              </Heading>
 
+              <Link
+                href={`https://www.youtube.com/watch?v=${video.id}`}
+                external
+                onClick={(e) => {
+                  e.preventDefault();
+                  setLatestLightboxOpen(video.id);
+                }}
+                className="group/trigger order-1 w-full max-w-2xl"
+                custom
+              >
+                <YouTubePreview
+                  videoId={video.id}
+                  alt={video.title}
+                  className="aspect-video h-auto w-full"
+                />
+              </Link>
+
+              <div className="order-2 my-1 flex w-full flex-wrap items-center justify-between px-1">
+                <p className="text-sm leading-tight text-alveus-green-200">
+                  {formatDateTime(video.published, { style: "long" })}
+                </p>
                 <Link
-                  href={`https://www.youtube.com/watch?v=${video.id}`}
+                  href={video.author.uri}
                   external
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setLatestLightboxOpen(video.id);
-                  }}
-                  className="group/trigger order-1 w-full max-w-2xl"
                   custom
+                  className="block rounded-full bg-alveus-tan px-2 py-1 text-xs leading-tight text-alveus-green-700 transition-colors hover:bg-alveus-green-800 hover:text-alveus-tan"
                 >
-                  <YouTubePreview
-                    videoId={video.id}
-                    alt={video.title}
-                    className="aspect-video h-auto w-full"
-                  />
+                  {video.author.name}
                 </Link>
-
-                <div className="order-2 my-1 flex w-full flex-wrap items-center justify-between px-1">
-                  <p className="text-sm leading-tight text-alveus-green-200">
-                    {formatDateTime(video.published, { style: "long" })}
-                  </p>
-                  <Link
-                    href={video.author.uri}
-                    external
-                    custom
-                    className="block rounded-full bg-alveus-tan px-2 py-1 text-xs leading-tight text-alveus-green-700 transition-colors hover:bg-alveus-green-800 hover:text-alveus-tan"
-                  >
-                    {video.author.name}
-                  </Link>
-                </div>
               </div>
-            ))}
-          </div>
-
-          <Lightbox
-            open={latestLightboxOpen}
-            onClose={() => setLatestLightboxOpen(undefined)}
-            items={latestLightboxItems}
-          />
+            </div>
+          ))}
         </div>
+
+        <Lightbox
+          open={latestLightboxOpen}
+          onClose={() => setLatestLightboxOpen(undefined)}
+          items={latestLightboxItems}
+        />
       </Section>
 
       {/* Grow the last section to cover the page */}
