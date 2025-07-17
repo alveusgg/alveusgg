779738dfac9345eb515597241b0364ca1fce5850
MattIPv4 <me@mattcowley.co.uk>
2025-07-17 16:08:24 +0100

    Update Prisma client cache to use globalThis in dev

--- apps/database/src/index.ts
+++ apps/database/src/index.ts
@@ -10,27 +10,32 @@ const opts = {
 } as const satisfies Prisma.PrismaClientOptions;
 
 const createClient = () => new PrismaClient({ ...opts });
-let cachedClient: ReturnType<typeof createClient>;
-const getClient = () => {
-  cachedClient ??= createClient();
-  return cachedClient;
-};
-
 const createEdge = () =>
   new PrismaClient({
     ...opts,
     adapter: new PrismaPlanetScale({ url: process.env.DATABASE_URL }),
   });
-let cachedEdge: ReturnType<typeof createEdge>;
+
+// Use globalThis for caching in development to avoid hot reloading issues
+const cache = (process.env.NODE_ENV === "production" ? {} : globalThis) as {
+  client: ReturnType<typeof createClient>;
+  edge: ReturnType<typeof createEdge>;
+};
+
+const getClient = () => {
+  cache.client ??= createClient();
+  return cache.client;
+};
+
 const getEdge = () => {
-  cachedEdge ??= createEdge();
-  return cachedEdge;
+  cache.edge ??= createEdge();
+  return cache.edge;
 };
 
 // Readonly to reflect that the Proxy only exposes a getter
 type PrismaClientWithEdge = Readonly<
-  typeof cachedClient & {
-    get edge(): typeof cachedEdge;
+  typeof cache.client & {
+    get edge(): typeof cache.edge;
   }
 >;
 
