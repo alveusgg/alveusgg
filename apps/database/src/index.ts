69fccd81ed2fab156f6927b663f81f7ac547428c
MattIPv4 <me@mattcowley.co.uk>
2025-07-18 03:51:16 +0100

    Generate seperate edge client with dedicated import

--- apps/database/src/index.ts
+++ apps/database/src/index.ts
@@ -1,55 +1,8 @@
-import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
+import { PrismaClient } from "../prisma/generated/node/client";
 
-import { type Prisma, PrismaClient } from "../prisma/generated/client";
+import cached from "./utils/cached";
+import opts from "./utils/opts";
 
-const opts = {
-  log:
-    process.env.NODE_ENV === "development"
-      ? ["query", "error", "warn"]
-      : ["error"],
-} as const satisfies Prisma.PrismaClientOptions;
+export const prisma = cached(() => new PrismaClient({ ...opts }), "prisma");
 
-const createClient = () => new PrismaClient({ ...opts });
-const createEdge = () =>
-  new PrismaClient({
-    ...opts,
-    adapter: new PrismaPlanetScale({ url: process.env.DATABASE_URL }),
-  });
-
-// Use globalThis for caching in development to avoid hot reloading issues
-const cache = (process.env.NODE_ENV === "production" ? {} : globalThis) as {
-  client: ReturnType<typeof createClient>;
-  edge: ReturnType<typeof createEdge>;
-};
-
-const getClient = () => {
-  cache.client ??= createClient();
-  return cache.client;
-};
-
-const getEdge = () => {
-  cache.edge ??= createEdge();
-  return cache.edge;
-};
-
-// Readonly to reflect that the Proxy only exposes a getter
-type PrismaClientWithEdge = Readonly<
-  typeof cache.client & {
-    get edge(): typeof cache.edge;
-  }
->;
-
-// Use a Proxy to allow lazy initialization of both clients
-export const prisma = new Proxy(
-  {},
-  {
-    get: (_, prop: string) => {
-      if (prop === "edge") {
-        return getEdge();
-      }
-      return Reflect.get(getClient(), prop);
-    },
-  },
-) as PrismaClientWithEdge;
-
-export * from "../prisma/generated/client";
+export type * from "../prisma/generated/node/client";
