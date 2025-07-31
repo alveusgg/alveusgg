a17616980abc2472be423be4b8bacd60ec147e09
Matt Cowley <me@mattcowley.co.uk>
2025-07-31 21:23:24 +0100

    Add news clips to about Alveus page (#1352)
    * Refactor StreamEmbed + add StreamPreview
    
    * Add news clips to about page w/ Lightbox + StreamEmbed
    
    * Consistency across embed/preview components
    
    * Tweak homepage about working + link to about page
    
    * Add link to orgs page from alveus about page
    
    * Add link to orgs page from collaborations page

--- apps/website/src/components/content/YouTube.tsx
+++ apps/website/src/components/content/YouTube.tsx
@@ -12,6 +12,7 @@ type PreviewProps = {
   videoId: string;
   alt?: string;
   className?: string;
+  icon?: boolean;
 };
 
 const imgSrc = (id: string, type: string) =>
@@ -25,8 +26,9 @@ const imgSrc = (id: string, type: string) =>
 
 export const YouTubePreview = ({
   videoId,
-  alt = "YouTube video thumbnail",
+  alt = "Video thumbnail",
   className,
+  icon = true,
 }: PreviewProps) => {
   // Handle falling back to hq if there isn't a maxres image
   const [type, setType] = useState<"maxresdefault" | "hqdefault">(
@@ -45,15 +47,20 @@ export const YouTubePreview = ({
         alt={alt}
         loading="lazy"
         className={classes(
-          "pointer-events-none rounded-2xl bg-alveus-green-800 object-cover shadow-xl transition group-hover/trigger:scale-102 group-hover/trigger:shadow-2xl",
+          "pointer-events-none bg-alveus-green-800 object-cover shadow-xl transition group-hover/trigger:scale-102 group-hover/trigger:shadow-2xl",
+          !/\brounded-/.test(className || "") && "rounded-2xl",
           className,
         )}
       />
-      <div className="absolute inset-0 m-auto box-content aspect-[10/7] w-20 rounded-2xl bg-alveus-green/25 p-0.5 backdrop-blur-sm transition group-hover/trigger:scale-110 group-hover/trigger:bg-alveus-green/50" />
-      <IconYouTube
-        size={80}
-        className="absolute inset-0 m-auto text-white drop-shadow-md transition group-hover/trigger:scale-110 group-hover/trigger:drop-shadow-xl"
-      />
+      {icon && (
+        <>
+          <div className="absolute inset-0 m-auto box-content aspect-[10/7] w-20 rounded-2xl bg-alveus-green/25 p-0.5 backdrop-blur-sm transition group-hover/trigger:scale-110 group-hover/trigger:bg-alveus-green/50" />
+          <IconYouTube
+            size={80}
+            className="absolute inset-0 m-auto text-white drop-shadow-md transition group-hover/trigger:scale-110 group-hover/trigger:drop-shadow-xl"
+          />
+        </>
+      )}
     </div>
   );
 };
@@ -73,7 +80,7 @@ export const YouTubeEmbed = ({ videoId, caption }: EmbedProps) => (
     <div className="mx-auto flex aspect-video max-w-full grow">
       <iframe
         src={iframeSrc(videoId)}
-        title="YouTube video embed"
+        title="Video embed"
         referrerPolicy="no-referrer"
         allow="fullscreen; encrypted-media"
         sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox"
