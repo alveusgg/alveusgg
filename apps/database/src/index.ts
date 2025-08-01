c7b1a8084bb85ca7a48576ee062821f06fe12d33
MattIPv4 <me@mattcowley.co.uk>
2025-08-01 03:23:01 +0100

    Use MySQL/MariaDB adapter instead of Rust engine

--- apps/database/src/index.ts
+++ apps/database/src/index.ts
@@ -1,8 +1,19 @@
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "../prisma/generated/node/client";
 
 import cached from "./utils/cached";
 import opts from "./utils/opts";
 
-export const prisma = cached(() => new PrismaClient({ ...opts }), "prisma");
+const optsWithAdapter = {
+  ...opts,
+  adapter: new PrismaMariaDb(
+    process.env.DATABASE_URL!.replace("mysql://", "mariadb://"),
+  ),
+} as const;
+
+export const prisma = cached(
+  () => new PrismaClient({ ...optsWithAdapter }),
+  "prisma",
+);
 
 export type * from "../prisma/generated/node/client";
