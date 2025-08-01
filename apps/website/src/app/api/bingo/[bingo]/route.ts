af63a40276219b2793c662613dd40934cf789483
MattIPv4 <me@mattcowley.co.uk>
2025-08-01 02:29:04 +0100

    Use single import with edge-light in exports

--- apps/website/src/app/api/bingo/[bingo]/route.ts
+++ apps/website/src/app/api/bingo/[bingo]/route.ts
@@ -1,5 +1,3 @@
-import { prisma } from "@alveusgg/database/edge";
-
 import { findActiveBingo } from "@/server/db/bingos";
 
 import {
@@ -25,7 +23,7 @@ export async function GET(
   try {
     const { bingo: bingoSlugOrId } = await params;
 
-    const bingo = await findActiveBingo(prisma, bingoSlugOrId);
+    const bingo = await findActiveBingo(bingoSlugOrId);
     if (!bingo) {
       return new Response("Row not found", { status: 404 });
     }
