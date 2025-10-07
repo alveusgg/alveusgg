cde2b602af5ea869d330fa618767399e3147ca34
MattIPv4 <me@mattcowley.co.uk>
2025-10-07 01:04:24 +0100

    Add new hook for finding active item in nav structure

--- apps/website/src/hooks/active.ts
+++ apps/website/src/hooks/active.ts
@@ -1,5 +1,8 @@
 import type { LinkProps } from "next/link";
 import { useRouter } from "next/router";
+import { useMemo } from "react";
+
+import { mainNavStructure } from "@/data/navigation";
 
 const useIsActivePath = (href: LinkProps["href"], exact = false) => {
   const router = useRouter();
@@ -16,3 +19,35 @@ const useIsActivePath = (href: LinkProps["href"], exact = false) => {
 };
 
 export default useIsActivePath;
+
+const flatNavStructure = Object.values(mainNavStructure)
+  .flatMap((item) => ("dropdown" in item ? Object.values(item.dropdown) : item))
+  .map((item) => item.link);
+
+export const useActiveNav = () => {
+  const { pathname } = useRouter();
+
+  // Find the path with the most matching segments in the nav structure
+  return useMemo(
+    () =>
+      flatNavStructure.reduce(
+        ({ path, segments }, link) => {
+          const linkSegments = link.split("/").filter(Boolean);
+          const pathSegments = pathname.split("/").filter(Boolean);
+
+          const invalidSegment = linkSegments.findIndex(
+            (seg, i) => seg !== pathSegments[i],
+          );
+          const matchSegments =
+            invalidSegment === -1 ? linkSegments.length : invalidSegment;
+
+          if (matchSegments > segments) {
+            return { path: link, segments: matchSegments };
+          }
+          return { path, segments };
+        },
+        { path: "/", segments: 0 },
+      ).path,
+    [pathname],
+  );
+};
