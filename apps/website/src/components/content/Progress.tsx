c7b19a90e197fcb3e2db36807383dc06cf92dc98
MattIPv4 <me@mattcowley.co.uk>
2025-10-08 16:28:20 +0100

    Allow progress bar to be dark or light

--- apps/website/src/components/content/Progress.tsx
+++ apps/website/src/components/content/Progress.tsx
@@ -1,18 +1,37 @@
 import { classes } from "@/utils/classes";
 
 const barClasses =
-  "absolute inset-y-0 left-0 min-w-10 rounded-full border-4 border-alveus-green-900 transition-all duration-[2s] ease-in-out";
+  "absolute inset-y-0 left-0 min-w-10 rounded-full border-4 transition-all duration-[2s] ease-in-out";
 
-const Progress = ({ progress }: { progress: number }) => (
-  <div className="relative my-1 h-10 w-full rounded-full bg-alveus-green-900 shadow-lg">
+const Progress = ({
+  progress,
+  dark = false,
+  className,
+}: {
+  progress: number;
+  dark?: boolean;
+  className?: string;
+}) => (
+  <div
+    className={classes(
+      "relative my-1 h-10 w-full rounded-full shadow-lg",
+      dark ? "bg-alveus-green-900" : "bg-alveus-green-100",
+      className,
+    )}
+  >
     <div
-      className={classes(barClasses, "bg-alveus-green")}
+      className={classes(
+        barClasses,
+        dark ? "border-alveus-green-900" : "border-alveus-green-100",
+        "bg-alveus-green",
+      )}
       style={{ width: `${progress}%` }}
     />
 
     <div
       className={classes(
         barClasses,
+        dark ? "border-alveus-green-900" : "border-alveus-green-100",
         "bg-alveus-tan bg-gradient-to-r from-blue-800 to-green-600",
         progress === 0 ? "opacity-0" : "animate-pulse-slow",
       )}
