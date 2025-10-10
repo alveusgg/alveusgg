0b876054785d3c3d3ea43868ab0c1009dfcc77f4
MattIPv4 <me@mattcowley.co.uk>
2025-10-10 17:13:24 +0100

    Wrap Donate button in box w/ icon animation

--- apps/website/src/components/content/Donate.tsx
+++ apps/website/src/components/content/Donate.tsx
@@ -7,10 +7,13 @@ import Link from "@/components/content/Link";
 
 import { type IconProps } from "@/icons/BaseIcon";
 import IconAmazon from "@/icons/IconAmazon";
+import IconArrowUp from "@/icons/IconArrowUp";
 import IconBitcoin from "@/icons/IconBitcoin";
 import IconBox from "@/icons/IconBox";
 import IconPayPal from "@/icons/IconPayPal";
 import IconTwitch from "@/icons/IconTwitch";
+
+import Box from "./Box";
 
 type DonateLink = {
   icon: ComponentType<IconProps>;
@@ -70,20 +73,28 @@ const Donate = ({ type }: { type: Type }) => {
   const link = links[type];
 
   return (
-    <Link
-      href={link.link}
-      external={link.external}
-      custom
-      className="group rounded-xl bg-alveus-green p-4 text-alveus-tan shadow-xl transition hover:scale-102 hover:shadow-2xl"
-    >
-      <div className="mb-1 flex items-center gap-4">
-        <div className="block rounded-xl border-2 border-alveus-tan bg-alveus-tan p-2 text-alveus-green transition-colors group-hover:bg-alveus-green group-hover:text-alveus-tan">
-          <link.icon size={24} />
+    <Box dark className="group p-0 transition hover:scale-102 hover:shadow-2xl">
+      <Link
+        href={link.link}
+        external={link.external}
+        custom
+        className="block p-4"
+      >
+        <div className="mb-2 flex items-center gap-4">
+          <div className="relative block overflow-clip rounded-xl border-2 border-alveus-tan bg-alveus-tan p-2 text-alveus-green transition-colors group-hover:bg-alveus-green group-hover:text-alveus-tan">
+            <link.icon
+              size={24}
+              className="transition-opacity group-hover:opacity-0"
+            />
+            <IconArrowUp className="absolute inset-2 -translate-x-full translate-y-full rotate-45 opacity-0 transition group-hover:translate-x-0 group-hover:translate-y-0 group-hover:opacity-100" />
+          </div>
+          <Heading level={2} className="my-0">
+            {link.title}
+          </Heading>
         </div>
-        <Heading level={2}>{link.title}</Heading>
-      </div>
-      <p>{link.description}</p>
-    </Link>
+        <p>{link.description}</p>
+      </Link>
+    </Box>
   );
 };
 
