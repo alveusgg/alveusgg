214bd25672963cc27cd531bee2176d1f8543cf97
Matt Cowley <me@mattcowley.co.uk>
2025-10-22 23:03:19 +0100

    Update institute copy for Pixel Project being completed (#1565)
    * Remove pixels push from homepage CTA
    
    * Replace USFWS images w/ Alveus drawings in homepage CTA
    
    * Reword pixels CTA on institute page
    
    * Reword explainer text on pixels page
    
    * Add institute launch to history timeline
    
    * Move donation options below total on pixels page
    
    * Fix typo in history timeline

--- apps/website/src/pages/institute/index.tsx
+++ apps/website/src/pages/institute/index.tsx
@@ -1,8 +1,6 @@
 import { type NextPage } from "next";
 import Image from "next/image";
 
-import { PixelSyncProviderProvider } from "@/hooks/pixels";
-
 import Box from "@/components/content/Box";
 import Button from "@/components/content/Button";
 import Heading from "@/components/content/Heading";
@@ -10,11 +8,7 @@ import Meta from "@/components/content/Meta";
 import Section from "@/components/content/Section";
 import Slideshow from "@/components/content/Slideshow";
 import Transparency from "@/components/content/Transparency";
-import PixelsDescription from "@/components/institute/PixelsDescription";
-import PixelsProgress from "@/components/institute/PixelsProgress";
 import Wolves from "@/components/institute/Wolves";
-
-import IconArrowRight from "@/icons/IconArrowRight";
 
 import leafLeftImage1 from "@/assets/floral/leaf-left-1.png";
 import buildingHeroImage from "@/assets/institute/hero/building.png";
@@ -60,7 +54,7 @@ const slides = [
 
 const InstitutePage: NextPage = () => {
   return (
-    <PixelSyncProviderProvider>
+    <>
       <Meta
         title="Alveus Research & Recovery Institute"
         description="The Alveus Research & Recovery Institute is taking the Alveus approach to the wild to help save species from extinction, aiming to create a conservation breeding program for the critically endangered Mexican Gray and Red wolves."
@@ -90,53 +84,47 @@ const InstitutePage: NextPage = () => {
 
           <Box
             dark
-            className="mt-auto rounded-2xl bg-alveus-green-900/50 backdrop-blur-md"
+            className="mt-auto flex flex-col gap-x-4 gap-y-8 rounded-2xl bg-alveus-green-900/25 backdrop-blur-sm lg:flex-row"
           >
-            <div className="flex flex-col items-center gap-8 lg:flex-row">
-              <div>
-                <Heading id="pixels" level={2} link className="text-balance">
-                  Support the Institute with the Pixel Project
-                </Heading>
+            <div className="flex grow flex-col items-start gap-4">
+              <Heading id="pixels" level={2} link className="my-0 text-balance">
+                The Pixel Project is Complete!
+              </Heading>
 
-                <p className="text-lg">
-                  We need your help to fund the initial development of the
-                  Alveus Research & Recovery Institute. We&apos;re aiming to
-                  raise $1,000,000 to kickstart our first projects and build out
-                  the infrastructure needed to ensure long-term success. Every
-                  donation of $100 or more will unlock a pixel on our institute
-                  mural, allowing you to leave your mark on this exciting new
-                  venture.
-                </p>
+              <p className="text-lg text-balance">
+                Thanks to your incredible support, all 10,000 pixels in our
+                mural have been unlocked, raising $1,000,000 to fund the initial
+                development of the institute! While this milestone has been
+                reached, we still need your continued support to fund the
+                ongoing work at the Alveus Research & Recovery Institute. Every
+                donation helps us work towards our goal of saving species from
+                extinction.
+              </p>
 
-                <Button
-                  href="/institute/pixels"
-                  dark
-                  className="mt-4 inline-flex items-center gap-2"
-                >
-                  Donate Now to Unlock a Pixel
-                  <IconArrowRight className="size-5" />
+              <div className="flex flex-wrap gap-4">
+                <Button href="/institute/pixels" dark>
+                  Explore the Completed Mural
                 </Button>
-              </div>
 
-              <div className="z-0 flex w-full max-w-lg items-center justify-center lg:order-first">
-                <Image
-                  src={usfwsRedWolfImage}
-                  width={256}
-                  alt="Red wolf, B. Bartel/USFWS, Public Domain, https://www.fws.gov/media/red-wolf-7"
-                  className="z-10 mr-[-10%] h-auto w-2/5 max-w-64 rounded-2xl shadow-lg transition-all hover:scale-102 hover:shadow-xl"
-                />
-                <Image
-                  src={usfwsMexicanWolfReleasedImage}
-                  width={384}
-                  alt="Mexican wolf released back into the wild, Mexican Wolf Interagency Field Team, Public Domain, https://www.fws.gov/media/mexican-wolf-released-back-wild"
-                  className="h-auto w-3/5 max-w-96 rounded-2xl shadow-lg transition-all hover:scale-102 hover:shadow-xl"
-                />
+                <Button href="/donate" dark>
+                  Donate to Support the Institute
+                </Button>
               </div>
             </div>
 
-            <div className="mt-8 flex flex-col items-center gap-x-4 gap-y-1 lg:flex-row">
-              <PixelsProgress className="bg-alveus-green-900/75 lg:flex-1" />
-              <PixelsDescription />
+            <div className="z-0 flex w-full items-center justify-center lg:order-first lg:max-w-lg">
+              <Image
+                src={usfwsRedWolfImage}
+                width={300}
+                alt="Red wolf, B. Bartel/USFWS, Public Domain, https://www.fws.gov/media/red-wolf-7"
+                className="z-10 mr-[-10%] h-auto w-2/5 rounded-2xl shadow-lg transition-all hover:scale-102 hover:shadow-xl"
+              />
+              <Image
+                src={usfwsMexicanWolfReleasedImage}
+                width={400}
+                alt="Mexican wolf released back into the wild, Mexican Wolf Interagency Field Team, Public Domain, https://www.fws.gov/media/mexican-wolf-released-back-wild"
+                className="h-auto w-3/5 rounded-2xl shadow-lg transition-all hover:scale-102 hover:shadow-xl"
+              />
             </div>
           </Box>
         </div>
@@ -186,7 +174,7 @@ const InstitutePage: NextPage = () => {
 
         <Transparency className="grow" />
       </div>
-    </PixelSyncProviderProvider>
+    </>
   );
 };
 
