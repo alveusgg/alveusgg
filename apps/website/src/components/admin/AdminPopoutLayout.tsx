c990714d527522ea7e9c1335dfedb667c6ac9abd
Paul Golmann <mail@pje-web.de>
2025-10-31 22:50:56 +0100

    Move permissions check for admin popouts to layout component

--- apps/website/src/components/admin/AdminPopoutLayout.tsx
+++ apps/website/src/components/admin/AdminPopoutLayout.tsx
@@ -1,20 +1,41 @@
 import { signOut, useSession } from "next-auth/react";
 import type { ReactNode } from "react";
 
+import {
+  type PermissionConfig,
+  checkRolesGivePermission,
+} from "@/data/permissions";
+
 import useTheme from "@/hooks/theme";
 
 import { LoginWithTwitchButton } from "@/components/shared/LoginWithTwitchButton";
+import { MessageBox } from "@/components/shared/MessageBox";
 
 import IconMoon from "@/icons/IconMoon";
 import IconSun from "@/icons/IconSun";
 
-function AdminPopoutLayout({ children }: { children: ReactNode }) {
+function AdminPopoutLayout({
+  title,
+  children,
+  needsPermission,
+}: {
+  title?: ReactNode;
+  children: ReactNode;
+  needsPermission?: PermissionConfig;
+}) {
   const session = useSession();
   const [theme, toggleTheme] = useTheme();
+  const user = session.data?.user;
+
+  const hasPermission =
+    !needsPermission ||
+    (user &&
+      (user.isSuperUser ||
+        checkRolesGivePermission(user.roles, needsPermission)));
 
   return (
     <div className="h-full min-h-screen w-full bg-white text-black dark:bg-gray-900 dark:text-white">
-      <div className="flex flex-row justify-between p-1">
+      <div className="flex flex-row items-center justify-between p-1">
         <button
           type="button"
           onClick={toggleTheme}
@@ -23,6 +44,7 @@ function AdminPopoutLayout({ children }: { children: ReactNode }) {
         >
           {theme === "light" ? <IconMoon /> : <IconSun />}
         </button>
+        <div className="shrink overflow-hidden text-ellipsis">{title}</div>
         <div className="flex w-fit shrink flex-row items-center gap-2 px-2">
           {session.status === "unauthenticated" ? (
             <LoginWithTwitchButton />
@@ -39,7 +61,23 @@ function AdminPopoutLayout({ children }: { children: ReactNode }) {
         </div>
       </div>
 
-      {children}
+      {session.status === "authenticated" ? (
+        hasPermission ? (
+          children
+        ) : (
+          <MessageBox className="m-4">
+            <p className="mb-4">
+              You do not have permission to view this popout!
+            </p>
+          </MessageBox>
+        )
+      ) : (
+        <MessageBox className="m-4">
+          <p className="mb-4">You need to be logged in with Twitch.</p>
+
+          <LoginWithTwitchButton />
+        </MessageBox>
+      )}
     </div>
   );
 }
--- apps/website/src/pages/admin/donations/popout.tsx
+++ apps/website/src/pages/admin/donations/popout.tsx
@@ -1,41 +1,17 @@
 import { type NextPage } from "next";
-import { useSession } from "next-auth/react";
 
-import { checkRolesGivePermission, permissions } from "@/data/permissions";
+import { permissions } from "@/data/permissions";
 
 import AdminPopoutLayout from "@/components/admin/AdminPopoutLayout";
 import { DonationFeed } from "@/components/admin/donations/DonationFeed";
-import { LoginWithTwitchButton } from "@/components/shared/LoginWithTwitchButton";
-import { MessageBox } from "@/components/shared/MessageBox";
 
 const DonationsPopoutPage: NextPage = () => {
-  const session = useSession();
-  const user = session.data?.user;
-
-  const canViewDonations =
-    user &&
-    (user.isSuperUser ||
-      checkRolesGivePermission(user.roles, permissions.manageDonations));
-
   return (
-    <AdminPopoutLayout>
-      {session.status === "authenticated" ? (
-        canViewDonations ? (
-          <DonationFeed />
-        ) : (
-          <MessageBox className="m-4">
-            <p className="mb-4">
-              You do not have permission to view donations.
-            </p>
-          </MessageBox>
-        )
-      ) : (
-        <MessageBox className="m-4">
-          <p className="mb-4">You need to be logged in with Twitch.</p>
-
-          <LoginWithTwitchButton />
-        </MessageBox>
-      )}
+    <AdminPopoutLayout
+      title="Donations"
+      needsPermission={permissions.manageDonations}
+    >
+      <DonationFeed />
     </AdminPopoutLayout>
   );
 };
