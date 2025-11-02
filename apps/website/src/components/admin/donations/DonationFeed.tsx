321250624f48be21f4966bdcd5ec251e51d22be3
Paul Golmann <mail@pje-web.de>
2025-11-02 14:53:29 +0100

    Scale donations feed down a bit

--- apps/website/src/components/admin/donations/DonationFeed.tsx
+++ apps/website/src/components/admin/donations/DonationFeed.tsx
@@ -76,7 +76,7 @@ export function DonationFeed() {
   }, [donationsQuery]);
 
   return (
-    <div className="w-full text-lg tabular-nums">
+    <div className="w-full tabular-nums">
       <div className="flex w-full flex-row gap-2 border-t border-b border-gray-400 px-2">
         <div className="grow text-center text-xl">
           {/* <PixelsDescription /> */}
