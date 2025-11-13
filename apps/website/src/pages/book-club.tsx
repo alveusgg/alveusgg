6a373bfc900509e120e7326f93307655e70dd8e6
MattIPv4 <me@mattcowley.co.uk>
2025-11-13 01:08:01 +0000

    Add placeholder book covers for missing months

--- apps/website/src/pages/book-club.tsx
+++ apps/website/src/pages/book-club.tsx
@@ -7,7 +7,7 @@ import { type NextPage } from "next";
 import Image from "next/image";
 import { type ReactNode, useState } from "react";
 
-import books, { type BookInfo } from "@/data/books";
+import books, { type BookInfo, type Month } from "@/data/books";
 
 import { classes } from "@/utils/classes";
 import { formatPartialDateString } from "@/utils/datetime-partial";
@@ -29,6 +29,43 @@ import bookClubLogo from "@/assets/book-club/logo.png";
 import leafLeftImage1 from "@/assets/floral/leaf-left-1.png";
 import leafLeftImage3 from "@/assets/floral/leaf-left-3.png";
 import leafRightImage2 from "@/assets/floral/leaf-right-2.png";
+
+type BookOrPlaceholder =
+  | ({ type: "book" } & BookInfo)
+  | { type: "placeholder"; month: Month };
+
+const booksWithPlaceholders = books.reduce<BookOrPlaceholder[]>(
+  (acc, book, index) => {
+    acc.push({ type: "book", ...book });
+
+    // Get the next book's month
+    const nextMonth = books[index + 1]?.month;
+    if (nextMonth) {
+      // Parse the current and next months
+      const currentDate = new Date(`${book.month}-01`);
+      const nextDate = new Date(`${nextMonth}-01`);
+
+      // Add cover placeholders for each missing month
+      const monthDiff =
+        (currentDate.getFullYear() - nextDate.getFullYear()) * 12 +
+        (currentDate.getMonth() - nextDate.getMonth());
+      for (let i = 1; i < monthDiff; i++) {
+        const date = new Date(
+          currentDate.getFullYear(),
+          currentDate.getMonth() - i,
+          1,
+        );
+        const monthString =
+          `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}` as Month;
+
+        acc.push({ type: "placeholder", month: monthString });
+      }
+    }
+
+    return acc;
+  },
+  [],
+);
 
 const lightboxItems = books.reduce<Record<string, ReactNode>>(
   (acc, book) =>
@@ -266,32 +303,45 @@ const BookClubPage: NextPage = () => {
 
         <Section className="grow">
           <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-4">
-            {books.map((book) => (
-              <Book
-                key={book.title}
-                {...book}
-                lightbox={setLightboxOpen}
-                width={256}
-                className="mx-auto w-64"
-              />
-            ))}
-
-            {/* Pad with placeholders to a multiple of 4 */}
-            {Array.from({ length: (4 - (books.length % 4)) % 4 }).map(
-              (_, i) => (
-                <div
-                  key={i}
-                  className={classes(
-                    "group mx-auto hidden w-64",
-                    // If we have an even number of books, show on desktop only
-                    // Otherwise, show on desktop except the first which is shown on tablet
-                    books.length % 2 === 0 || i > 0 ? "xl:block" : "md:block",
-                  )}
-                >
+            {booksWithPlaceholders.map((book, i) =>
+              book.type === "placeholder" ? (
+                <div key={`placeholder-${i}`} className="mx-auto w-64">
                   <Cover />
+
+                  <Heading level={2} className="relative mt-4 mb-0">
+                    <div className="absolute -top-1 left-0 h-1 w-16 bg-alveus-green/50" />
+                    <span className="opacity-25">
+                      {formatPartialDateString(book.month)}
+                    </span>
+                  </Heading>
                 </div>
+              ) : (
+                <Book
+                  key={book.title}
+                  {...book}
+                  lightbox={setLightboxOpen}
+                  width={256}
+                  className="mx-auto w-64"
+                />
               ),
             )}
+
+            {/* Pad with placeholders to a multiple of 4 */}
+            {Array.from({
+              length: (4 - (booksWithPlaceholders.length % 4)) % 4,
+            }).map((_, i) => (
+              <div
+                key={i}
+                className={classes(
+                  "mx-auto hidden w-64",
+                  // If we have an even number of books, show on desktop only
+                  // Otherwise, show on desktop except the first which is shown on tablet
+                  books.length % 2 === 0 || i > 0 ? "xl:block" : "md:block",
+                )}
+              >
+                <Cover />
+              </div>
+            ))}
           </div>
 
           <Lightbox
