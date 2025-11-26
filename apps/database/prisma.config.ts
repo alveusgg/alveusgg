a69cc77e8dc13fc6f094c3bbd7ba4486d3f373c5
MattIPv4 <me@mattcowley.co.uk>
2025-11-26 18:41:11 +0000

    Add Prisma config file with urls

---
+++ apps/database/prisma.config.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  datasource: {
+    url: env("DATABASE_URL"),
+    shadowDatabaseUrl: env("SHADOW_DATABASE_URL"),
+  },
+});
