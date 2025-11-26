cd89666fd9ac8a70593f0c7f7bc4e81a73d4ff5a
MattIPv4 <me@mattcowley.co.uk>
2025-11-26 18:55:13 +0000

    Don't rely on Prisma's `env` method

--- apps/database/prisma.config.ts
+++ apps/database/prisma.config.ts
@@ -1,10 +1,11 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "src/schema.prisma",
   datasource: {
-    url: env("DATABASE_URL"),
-    shadowDatabaseUrl: env("SHADOW_DATABASE_URL"),
+    // Don't use Prisma's `env` method here, as it makes these required which breaks CI builds
+    url: process.env.DATABASE_URL ?? "",
+    shadowDatabaseUrl: process.env.SHADOW_DATABASE_URL,
   },
 });
