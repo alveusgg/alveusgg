cc0d7bc992d91a7254648e8c6b3166833f291ad8
MattIPv4 <me@mattcowley.co.uk>
2025-11-26 18:44:26 +0000

    Move Prisma schema into src directory

--- apps/database/prisma.config.ts
+++ apps/database/prisma.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "src/schema.prisma",
   datasource: {
     url: env("DATABASE_URL"),
     shadowDatabaseUrl: env("SHADOW_DATABASE_URL"),
