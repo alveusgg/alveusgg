cd94b2077db50125363e1bcc46513893517f65c5
MattIPv4 <me@mattcowley.co.uk>
2026-01-24 22:13:12 +0000

    Abstract overlay disclaimer to generic text

--- apps/website/src/pages/stream/overlay.tsx
+++ apps/website/src/pages/stream/overlay.tsx
@@ -13,12 +13,12 @@ import useChat from "@/hooks/chat";
 import Crunch from "@/components/overlay/Crunch";
 import Cycle from "@/components/overlay/Cycle";
 import Datetime from "@/components/overlay/Datetime";
-import Disclaimer from "@/components/overlay/Disclaimer";
 import Event from "@/components/overlay/Event";
 import Raid from "@/components/overlay/Raid";
 import Rounds from "@/components/overlay/Rounds";
 import Socials from "@/components/overlay/Socials";
 import Subs from "@/components/overlay/Subs";
+import Text from "@/components/overlay/Text";
 import Timecode from "@/components/overlay/Timecode";
 import Weather from "@/components/overlay/Weather";
 
@@ -28,6 +28,8 @@ import border6camChristmas from "@/assets/stream/border-6cam-christmas.png";
 import border6cam from "@/assets/stream/border-6cam.png";
 
 const cycleTime = 60;
+const disclaimerText =
+  "The rescued animals on screen are educational ambassadors, not pets!";
 
 const layouts = ["fullscreen", "4cam", "6cam"] as const;
 type Layout = (typeof layouts)[number];
@@ -212,12 +214,14 @@ const OverlayPage: NextPage = () => {
             )}
 
             {index === 0 && !hide.has("disclaimer") && disclaimer && (
-              <Disclaimer
+              <Text
                 className={classes(
                   "absolute inset-x-0 bottom-0 p-6 text-4xl",
                   layout === "fullscreen" && "text-center",
                 )}
-              />
+              >
+                {disclaimerText}
+              </Text>
             )}
           </div>
         ))}
@@ -267,7 +271,9 @@ const OverlayPage: NextPage = () => {
           () =>
             [
               !hide.has("disclaimer") && !disclaimer && (
-                <Disclaimer key="disclaimer" className="text-xl text-stroke" />
+                <Text key="disclaimer" className="text-xl text-stroke">
+                  {disclaimerText}
+                </Text>
               ),
               !hide.has("subs") && <Subs key="subs" />,
             ].filter((x) => !!x),
