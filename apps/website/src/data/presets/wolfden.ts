a3efc83605bd6ffcc18d0d3fb415551bf5fc01d5
Alveus.gg <opensource@alveussanctuary.org>
2026-02-16 01:06:33 +0000

    Update presets data (#1864)
    Co-authored-by: MattIPv4 <12371363+MattIPv4@users.noreply.github.com>

--- apps/website/src/data/presets/wolfden.ts
+++ apps/website/src/data/presets/wolfden.ts
@@ -1,4 +1,5 @@
 import home from "@/assets/presets/wolfden/home.png";
+import leftcorner from "@/assets/presets/wolfden/leftcorner.png";
 import rightcorner from "@/assets/presets/wolfden/rightcorner.png";
 
 import type { Preset } from "../tech/cameras.types";
@@ -9,6 +10,12 @@ const wolfdenPresets: Record<string, Preset> = {
     image: home,
     position: { pan: 0, tilt: 0, zoom: 1 },
     // modified: 2025-10-09T11:10:30.380Z
+  },
+  leftcorner: {
+    description: "leftcorner",
+    image: leftcorner,
+    position: { pan: -1.02, tilt: 103.17, zoom: 6256 },
+    // modified: 2026-02-15T12:29:51.104Z
   },
   rightcorner: {
     description: "Right Corner",
