39d2f7b940dd546677b217858ad10ac97612bf0b
Alveus.gg <opensource@alveussanctuary.org>
2026-03-07 01:05:30 +0000

    Update presets data (#1913)
    Co-authored-by: MattIPv4 <12371363+MattIPv4@users.noreply.github.com>

--- apps/website/src/data/presets/foxcovered.ts
+++ apps/website/src/data/presets/foxcovered.ts
@@ -1,6 +1,7 @@
 import home from "@/assets/presets/foxcovered/home.png";
 import left from "@/assets/presets/foxcovered/left.png";
 import platform from "@/assets/presets/foxcovered/platform.png";
+import rampb from "@/assets/presets/foxcovered/rampb.png";
 import right from "@/assets/presets/foxcovered/right.png";
 
 import type { Preset } from "../tech/cameras.types";
@@ -23,6 +24,12 @@ const foxcoveredPresets: Record<string, Preset> = {
     image: platform,
     position: { pan: -130.47, tilt: 0, zoom: 500 },
     // modified: 2026-03-05T14:23:44.204Z
+  },
+  rampb: {
+    description: "rampb",
+    image: rampb,
+    position: { pan: -130.21, tilt: -4.86, zoom: 2185 },
+    // modified: 2026-03-06T02:44:06.603Z
   },
   right: {
     description: "right",
