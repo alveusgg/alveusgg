0291ea051a66c52bf53d7bfa0f4f01c3278fb6dd
MattIPv4 <me@mattcowley.co.uk>
2026-03-30 01:46:13 +0100

    Split out PTZ preset specific processing

--- apps/website/build-scripts/update-presets.ts
+++ apps/website/build-scripts/update-presets.ts
@@ -3,7 +3,11 @@ import { mkdir, unlink, writeFile } from "node:fs/promises";
 import { dirname } from "node:path";
 
 import {
+  type CodeBlockWriter,
+  type ObjectLiteralElementLike,
+  type ObjectLiteralExpression,
   Project,
+  type SourceFile,
   StructureKind,
   SyntaxKind,
   VariableDeclarationKind,
@@ -20,7 +24,7 @@ const upstreamCameraMap: Record<string, string> = {
   macaw: "macaws",
 };
 
-const upstreamPresetsSchema = z.object({
+const upstreamCameraSchema = z.object({
   name: z.string().toLowerCase(),
   presets: z.array(
     z.object({
@@ -36,34 +40,32 @@ const upstreamPresetsSchema = z.object({
   ),
 });
 
-type UpstreamPresets = z.infer<typeof upstreamPresetsSchema>;
+type UpstreamCamera = z.infer<typeof upstreamCameraSchema>;
 
-const getUpstreamPresets = async () => {
+const getUpstreamCameras = async () => {
   const response = await fetch("https://ptz.app/api/presets-ext");
   if (!response.ok) {
     throw new Error(
-      `Failed to fetch upstream presets: ${response.status} ${response.statusText}`,
+      `Failed to fetch upstream cameras: ${response.status} ${response.statusText}`,
     );
   }
 
   const data = await response.json();
-  return z.array(upstreamPresetsSchema).parse(data);
+  return z.array(upstreamCameraSchema).parse(data);
 };
 
-const getUpstreamImage = async (camera: string, preset: string) => {
-  const response = await fetch(
-    `https://ptz.app/images/button-img/${encodeURIComponent(camera)}/${encodeURIComponent(preset)}.png`,
-  );
+const getUpstreamImage = async (path: string) => {
+  const response = await fetch(`https://ptz.app/images/${path}`);
   if (!response.ok) {
     throw new Error(
-      `Failed to fetch upstream image for ${camera} ${preset}: ${response.status} ${response.statusText}`,
+      `Failed to fetch upstream image ${path}: ${response.status} ${response.statusText}`,
     );
   }
 
   return response.arrayBuffer();
 };
 
-const processCamera = async (project: Project, camera: UpstreamPresets) => {
+const processCamera = async (project: Project, camera: UpstreamCamera) => {
   const name = upstreamCameraMap[camera.name] || camera.name;
   const file = project.addSourceFileAtPathIfExists(
     `src/data/presets/${name}.ts`,
@@ -88,7 +90,6 @@ const processCamera = async (project: Project, camera: UpstreamPresets) => {
               writer.block(() => {
                 writer.writeLine(`title: "${sentenceToTitle(name)}",`);
                 writer.writeLine(`group: "${name}",`);
-                writer.writeLine(`presets: ${name}Presets,`);
               });
             },
           },
@@ -119,6 +120,126 @@ const processCamera = async (project: Project, camera: UpstreamPresets) => {
     .setOrder(file.getStatements().length - 1);
   exportAssignment.appendWhitespace((writer) => writer.newLine().newLine());
   exportAssignment.setOrder(file.getStatements().length - 1);
+
+  const added = await processCameraPTZ(camera, file, objCamera);
+
+  // Remove any imports that are no longer used
+  const removed = file
+    .getImportDeclarations()
+    .map((imp) => {
+      const importDefault = imp.getDefaultImport();
+      if (
+        !importDefault ||
+        importDefault
+          .findReferencesAsNodes()
+          .filter((ref) => ref !== importDefault).length > 0
+      ) {
+        return null;
+      }
+
+      const importPath = imp.getModuleSpecifierValue();
+      imp.remove();
+      return importPath;
+    })
+    .filter((imp) => imp !== null);
+
+  return { added, removed };
+};
+
+const processImage = async (
+  file: SourceFile,
+  object: ObjectLiteralExpression,
+  image: { name: string; upstream: string; local: string; modified: Date },
+  initializer?: (
+    writer: CodeBlockWriter,
+    existing?: ObjectLiteralElementLike,
+  ) => void,
+) => {
+  let added: [string, ArrayBuffer] | undefined = undefined;
+
+  // Try to find an existing entry for the preset
+  const existing = object.getProperty(image.name);
+  const modified = existing
+    ?.getDescendantsOfKind(SyntaxKind.SingleLineCommentTrivia)
+    .map((comment) => {
+      const text = comment.getText();
+      if (!text.startsWith("// modified: ")) return null;
+
+      const dateStr = text.replace("// modified: ", "").trim();
+      const date = new Date(dateStr);
+      if (isNaN(date.getTime())) return null;
+
+      return {
+        comment,
+        date,
+      };
+    })
+    .find((date) => date !== null);
+
+  // If we don't have a modified date, or the upstream is newer, download the image
+  if (!modified || modified.date < image.modified) {
+    // Download the image from the upstream API and add an import for it
+    // Intentionally use the original camera name here for the upstream API, not the mapped name
+    const data = await getUpstreamImage(image.upstream);
+    const imp = file.addImportDeclaration({
+      moduleSpecifier: `@/assets/presets/${image.local}`,
+      defaultImport: image.name,
+    });
+    added = [imp.getModuleSpecifierValue(), data];
+
+    // If there was already an existing entry, we don't need to duplicate the import
+    if (existing) {
+      imp.remove();
+    }
+  }
+
+  // Get the old description if it exists
+  const description = existing
+    ? existing
+        .getFirstChildByKindOrThrow(SyntaxKind.ObjectLiteralExpression)
+        .getPropertyOrThrow("description")
+        .getFirstChildByKindOrThrow(SyntaxKind.StringLiteral)
+        .getText()
+    : JSON.stringify(image.name);
+
+  // Add a new property for the preset
+  object.addPropertyAssignment({
+    name: image.name,
+    initializer: (writer) => {
+      writer.block(() => {
+        writer.writeLine(`description: ${description},`);
+        writer.writeLine(`image: ${image.name},`);
+
+        initializer?.(writer, existing);
+
+        writer.writeLine(`// modified: ${image.modified?.toISOString()}`);
+      });
+    },
+  });
+
+  // Remove the old preset property if it exists, now that we've used it for the description
+  if (existing) {
+    existing.remove();
+  }
+
+  return added;
+};
+
+const processCameraPTZ = async (
+  camera: UpstreamCamera,
+  file: SourceFile,
+  objCamera: ObjectLiteralExpression,
+) => {
+  const name = upstreamCameraMap[camera.name] || camera.name;
+
+  // Ensure the camera object has the required presets property
+  const propPresets = objCamera.getProperty("presets");
+  if (!propPresets) {
+    objCamera.addPropertyAssignment({
+      name: "presets",
+      initializer: `${name}Presets`,
+    });
+  }
 
   // Find, or create, the presets object declaration
   let objPresets = file
@@ -173,83 +294,34 @@ const processCamera = async (project: Project, camera: UpstreamPresets) => {
       continue;
     }
 
-    // Try to find an existing entry for the preset
-    const existing = objPresets.getProperty(preset.name);
-    const modified = existing
-      ?.getDescendantsOfKind(SyntaxKind.SingleLineCommentTrivia)
-      .map((comment) => {
-        const text = comment.getText();
-        if (!text.startsWith("// modified: ")) return null;
-
-        const dateStr = text.replace("// modified: ", "").trim();
-        const date = new Date(dateStr);
-        if (isNaN(date.getTime())) return null;
-
-        return {
-          comment,
-          date,
-        };
-      })
-      .find((date) => date !== null);
-
     try {
-      // If we don't have a modified date, or the upstream is newer, download the image
-      if (!modified || modified.date < preset.modified) {
-        // Download the preset image from the upstream API and add an import for it
-        // Intentionally use the original camera name here for the upstream API, not the mapped name
-        const data = await getUpstreamImage(camera.name, preset.name);
-        const imp = file.addImportDeclaration({
-          moduleSpecifier: `@/assets/presets/${name}/${preset.name}.png`,
-          defaultImport: preset.name,
-        });
-        added.push([imp.getModuleSpecifierValue(), data]);
-
-        // If there was already an existing entry, we don't need to duplicate the import
-        if (existing) {
-          imp.remove();
-        }
-      }
-
-      // Get the old description if it exists
-      const description = existing
-        ? existing
-            .getFirstChildByKindOrThrow(SyntaxKind.ObjectLiteralExpression)
-            .getPropertyOrThrow("description")
-            .getFirstChildByKindOrThrow(SyntaxKind.StringLiteral)
-            .getText()
-        : JSON.stringify(preset.name);
-
-      // Add a new property for the preset
-      objPresets.addPropertyAssignment({
-        name: preset.name,
-        initializer: (writer) => {
-          writer.block(() => {
-            writer.writeLine(`description: ${description},`);
-            writer.writeLine(`image: ${preset.name},`);
-
-            if (
-              preset.pan !== undefined &&
-              preset.tilt !== undefined &&
-              preset.zoom !== undefined
-            ) {
-              writer.writeLine(
-                `position: { pan: ${preset.pan}, tilt: ${preset.tilt}, zoom: ${preset.zoom} },`,
-              );
-            } else {
-              console.warn(
-                `Preset ${preset.name} for camera ${name} is missing position data`,
-              );
-            }
-
-            writer.writeLine(`// modified: ${preset.modified?.toISOString()}`);
-          });
+      const result = await processImage(
+        file,
+        objPresets,
+        {
+          name: preset.name,
+          upstream: `button-img/${encodeURIComponent(camera.name)}/${encodeURIComponent(preset.name)}.png`,
+          local: `${name}/${preset.name}.png`,
+          modified: preset.modified,
         },
-      });
+        (writer) => {
+          if (
+            preset.pan !== undefined &&
+            preset.tilt !== undefined &&
+            preset.zoom !== undefined
+          ) {
+            writer.writeLine(
+              `position: { pan: ${preset.pan}, tilt: ${preset.tilt}, zoom: ${preset.zoom} },`,
+            );
+          } else {
+            console.warn(
+              `Preset ${preset.name} for camera ${name} is missing position data`,
+            );
+          }
+        },
+      );
 
-      // Remove the old preset property if it exists, now that we've used it for the description
-      if (existing) {
-        existing.remove();
-      }
+      if (result) added.push(result);
     } catch (error) {
       // Log an error on failure but continue processing other presets
       // We should only hit this if the upstream API does not have an image for the preset
@@ -288,36 +360,16 @@ const processCamera = async (project: Project, camera: UpstreamPresets) => {
     homePreset.remove();
   }
 
-  // Remove any imports that are no longer used
-  const removed = file
-    .getImportDeclarations()
-    .map((imp) => {
-      const importDefault = imp.getDefaultImport();
-      if (
-        !importDefault ||
-        importDefault
-          .findReferencesAsNodes()
-          .filter((ref) => ref !== importDefault).length > 0
-      ) {
-        return null;
-      }
-
-      const importPath = imp.getModuleSpecifierValue();
-      imp.remove();
-      return importPath;
-    })
-    .filter((imp) => imp !== null);
-
-  return { added, removed };
+  return added;
 };
 
 const main = async () => {
   const project = new Project();
 
   // Update all the preset data files for each camera from the upstream API
-  const upstreamPresets = await getUpstreamPresets();
+  const upstreamCameras = await getUpstreamCameras();
   const { added, removed } = await Promise.all(
-    upstreamPresets.map((camera) => processCamera(project, camera)),
+    upstreamCameras.map((camera) => processCamera(project, camera)),
   ).then((results) =>
     results.reduce<{ added: [string, ArrayBuffer][]; removed: string[] }>(
       (acc, curr) => {
@@ -333,7 +385,7 @@ const main = async () => {
 
   // Warn about any local preset files that don't have a corresponding upstream camera
   const cameras = new Set(
-    upstreamPresets.map(
+    upstreamCameras.map(
       (camera) => upstreamCameraMap[camera.name] || camera.name,
     ),
   );
