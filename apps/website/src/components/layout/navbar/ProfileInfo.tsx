3d8318d9d61b43e27d7a172c8eed49eb4770c767
Paul Golmann <mail@pje-web.de>
2026-04-25 01:03:56 +0200

    Cleanup navbar components

--- apps/website/src/components/layout/navbar/ProfileInfo.tsx
+++ apps/website/src/components/layout/navbar/ProfileInfo.tsx
@@ -1,41 +1,44 @@
 import { useSession } from "next-auth/react";
 
+import { classes } from "@/utils/classes";
+
 export const ProfileInfoImage = () => {
   const { data: sessionData } = useSession();
   const user = sessionData?.user;
 
-  if (user?.image) {
-    return (
-      // NOTE: Profile avatars make little sense to be optimized as they are only
-      //       shown to the user and are probably already cached in their browser
-      // eslint-disable-next-line @next/next/no-img-element
-      <img
-        src={user.image}
-        alt=""
-        className="size-8 overflow-hidden rounded-full"
-      />
-    );
+  if (!user?.image) {
+    return;
   }
 
-  return <></>;
+  return (
+    // NOTE: Profile avatars make little sense to be optimized as they are only
+    //       shown to the user and are probably already cached in their browser
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      src={user.image}
+      alt=""
+      className="size-8 overflow-hidden rounded-full"
+    />
+  );
 };
 
 export const ProfileInfo = ({ full = false }) => {
   const { data: sessionData } = useSession();
   const user = sessionData?.user;
 
-  if (user) {
-    return (
-      <div
-        className={`flex items-center gap-8 ${
-          full ? "w-full min-w-max justify-between" : ""
-        }`}
-      >
-        {full && <span>{user.name}</span>}
-        <ProfileInfoImage />
-      </div>
-    );
+  if (!user) {
+    return;
   }
 
-  return <></>;
+  return (
+    <div
+      className={classes(
+        "flex items-center gap-8",
+        full && "w-full min-w-max justify-between",
+      )}
+    >
+      {full && <span>{user.name}</span>}
+      <ProfileInfoImage />
+    </div>
+  );
 };
