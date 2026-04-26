159d6c5bc17694d94d0d08801cb0b0cba3090fdd
James <james@strangecyan.com>
2026-04-26 16:38:44 +0100

    Decoupled persisting donations & pixels (#2029)
    * Added alt dequeuer
    
    * Updated sentry configuration to work more effectively locally & added additional queue for pixels
    
    * Updated naming for preview pixels
    
    * Refactor queue checks in donation worker to use endsWith for improved flexibility

--- apps/donations/worker/src/index.ts
+++ apps/donations/worker/src/index.ts
@@ -8,14 +8,12 @@ import superjson, { parse } from "superjson";
 import { setSentryTagsMiddleware } from "./utils/middleware";
 import { getSentryConfig } from "./utils/sentry";
 import { forwardWithoutRoutePrefix } from "./utils/url";
-
 export { DonationsManagerDurableObject } from "./managers/donations";
 export { PixelsManagerDurableObject } from "./managers/pixels";
 
 const app = new Hono<{ Bindings: Env }>();
 app.use(logger());
-
-app.all("*", setSentryTagsMiddleware);
+app.use(setSentryTagsMiddleware());
 
 app.all("/donations/*", async (c) => {
   const manager = c.env.DONATIONS_MANAGER.getByName("alveus");
@@ -54,9 +52,19 @@ export default Sentry.withSentry<Env, string>(getSentryConfig, {
     const donations: Donation[] = batch.messages.map((message) =>
       parse(message.body),
     );
-    await api.donations.createDonations.mutate({ donations });
 
-    const manager = env.PIXELS_MANAGER.getByName(`alveus-${env.MURAL_ID}`);
-    await manager.process(donations);
+    if (batch.queue.endsWith("donations")) {
+      await api.donations.createDonations.mutate({ donations });
+      await env.PIXELS_QUEUE.sendBatch(batch.messages);
+      return;
+    }
+
+    if (batch.queue.endsWith("pixels")) {
+      const manager = env.PIXELS_MANAGER.getByName(`alveus-${env.MURAL_ID}`);
+      await manager.process(donations);
+      return;
+    }
+
+    throw new Error(`Unknown queue: ${batch.queue}`);
   },
 } satisfies ExportedHandler<Env, string>);
