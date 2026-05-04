fdbedcb0af678553762239fa2290ca6408f19c85
MattIPv4 <me@mattcowley.co.uk>
2026-05-04 17:52:16 +0100

    Show placeholders when loading notifications

--- apps/website/src/components/notifications/RecentNotifications.tsx
+++ apps/website/src/components/notifications/RecentNotifications.tsx
@@ -12,6 +12,9 @@ import IconSearch from "@/icons/IconSearch";
 import { selectionButtonClasses } from "../calendar/Schedule";
 import Button from "../content/Button";
 
+const notificationClasses =
+  "flex items-center gap-3 rounded-xl bg-alveus-green/30 px-4 py-2 transition-all hover:scale-102 hover:bg-alveus-green/40";
+
 export function RecentNotifications({ tags }: { tags: Array<string> }) {
   const [search, setSearch] = useState("");
 
@@ -60,42 +63,46 @@ function RecentNotificationsList({
       },
     );
 
-  if (recentNotifications.isPending) {
-    return <p>Loading recent notifications...</p>;
-  }
-
-  if (recentNotifications.status === "error") {
+  if (recentNotifications.isError) {
     return <p>Error loading recent notifications.</p>;
   }
 
-  const hasNotifications = recentNotifications.data?.pages.some(
-    (page) => page.items.length > 0,
-  );
-
-  if (!hasNotifications) {
+  if (
+    recentNotifications.isSuccess &&
+    !recentNotifications.data?.pages.some((page) => page.items.length > 0)
+  ) {
     return <p>No recent notifications.</p>;
   }
 
   return (
     <>
       <ul className="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
-        {recentNotifications.data?.pages.map((page, i) => (
-          <Fragment
-            key={page.nextCursor || `page-${page.items[0]?.id || "empty"}`}
-          >
-            {page.items.map((notification) => (
+        {recentNotifications.isPending
+          ? Array.from({ length: 9 }).map((_, i) => (
               <li
-                key={notification.id}
-                className={classes(
-                  "flex items-center gap-3 rounded-xl bg-alveus-green/30 px-4 py-2 transition-all hover:scale-102 hover:bg-alveus-green/40",
-                  i !== 0 && "starting:opacity-0",
-                )}
+                key={i}
+                className={classes(notificationClasses, "animate-pulse")}
               >
-                <NotificationEntry notification={notification} />
+                <div className="h-22" />
               </li>
+            ))
+          : recentNotifications.data?.pages.map((page, i) => (
+              <Fragment
+                key={page.nextCursor || `page-${page.items[0]?.id || "empty"}`}
+              >
+                {page.items.map((notification) => (
+                  <li
+                    key={notification.id}
+                    className={classes(
+                      notificationClasses,
+                      i !== 0 && "starting:opacity-0",
+                    )}
+                  >
+                    <NotificationEntry notification={notification} />
+                  </li>
+                ))}
+              </Fragment>
             ))}
-          </Fragment>
-        ))}
       </ul>
 
       {recentNotifications.hasNextPage && (
