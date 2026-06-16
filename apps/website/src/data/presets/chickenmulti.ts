ca56aa2a6b30789d9a165e4b897cd5cf14e906ff
MattIPv4 <me@mattcowley.co.uk>
2026-06-16 10:30:05 +0100

    Restore chickenmulti description

--- apps/website/src/data/presets/chickenmulti.ts
+++ apps/website/src/data/presets/chickenmulti.ts
@@ -4,7 +4,7 @@ const chickenmulti = {
   title: "Chicken Multi-View",
   group: "chicken",
   multi: {
-    description: "multi",
+    description: "Chicken camera with chicken indoor picture-in-picture",
     image: multi,
     cameras: ["chicken", "chickenindoor"],
     // modified: 2025-10-09T11:10:30.496Z
