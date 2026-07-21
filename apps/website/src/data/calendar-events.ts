dd961224f9f7228cad4e54f93a5aa9181dab6166
MattIPv4 <me@mattcowley.co.uk>
2026-07-21 01:04:37 +0100

    Add Chandler Power Hour on Fridays

--- apps/website/src/data/calendar-events.ts
+++ apps/website/src/data/calendar-events.ts
@@ -155,6 +155,14 @@ export const regularEventsWeekly = [
   // Friday
   [
     {
+      title: "Chandler Power Hour",
+      description:
+        "Join Chandler for an hour of chaos around the sanctuary to start your Friday.",
+      category: "Alveus Regular Stream",
+      link: "https://twitch.tv/AlveusSanctuary",
+      startTime: { hour: 10, minute: 0 },
+    },
+    {
       title: "Show & Tell",
       description:
         "Join us as we look at what the Alveus community has been doing recently.",
